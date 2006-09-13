1a7578bcee9b68c3295c590400fa1b430b7ed1af
Jérôme Louvel <jerome.louvel@noelios.com>
2006-09-13 18:09:26 +0000

    - Added "converter" parameter to HttpServer and HttpClient base classes and thus to all HTTP connectors (Jetty, Simple, AsyncWeb, HttpUrlConnection).

--- plugins/internal/com.noelios.restlet/src/com/noelios/restlet/connector/HttpClient.java
+++ plugins/internal/com.noelios.restlet/src/com/noelios/restlet/connector/HttpClient.java
@@ -30,6 +30,20 @@ import org.restlet.connector.Client;
 
 /**
  * Base HTTP client connector.
+ * <table>
+ * 	<tr>
+ * 		<th>Parameter name</th>
+ * 		<th>Value type</th>
+ * 		<th>Default value</th>
+ * 		<th>Description</th>
+ * 	</tr>
+ * 	<tr>
+ * 		<td>converter</td>
+ * 		<td>String</td>
+ * 		<td>com.noelios.restlet.connector.HttpClientConverter</td>
+ * 		<td>The qualified class name of the converter from uniform calls to HTTP client calls.</td>
+ * 	</tr>
+ * </table>
  * @author Jerome Louvel (contact@noelios.com) <a href="http://www.noelios.com/">Noelios Consulting</a>
  */
 public abstract class HttpClient extends Client
@@ -40,12 +54,16 @@ public abstract class HttpClient extends Client
    /** The converter from uniform calls to HTTP calls. */
    private HttpClientConverter converter;
 
+	/** The qualified class name of the call converter. */
+	private String converterName;
+
    /**
     * Constructor.
     */
    public HttpClient()
    {
       this.converter = null;
+		this.converterName = null;
    }
 
    /**
@@ -73,15 +91,59 @@ public abstract class HttpClient extends Client
       }
    }
 
-   /**
-    * Returns the converter from uniform calls to HTTP calls.
-    * @return the converter from uniform calls to HTTP calls.
-    */
-   public HttpClientConverter getConverter()
-   {
-      if(this.converter == null) this.converter = new HttpClientConverter();
-      return this.converter;
-   }
+	/**
+	 * Returns the converter from uniform calls to HTTP calls.
+	 * @return the converter from uniform calls to HTTP calls.
+	 */
+	public HttpClientConverter getConverter()
+	{
+		if (this.converter == null)
+		{
+			if (getConverterName() != null)
+			{
+				try
+				{
+					// Load the converter class using the given class name
+					Class converterClass = Class.forName(getConverterName());
+					this.converter = (HttpClientConverter) converterClass.newInstance();
+				}
+				catch (ClassNotFoundException e)
+				{
+					getContext().getLogger().log(
+							Level.WARNING,
+							"Couldn't find the converter class. Please check that your classpath includes "
+									+ converterName, e);
+				}
+				catch (InstantiationException e)
+				{
+					getContext()
+							.getLogger()
+							.log(
+									Level.WARNING,
+									"Couldn't instantiate the converter class. Please check this class has an empty constructor "
+											+ converterName, e);
+				}
+				catch (IllegalAccessException e)
+				{
+					getContext()
+							.getLogger()
+							.log(
+									Level.WARNING,
+									"Couldn't instantiate the converter class. Please check that you have to proper access rights to "
+											+ converterName, e);
+				}
+			}
+
+			if (this.converter == null)
+			{
+				getContext().getLogger().log(Level.WARNING,
+						"Instantiating the default HTTP call converter");
+				this.converter = new HttpClientConverter();
+			}
+		}
+
+		return this.converter;
+	}
 
    /**
     * Sets the converter from uniform calls to HTTP calls.
@@ -91,4 +153,28 @@ public abstract class HttpClient extends Client
    {
       this.converter = converter;
    }
+
+	/**
+	 * Returns the qualified class name of the call converter.
+	 * @return the qualified class name of the call converter.
+	 */
+	public String getConverterName()
+	{
+		if (this.converterName == null)
+		{
+			this.converterName = getContext().getParameters().getFirstValue("converter",
+					HttpClientConverter.class.getCanonicalName());
+		}
+
+		return this.converterName;
+	}
+
+	/**
+	 * Sets the qualified class name of the call converter.
+	 * @param converterName The qualified class name of the call converter.
+	 */
+	public void setConverterName(String converterName)
+	{
+		this.converterName = converterName;
+	}
 }
