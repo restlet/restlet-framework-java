37a16f37a16d8b67eddda812b4cb9b5c8d02e4c9
Thierry Boileau <thboileau@gmail.com>
2007-03-16 22:02:07 +0000

    Fixed typo mistake in ByteUtils comments.

--- modules/org.restlet/src/org/restlet/util/ByteUtils.java
+++ modules/org.restlet/src/org/restlet/util/ByteUtils.java
@@ -220,7 +220,7 @@ public final class ByteUtils {
 
     /**
      * Private constructor to ensure that the class acts as a true utility class
-     * i.e. it isn't instatiable and extensible.
+     * i.e. it isn't instantiable and extensible.
      */
     private ByteUtils() {
 
