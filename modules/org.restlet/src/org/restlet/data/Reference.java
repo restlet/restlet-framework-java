ead8d972b456e765391916d6a1364cf746b8b5ad
Thierry Boileau <thboileau@gmail.com>
2007-03-21 09:20:33 +0000

    Updated the normalization process of a reference by ensuring that default ports are ignored.

--- modules/org.restlet/src/org/restlet/data/Reference.java
+++ modules/org.restlet/src/org/restlet/data/Reference.java
@@ -33,21 +33,21 @@ import java.util.logging.Logger;
  * conforms to the RFC 3986 specifying URIs and follow its naming conventions.<br/>
  * 
  * <pre>
- *                          URI reference        = absolute-reference | relative-reference
- *                       
- *                          absolute-reference   = scheme &quot;:&quot; scheme-specific-part [ &quot;#&quot; fragment ]
- *                          scheme-specific-part = ( hierarchical-part [ &quot;?&quot; query ] ) | opaque-part
- *                          hierarchical-part    = ( &quot;//&quot; authority path-abempty ) | path-absolute | path-rootless | path-empty
- *                          authority            = [ user-info &quot;@&quot; ] host-domain [ &quot;:&quot; host-port ]
- *                                                         
- *                          relative-reference   = relative-part [ &quot;?&quot; query ] [ &quot;#&quot; fragment ]
- *                          relative-part        = ( &quot;//&quot; authority path-abempty ) | path-absolute | path-noscheme | path-empty
- *                                                         
- *                          path-abempty         = begins with &quot;/&quot; or is empty
- *                          path-absolute        = begins with &quot;/&quot; but not &quot;//&quot;
- *                          path-noscheme        = begins with a non-colon segment
- *                          path-rootless        = begins with a segment
- *                          path-empty           = zero characters
+ *                             URI reference        = absolute-reference | relative-reference
+ *                          
+ *                             absolute-reference   = scheme &quot;:&quot; scheme-specific-part [ &quot;#&quot; fragment ]
+ *                             scheme-specific-part = ( hierarchical-part [ &quot;?&quot; query ] ) | opaque-part
+ *                             hierarchical-part    = ( &quot;//&quot; authority path-abempty ) | path-absolute | path-rootless | path-empty
+ *                             authority            = [ user-info &quot;@&quot; ] host-domain [ &quot;:&quot; host-port ]
+ *                                                            
+ *                             relative-reference   = relative-part [ &quot;?&quot; query ] [ &quot;#&quot; fragment ]
+ *                             relative-part        = ( &quot;//&quot; authority path-abempty ) | path-absolute | path-noscheme | path-empty
+ *                                                            
+ *                             path-abempty         = begins with &quot;/&quot; or is empty
+ *                             path-absolute        = begins with &quot;/&quot; but not &quot;//&quot;
+ *                             path-noscheme        = begins with a non-colon segment
+ *                             path-rootless        = begins with a segment
+ *                             path-empty           = zero characters
  * </pre>
  * 
  * Note that this class doesn't encode or decode the reserved characters. It
@@ -1274,8 +1274,7 @@ public class Reference {
      */
     public Reference normalize() {
         // 1. The input buffer is initialized with the now-appended path
-        // components
-        // and the output buffer is initialized to the empty string.
+        // components and the output buffer is initialized to the empty string.
         StringBuilder output = new StringBuilder();
         StringBuilder input = new StringBuilder();
         String path = getPath();
@@ -1357,6 +1356,15 @@ public class Reference {
         setScheme(getScheme());
         setHostDomain(getHostDomain());
 
+        // Remove the port if it is equal to the default port of the reference's
+        // Protocol.
+        int hostPort = getHostPort();
+        if (hostPort != -1) {
+            int defaultPort = Protocol.valueOf(getScheme()).getDefaultPort();
+            if (hostPort == defaultPort) {
+                setHostPort(null);
+            }
+        }
         return this;
     }
 
