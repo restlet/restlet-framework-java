8332734eed1b3d498ef295d8d5f2de6f8e480778
Jérôme Louvel <jerome.louvel@noelios.com>
2007-07-24 21:13:10 +0000

    - Added Spring test case

---
+++ modules/org.restlet.test/src/org/restlet/test/SpringTestCase.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2005-2007 Noelios Consulting.
+ * 
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the "License"). You may not use this file except in
+ * compliance with the License.
+ * 
+ * You can obtain a copy of the license at
+ * http://www.opensource.org/licenses/cddl1.txt See the License for the specific
+ * language governing permissions and limitations under the License.
+ * 
+ * When distributing Covered Code, include this CDDL HEADER in each file and
+ * include the License file at http://www.opensource.org/licenses/cddl1.txt If
+ * applicable, add the following below this CDDL HEADER, with the fields
+ * enclosed by brackets "[]" replaced with your own identifying information:
+ * Portions Copyright [yyyy] [name of copyright owner]
+ */
+
+package org.restlet.test;
+
+import junit.framework.TestCase;
+
+import org.restlet.Component;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+/**
+ * Unit test case for the Spring extension.
+ * 
+ * @author Jerome Louvel (contact@noelios.com)
+ */
+public class SpringTestCase extends TestCase {
+
+    public void testSpring() throws Exception {
+        // Load the Spring container
+        ClassPathResource resource = new ClassPathResource(
+                "org/restlet/test/SpringTestCase.xml");
+        BeanFactory factory = new XmlBeanFactory(resource);
+
+        // Start the Restlet component
+        Component component = (Component) factory.getBean("component");
+        component.start();
+        Thread.currentThread().join();
+    }
+
+}
