bda547c34ac757172d95b497fb5412852d56b38a
Thierry Boileau <thboileau@gmail.com>
2007-09-27 19:53:48 +0000

    Fixed bug in the SimpleWeb connector which requires the request input stream to be fully read. Otherwise, the connectors hangs. Reported by Adam Taft, Nicolas Capdevila and Rob Heittman.

--- modules/com.noelios.restlet.ext.simple_3.1/src/com/noelios/restlet/ext/simple/SimpleProtocolHandler.java
+++ modules/com.noelios.restlet.ext.simple_3.1/src/com/noelios/restlet/ext/simple/SimpleProtocolHandler.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2005-2007 Noelios Consulting.
- * 
+ *
  * The contents of this file are subject to the terms of the Common Development
  * and Distribution License (the "License"). You may not use this file except in
  * compliance with the License.
- * 
+ *
  * You can obtain a copy of the license at
  * http://www.opensource.org/licenses/cddl1.txt See the License for the specific
  * language governing permissions and limitations under the License.
- * 
+ *
  * When distributing Covered Code, include this CDDL HEADER in each file and
  * include the License file at http://www.opensource.org/licenses/cddl1.txt If
  * applicable, add the following below this CDDL HEADER, with the fields
@@ -27,7 +27,7 @@ import simple.http.Response;
 
 /**
  * Simple protocol handler delegating the calls to the Restlet server helper.
- * 
+ *
  * @author Jerome Louvel (contact@noelios.com) <a
  *         href="http://www.noelios.com/">Noelios Consulting</a>
  */
@@ -37,7 +37,7 @@ public class SimpleProtocolHandler implements ProtocolHandler {
 
     /**
      * Constructor.
-     * 
+     *
      * @param helper
      *            The delegate Restlet server helper.
      */
@@ -47,7 +47,7 @@ public class SimpleProtocolHandler implements ProtocolHandler {
 
     /**
      * Returns the delegate Restlet server helper.
-     * 
+     *
      * @return The delegate Restlet server helper.
      */
     public SimpleServerHelper getHelper() {
@@ -56,7 +56,7 @@ public class SimpleProtocolHandler implements ProtocolHandler {
 
     /**
      * Handles a Simple request/response transaction.
-     * 
+     *
      * @param request
      *            The Simple request.
      * @param response
@@ -68,6 +68,14 @@ public class SimpleProtocolHandler implements ProtocolHandler {
                         getHelper().isConfidential()));
 
         try {
+            // Once the request is handled, the request input stream must be
+            // entirely consumed. Not doing so blocks invariably the transaction
+            // managed by the SimpleWeb connector.
+            if (request.getInputStream() != null) {
+                while (request.getInputStream().read() != -1) {
+                    // just consume the stream
+                }
+            }
             response.getOutputStream().close();
         } catch (IOException ioe) {
             getHelper()
