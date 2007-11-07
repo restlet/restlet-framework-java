46dd5e4a3c16398da6004bfef3c0d4948f67a4cf
Thierry Boileau <thboileau@gmail.com>
2007-11-07 23:52:59 +0000

    Directory restlet not showing files with whitespaces. Reported by Davide Angelocola.

--- modules/com.noelios.restlet/src/com/noelios/restlet/local/FileClientHelper.java
+++ modules/com.noelios.restlet/src/com/noelios/restlet/local/FileClientHelper.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2005-2007 Noelios Consulting.
- * 
+ *
  * The contents of this file are subject to the terms of the Common Development
  * and Distribution License (the "License"). You may not use this file except in
  * compliance with the License.
- * 
+ *
  * You can obtain a copy of the license at
  * http://www.opensource.org/licenses/cddl1.txt See the License for the specific
  * language governing permissions and limitations under the License.
- * 
+ *
  * When distributing Covered Code, include this CDDL HEADER in each file and
  * include the License file at http://www.opensource.org/licenses/cddl1.txt If
  * applicable, add the following below this CDDL HEADER, with the fields
@@ -54,14 +54,14 @@ import org.restlet.util.ByteUtils;
 
 /**
  * Connector to the file resources accessible
- * 
+ *
  * @author Jerome Louvel (contact@noelios.com)
  * @author Thierry Boileau
  */
 public class FileClientHelper extends LocalClientHelper {
     /**
      * Constructor.
-     * 
+     *
      * @param client
      *                The client to help.
      */
@@ -72,7 +72,7 @@ public class FileClientHelper extends LocalClientHelper {
 
     /**
      * Handles a call.
-     * 
+     *
      * @param request
      *                The request to handle.
      * @param response
@@ -97,7 +97,7 @@ public class FileClientHelper extends LocalClientHelper {
 
     /**
      * Handles a call for the FILE protocol.
-     * 
+     *
      * @param request
      *                The request to handle.
      * @param response
@@ -137,7 +137,7 @@ public class FileClientHelper extends LocalClientHelper {
                         .lastIndexOf("/"));
                 String encodedFileName = path
                         .substring(path.lastIndexOf("/") + 1);
-                
+
                 for (File entry : files) {
                     if (entry.getName().startsWith(baseName)) {
                         rl.add(LocalReference
@@ -514,7 +514,7 @@ public class FileClientHelper extends LocalClientHelper {
     /**
      * Returns the base name as the longest part of the name without known
      * extensions (beginning from the left)
-     * 
+     *
      * @param file
      * @param metadataService
      * @return the base name of the file
@@ -534,7 +534,7 @@ public class FileClientHelper extends LocalClientHelper {
 
     /**
      * Returns the Set of extensions of a file
-     * 
+     *
      * @param file
      * @param metadataService
      * @return
@@ -561,7 +561,7 @@ public class FileClientHelper extends LocalClientHelper {
      * Checks that the URI and the representation are compatible. The whole set
      * of metadata of the representation must be included in the set of those of
      * the URI
-     * 
+     *
      * @param fileName
      *                The name of the resource
      * @param metadataService
@@ -599,7 +599,7 @@ public class FileClientHelper extends LocalClientHelper {
 
     /**
      * Check that all extensions of the file correspond to a known metadata
-     * 
+     *
      * @param file
      * @param metadataService
      * @param representation
@@ -617,13 +617,13 @@ public class FileClientHelper extends LocalClientHelper {
 
         return knownExtension;
     }
-    
+
 
     /**
      * Percent-encodes the given percent-decoded variant name of a resource
      * whose percent-encoded name is given. Tries to match the longest common
      * part of both encoded file name and decoded variant name.
-     * 
+     *
      * @param encodedFileName
      *                the percent-encoded name of the initial resource
      * @param decodedVariantFileName
@@ -664,7 +664,12 @@ public class FileClientHelper extends LocalClientHelper {
             return encodedFileName.substring(0, j)
                     + decodedVariantFileName.substring(i - 1);
         } else {
-            return encodedFileName.substring(0, j);
+            if(j == encodedFileName.length()){
+                return encodedFileName.substring(0, j)
+                + decodedVariantFileName.substring(i);
+            } else {
+                return encodedFileName.substring(0, j);
+            }
         }
     }
 }
