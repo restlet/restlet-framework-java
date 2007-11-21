60fa26991a5d821967e27bb657019a9adb4e3e65
Jérôme Louvel <jerome.louvel@noelios.com>
2007-11-21 21:37:25 +0000

    - Simple could log an exception if the input entity was already closed when returning    from the handling, as we are trying to consume unread bytes. Reported by Matt Allen.

--- modules/com.noelios.restlet.ext.simple_3.1/src/com/noelios/restlet/ext/simple/SimpleProtocolHandler.java
+++ modules/com.noelios.restlet.ext.simple_3.1/src/com/noelios/restlet/ext/simple/SimpleProtocolHandler.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2005-2007 Noelios Consulting.
- *
+ * 
  * The contents of this file are subject to the terms of the Common Development
  * and Distribution License (the "License"). You may not use this file except in
  * compliance with the License.
- *
+ * 
  * You can obtain a copy of the license at
  * http://www.opensource.org/licenses/cddl1.txt See the License for the specific
  * language governing permissions and limitations under the License.
- *
+ * 
  * When distributing Covered Code, include this CDDL HEADER in each file and
  * include the License file at http://www.opensource.org/licenses/cddl1.txt If
  * applicable, add the following below this CDDL HEADER, with the fields
@@ -27,7 +27,7 @@ import simple.http.Response;
 
 /**
  * Simple protocol handler delegating the calls to the Restlet server helper.
- *
+ * 
  * @author Jerome Louvel (contact@noelios.com) <a
  *         href="http://www.noelios.com/">Noelios Consulting</a>
  */
@@ -37,9 +37,9 @@ public class SimpleProtocolHandler implements ProtocolHandler {
 
     /**
      * Constructor.
-     *
+     * 
      * @param helper
-     *            The delegate Restlet server helper.
+     *                The delegate Restlet server helper.
      */
     public SimpleProtocolHandler(SimpleServerHelper helper) {
         this.helper = helper;
@@ -47,7 +47,7 @@ public class SimpleProtocolHandler implements ProtocolHandler {
 
     /**
      * Returns the delegate Restlet server helper.
-     *
+     * 
      * @return The delegate Restlet server helper.
      */
     public SimpleServerHelper getHelper() {
@@ -56,11 +56,11 @@ public class SimpleProtocolHandler implements ProtocolHandler {
 
     /**
      * Handles a Simple request/response transaction.
-     *
+     * 
      * @param request
-     *            The Simple request.
+     *                The Simple request.
      * @param response
-     *            The Simple response.
+     *                The Simple response.
      */
     public void handle(Request request, Response response) {
         getHelper().handle(
@@ -72,16 +72,23 @@ public class SimpleProtocolHandler implements ProtocolHandler {
             // entirely consumed. Not doing so blocks invariably the transaction
             // managed by the SimpleWeb connector.
             if (request.getInputStream() != null) {
-                while (request.getInputStream().read() != -1) {
-                    // just consume the stream
+                try {
+                    while (request.getInputStream().read() != -1) {
+                        // just consume the stream
+                    }
+                } catch (IOException ioe) {
+                    // This is probably ok, the stream was certainly already
+                    // closed by the Representation.release() method for
+                    // example.
                 }
             }
+
             response.getOutputStream().close();
         } catch (IOException ioe) {
             getHelper()
                     .getLogger()
                     .log(
-                            Level.INFO,
+                            Level.FINE,
                             "Exception while closing the Simple response's output stream",
                             ioe);
         }
