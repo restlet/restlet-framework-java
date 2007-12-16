420828314e4430757e9a177ebae25edf2c7e798b
Jérôme Louvel <jerome.louvel@noelios.com>
2007-12-16 21:12:36 +0000

    - Fixed casting issue on RouteList. Regression in 1.0.6. Reported by Valdis Rigdon and fixed    by Kevin Conaway.

--- modules/org.restlet.test/src/org/restlet/test/RestletTestSuite.java
+++ modules/org.restlet.test/src/org/restlet/test/RestletTestSuite.java
@@ -40,6 +40,7 @@ public class RestletTestSuite extends TestSuite {
         addTestSuite(RedirectTestCase.class);
         addTestSuite(ReferenceTestCase.class);
         addTestSuite(RestartTestCase.class);
+        addTestSuite(RouteListTestCase.class);
         addTestSuite(TransformerTestCase.class);
         addTestSuite(VelocityTestCase.class);
         addTestSuite(HTTPBasicTest.class);
