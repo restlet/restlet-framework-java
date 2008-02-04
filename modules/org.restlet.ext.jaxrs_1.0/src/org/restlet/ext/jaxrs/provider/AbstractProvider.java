049351313323764fe3ad0a2e9eea9a2d62cbce42
Stephan Koops <Stephan.Koops@web.de>
2008-02-04 14:33:01 +0000

    JAX-RS extension continued - default MessageBodyReaders and MessageBodyWriters load on startup - all MessageBodyReaders and MessageBodyWriters loadable from Classpath (iterating not over all possibilities of classpath yet) - Add CDDL-License header in some files - JAX-RS API updated to newest version (2008-02-01)

---
+++ modules/org.restlet.ext.jaxrs_1.0/src/org/restlet/ext/jaxrs/provider/AbstractProvider.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2005-2008 Noelios Consulting.
+ * 
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the "License"). You may not use this file except in
+ * compliance with the License.
+ * 
+ * You can obtain a copy of the license at
+ * http://www.opensource.org/licenses/cddl1.txt See the License for the specific
+ * language governing permissions and limitations under the License.
+ * 
+ * When distributing Covered Code, include this CDDL HEADER in each file and
+ * include the License file at http://www.opensource.org/licenses/cddl1.txt If
+ * applicable, add the following below this CDDL HEADER, with the fields
+ * enclosed by brackets "[]" replaced with your own identifying information:
+ * Portions Copyright [yyyy] [name of copyright owner]
+ */
+package org.restlet.ext.jaxrs.provider;
+
+import javax.ws.rs.ext.MessageBodyReader;
+import javax.ws.rs.ext.MessageBodyWriter;
+
+/**
+ * This abstract class ease the development of {@link MessageBodyReader}s and
+ * {@link MessageBodyWriter}.
+ * 
+ * @author Stephan Koops
+ * @param <T> the type that can be read and written
+ * @see MessageBodyReader
+ * @see MessageBodyWriter
+ */
+public abstract class AbstractProvider<T> implements MessageBodyWriter<T>,
+        MessageBodyReader<T> {
+
+    protected abstract boolean isReadableAndWriteable(Class<?> type);
+
+    public final boolean isWriteable(Class<?> type) {
+        return isReadableAndWriteable(type);
+    }
+
+    public final boolean isReadable(Class<?> type) {
+        return isReadableAndWriteable(type);
+    }
+}
---
+++ modules/org.restlet.ext.jaxrs_1.0/src/org/restlet/ext/jaxrs/provider/StringProvider.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2005-2008 Noelios Consulting.
+ * 
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the "License"). You may not use this file except in
+ * compliance with the License.
+ * 
+ * You can obtain a copy of the license at
+ * http://www.opensource.org/licenses/cddl1.txt See the License for the specific
+ * language governing permissions and limitations under the License.
+ * 
+ * When distributing Covered Code, include this CDDL HEADER in each file and
+ * include the License file at http://www.opensource.org/licenses/cddl1.txt If
+ * applicable, add the following below this CDDL HEADER, with the fields
+ * enclosed by brackets "[]" replaced with your own identifying information:
+ * Portions Copyright [yyyy] [name of copyright owner]
+ */
+package org.restlet.ext.jaxrs.provider;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.ws.rs.ProduceMime;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.Provider;
+
+import org.restlet.ext.jaxrs.todo.NotYetImplementedException;
+
+/**
+ * @author Stephan Koops
+ * 
+ */
+@Provider
+@ProduceMime("text/*")
+public class StringProvider extends AbstractProvider<CharSequence> {
+
+    /**
+     * 
+     */
+    public StringProvider() {
+        //
+    }
+
+    /**
+     * @see javax.ws.rs.ext.MessageBodyWriter#getSize(java.lang.Object)
+     */
+    public long getSize(CharSequence t) {
+        return t.length();
+    }
+
+    /**
+     * @see javax.ws.rs.ext.MessageBodyWriter#isWriteable(java.lang.Class)
+     * @see javax.ws.rs.ext.MessageBodyReader#isReadable(Class)
+     */
+    @Override
+    protected boolean isReadableAndWriteable(Class<?> type) {
+        return CharSequence.class.isAssignableFrom(type);
+    }
+
+    /**
+     * @see javax.ws.rs.ext.MessageBodyWriter#writeTo(java.lang.Object,
+     *      javax.ws.rs.core.MediaType, javax.ws.rs.core.MultivaluedMap,
+     *      java.io.OutputStream)
+     */
+    public void writeTo(CharSequence cs, MediaType mediaType,
+            MultivaluedMap<String, Object> httpHeaders,
+            OutputStream entityStream) throws IOException {
+        byte[] array = cs.toString().getBytes();
+        entityStream.write(array);
+    }
+
+    public CharSequence readFrom(Class<CharSequence> type, MediaType mediaType,
+            MultivaluedMap<String, String> httpHeaders, InputStream entityStream)
+            throws IOException {
+        throw new NotYetImplementedException();
+    }
+}
