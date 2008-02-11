0b1f0071b97c374fdeea6c9edbdbe4aca1f3e936
Stephan Koops <Stephan.Koops@web.de>
2008-02-11 13:15:55 +0000

    JAX-RS-Extension - continued a very little bit in XmlTransformSourceProvider

--- modules/org.restlet.ext.jaxrs_1.0/src/org/restlet/ext/jaxrs/provider/XmlTransformSourceProvider.java
+++ modules/org.restlet.ext.jaxrs_1.0/src/org/restlet/ext/jaxrs/provider/XmlTransformSourceProvider.java
@@ -7,6 +7,7 @@ import java.io.OutputStream;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
 
 import org.restlet.ext.jaxrs.todo.NotYetImplementedException;
 
@@ -16,11 +17,10 @@ import org.restlet.ext.jaxrs.todo.NotYetImplementedException;
  */
 public class XmlTransformSourceProvider extends AbstractProvider<Source> {
 
-    // TODO wie geht man mit javax.xml.transform.Source um?
-    
+    // TODO JSR311: wie geht man mit javax.xml.transform.Source um?
+
     @Override
     public long getSize(Source object) {
-        // TODO Auto-generated method stub
         return -1;
     }
 
@@ -30,17 +30,28 @@ public class XmlTransformSourceProvider extends AbstractProvider<Source> {
         return false;
     }
 
+    /**
+     * @see org.restlet.ext.jaxrs.provider.AbstractProvider#readFrom(java.lang.Class,
+     *      javax.ws.rs.core.MediaType, javax.ws.rs.core.MultivaluedMap,
+     *      java.io.InputStream)
+     */
     @Override
     public Source readFrom(Class<Source> type, MediaType mediaType,
             MultivaluedMap<String, String> httpHeaders, InputStream entityStream)
             throws IOException {
-        throw new NotYetImplementedException();
+        return new StreamSource(entityStream);
     }
 
     @Override
     public void writeTo(Source source, MediaType mediaType,
             MultivaluedMap<String, Object> httpHeaders,
             OutputStream entityStream) throws IOException {
+        // StreamResult streamResult = new StreamResult(entityStream);
+        // TransformerFactory transformerFactory = TransformerFactory
+        //         .newInstance();
+        // Source xsltSource = null;
+        // Transformer trans = transformerFactory.newTransformer(xsltSource);
+        // trans.transform(source, streamResult);
         throw new NotYetImplementedException();
     }
 }
