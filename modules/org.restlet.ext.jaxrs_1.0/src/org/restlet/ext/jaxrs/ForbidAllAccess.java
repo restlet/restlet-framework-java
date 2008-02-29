1cb2a964ab0cf43591aea540ae45262f37fdd209
Jérôme Louvel <jerome.louvel@noelios.com>
2008-02-29 20:19:21 +0000

    - Javadocs

--- modules/org.restlet.ext.jaxrs_1.0/src/org/restlet/ext/jaxrs/ForbidAllAccess.java
+++ modules/org.restlet.ext.jaxrs_1.0/src/org/restlet/ext/jaxrs/ForbidAllAccess.java
@@ -1,22 +1,18 @@
 /*
  * Copyright 2005-2008 Noelios Consulting.
- *
- * The contents of this file are subject to the terms
- * of the Common Development and Distribution License
- * (the "License").  You may not use this file except
- * in compliance with the License.
- *
+ * 
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the "License"). You may not use this file except in
+ * compliance with the License.
+ * 
  * You can obtain a copy of the license at
- * http://www.opensource.org/licenses/cddl1.txt
- * See the License for the specific language governing
- * permissions and limitations under the License.
- *
- * When distributing Covered Code, include this CDDL
- * HEADER in each file and include the License file at
- * http://www.opensource.org/licenses/cddl1.txt
- * If applicable, add the following below this CDDL
- * HEADER, with the fields enclosed by brackets "[]"
- * replaced with your own identifying information:
+ * http://www.opensource.org/licenses/cddl1.txt See the License for the specific
+ * language governing permissions and limitations under the License.
+ * 
+ * When distributing Covered Code, include this CDDL HEADER in each file and
+ * include the License file at http://www.opensource.org/licenses/cddl1.txt If
+ * applicable, add the following below this CDDL HEADER, with the fields
+ * enclosed by brackets "[]" replaced with your own identifying information:
  * Portions Copyright [yyyy] [name of copyright owner]
  */
 package org.restlet.ext.jaxrs;
@@ -25,6 +21,7 @@ import java.security.Principal;
 
 /**
  * An AccessControl that forbid every what it is requested.
+ * 
  * @see AccessControl
  * @see AllowAllAccess
  * @see ThrowExcAccessControl
@@ -33,20 +30,21 @@ import java.security.Principal;
 public class ForbidAllAccess implements AccessControl {
 
     private static ForbidAllAccess instance;
-    
+
     /**
      * Returns an instance of the AllowAllAccess
+     * 
      * @return the singelton instance.
      */
-    public static ForbidAllAccess getInstance()
-    {
-        if(instance == null)
+    public static ForbidAllAccess getInstance() {
+        if (instance == null)
             instance = new ForbidAllAccess();
         return instance;
     }
-    
+
     /**
-     * @see org.restlet.ext.jaxrs.AccessControl#isUserInRole(Principal, java.lang.String)
+     * @see org.restlet.ext.jaxrs.AccessControl#isUserInRole(Principal,
+     *      java.lang.String)
      */
     public boolean isUserInRole(Principal principal, String role) {
         return false;
