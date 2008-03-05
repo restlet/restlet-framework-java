c416ff482678971c42f8b1bc1daf6b7ef2083597
Marcelo F. Ochoa <marcelo.ochoa@gmail.com>
2008-03-05 19:31:12 +0000

    refactored source according to new methods at ServerServlet, overrding getClass and createCall

--- modules/com.noelios.restlet.ext.xdb_11.1/src/com/noelios/restlet/ext/xdb/XdbServerServlet.java
+++ modules/com.noelios.restlet.ext.xdb_11.1/src/com/noelios/restlet/ext/xdb/XdbServerServlet.java
@@ -18,15 +18,11 @@
 
 package com.noelios.restlet.ext.xdb;
 
-import com.noelios.restlet.application.ApplicationContext;
 import com.noelios.restlet.ext.servlet.ServerServlet;
-import com.noelios.restlet.ext.servlet.ServletContextAdapter;
+import com.noelios.restlet.http.HttpServerCall;
 import com.noelios.restlet.http.HttpServerHelper;
 
-import java.io.IOException;
-
 import java.lang.reflect.InvocationTargetException;
-
 import java.lang.reflect.Method;
 
 import java.sql.CallableStatement;
@@ -39,7 +35,6 @@ import java.sql.Statement;
 import java.sql.Types;
 
 import java.util.ArrayList;
-import java.util.Enumeration;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -47,8 +42,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.restlet.Application;
 import org.restlet.Component;
-import org.restlet.Context;
-import org.restlet.Route;
 import org.restlet.Server;
 import org.restlet.data.Protocol;
 import org.restlet.util.Engine;
@@ -100,18 +93,6 @@ import org.restlet.util.Engine;
  * @author Marcelo F. Ochoa (mochoa@ieee.org)
  */
 public class XdbServerServlet extends ServerServlet {
-    /**
-     * The Servlet initialization parameter name containing the list of coma
-     * separated string of the query extract arguments separated by ; for
-     * example keywords,kwd,true;xx,yy,false. This parameters will be used with
-     * route.extractQuery(arg1,arg2,arg3) where arg1=keywords, arg2=kwd and
-     * arg3=true for the above example
-     */
-    private static final String EXTRACT_QUERY_ATTRIBUTE = "org.restlet.query";
-
-    /** The default value for the NAME_APPLICATION_ATTRIBUTE parameter. */
-    private static final String EXTRACT_QUERY_ATTRIBUTE_DEFAULT = "";
-
     /** Serial version identifier. */
     private static final long serialVersionUID = 1L;
 
@@ -158,7 +139,7 @@ public class XdbServerServlet extends ServerServlet {
                         "default", "default");
             } else {
                 throw new ServletException(
-                        "Class designed to be used at Server side: jdbc:oracle:thin:@");
+                        "Class designed to be used at Server side using: jdbc:oracle:kprb:@ driver");
             }
         } catch (SQLException s) {
             System.err.println("Exception getting SQL Connection: "
@@ -189,129 +170,8 @@ public class XdbServerServlet extends ServerServlet {
         super();
     }
 
-    /**
-     * Creates the single Application used by this Servlet. Do not attach WAR
-     * protocol handler because WAR are not loaded into XMLDB repository
-     * 
-     * TODO: provide an alternative method to deploy applications
-     * 
-     * @param context
-     *                The Context for the Application.
-     * 
-     * @return The newly created Application or null if unable to create.
-     */
     @Override
-    @SuppressWarnings("unchecked")
-    protected Application createApplication(Context context) {
-        Application application = null;
-
-        // Try to instantiate a new target application
-        // First, find the application class name
-        String applicationClassName = getInitParameter(Application.KEY, null);
-
-        // Load the application class using the given class name
-        if (applicationClassName != null) {
-            try {
-                int doubleDotPos = applicationClassName.indexOf(':');
-                Class targetClass;
-
-                if (doubleDotPos > 0) {
-                    // Use DbmsJava by reflection to avoid dependency to Oracle
-                    // libs at compiling time
-                    Class loaderClass = Engine
-                            .classForName("oracle.aurora.rdbms.DbmsJava");
-                    Method meth = loaderClass.getMethod(
-                            "classForNameAndSchema", new Class[] {
-                                    String.class, String.class });
-                    String sch = applicationClassName
-                            .substring(0, doubleDotPos);
-                    String className = applicationClassName
-                            .substring(doubleDotPos + 1);
-                    log("[Noelios Restlet Engine] - Schema: " + sch
-                            + " class: " + className + " loader: "
-                            + loaderClass);
-                    targetClass = (Class) meth.invoke(null, new Object[] {
-                            className, sch });
-                } else
-                    targetClass = Engine.classForName(applicationClassName);
-
-                try {
-                    // Create a new instance of the application class by
-                    // invoking the constructor with the Context parameter.
-                    application = (Application) targetClass.getConstructor(
-                            Context.class).newInstance(
-                            new ServletContextAdapter(this, context));
-                } catch (NoSuchMethodException e) {
-                    log(
-                            "[Noelios Restlet Engine] - The ServerServlet couldn't invoke the constructor of the target class. Please check this class has a constructor with a single parameter of type Context. The empty constructor and the context setter will be used instead.",
-                            e);
-                    // The constructor with the Context parameter does not
-                    // exist. Instantiate an application with the default
-                    // constructor then invoke the setContext method.
-                    application = (Application) targetClass.getConstructor()
-                            .newInstance();
-
-                    // Set the context based on the Servlet's context
-                    ApplicationContext applicationContext = (ApplicationContext) application
-                            .getContext();
-                    application.setContext(new ApplicationContext(application,
-                            new ServletContextAdapter(this, context),
-                            applicationContext.getLogger()));
-                }
-            } catch (ClassNotFoundException e) {
-                log(
-                        "[Noelios Restlet Engine] - The ServerServlet couldn't find the target class. Please check that your classpath includes "
-                                + applicationClassName, e);
-
-            } catch (InstantiationException e) {
-                log(
-                        "[Noelios Restlet Engine] - The ServerServlet couldn't instantiate the target class. Please check this class has an empty constructor "
-                                + applicationClassName, e);
-            } catch (IllegalAccessException e) {
-                log(
-                        "[Noelios Restlet Engine] - The ServerServlet couldn't instantiate the target class. Please check that you have to proper access rights to "
-                                + applicationClassName, e);
-            } catch (NoSuchMethodException e) {
-                log(
-                        "[Noelios Restlet Engine] - The ServerServlet couldn't invoke the constructor of the target class. Please check this class has a constructor with a single parameter of Context "
-                                + applicationClassName, e);
-            } catch (InvocationTargetException e) {
-                log(
-                        "[Noelios Restlet Engine] - The ServerServlet couldn't instantiate the target class. An exception was thrown while creating "
-                                + applicationClassName, e);
-            }
-        }
-
-        if (application != null) {
-            ApplicationContext applicationContext = (ApplicationContext) application
-                    .getContext();
-
-            // Copy all the servlet parameters into the context
-            String initParam;
-
-            // Copy all the Servlet component initialization parameters
-            javax.servlet.ServletConfig servletConfig = getServletConfig();
-            for (Enumeration<String> enum1 = servletConfig
-                    .getInitParameterNames(); enum1.hasMoreElements();) {
-                initParam = enum1.nextElement();
-                applicationContext.getParameters().add(initParam,
-                        servletConfig.getInitParameter(initParam));
-            }
-
-            // Copy all the Servlet application initialization parameters
-            for (Enumeration<String> enum1 = getServletContext()
-                    .getInitParameterNames(); enum1.hasMoreElements();) {
-                initParam = enum1.nextElement();
-                applicationContext.getParameters().add(initParam,
-                        getServletContext().getInitParameter(initParam));
-            }
-        }
-
-        return application;
-    }
-
-    @Override
-    protected HttpServerHelper createServer(HttpServletRequest request) {
+    public HttpServerHelper createServer(HttpServletRequest request) {
         HttpServerHelper result = null;
         Component component = getComponent();
         Application application = getApplication();
@@ -329,21 +189,8 @@ public class XdbServerServlet extends ServerServlet {
             String uriPattern = request.getServletPath();
             log("[Noelios Restlet Engine] - Attaching application: "
                     + application + " uri: " + uriPattern);
-            Route route = component.getDefaultHost().attach(uriPattern,
+            component.getDefaultHost().attach(uriPattern,
                     application);
-
-            String extractQueries = getInitParameter(EXTRACT_QUERY_ATTRIBUTE,
-                    EXTRACT_QUERY_ATTRIBUTE_DEFAULT);
-            log("[Noelios Restlet Engine] - Parsing query attributes: "
-                    + extractQueries);
-            if (extractQueries != null && extractQueries.length() > 0) {
-                String extractQuery[] = extractQueries.split(";");
-                for (int i = 0; i < extractQuery.length; i++) {
-                    String args[] = extractQuery[i].split(",");
-                    route.extractQuery(args[0], args[1], "true"
-                            .equalsIgnoreCase(args[2]));
-                }
-            }
         }
 
         return result;
@@ -478,16 +325,47 @@ public class XdbServerServlet extends ServerServlet {
     }
 
     @Override
-    public void service(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        HttpServerHelper helper = getServer(request);
+    protected HttpServerCall createCall(Server server, HttpServletRequest request,
+                                     HttpServletResponse response) {
+        return new XdbServletCall(server, request,
+                    response);
+    }
 
-        if (helper != null) {
-            helper.handle(new XdbServletCall(helper.getServer(), request,
-                    response));
-        } else {
-            log("[Noelios Restlet Engine] - Unable to get the Restlet HTTP server connector. Status code 500 returned.");
-            response.sendError(500);
-        }
+    @Override
+    protected Class<?> getClass(String className) throws ClassNotFoundException {
+        int doubleDotPos = className.indexOf(':');
+        Class<?> targetClass;
+
+        if (doubleDotPos > 0) {
+            // Use DbmsJava by reflection to avoid dependency to Oracle libs
+            // at compiling time
+            String sch = className.substring(0, doubleDotPos);
+            String cName = className.substring(doubleDotPos + 1);
+            try {
+                Class loaderClass =
+                    Engine.classForName("oracle.aurora.rdbms.DbmsJava");
+                Method meth =
+                    loaderClass.getMethod("classForNameAndSchema", new Class[] { String.class,
+                                                                                 String.class });
+                log("[Noelios Restlet Engine] - Schema: " + sch + " class: " +
+                    className + " loader: " + loaderClass);
+                targetClass =
+                        (Class<?>)meth.invoke(null, new Object[] { cName, sch });
+            } catch (NoSuchMethodException nse) {
+                log("[Noelios Restlet Engine] - Could not instantiate a class using SCHEMA: " +
+                    sch + " and class: " + cName, nse);
+                targetClass = Engine.classForName(className);
+            } catch (IllegalAccessException iae) {
+                log("[Noelios Restlet Engine] - Could not instantiate a class using SCHEMA: " +
+                    sch + " and class: " + cName, iae);
+                targetClass = Engine.classForName(className);
+            } catch (InvocationTargetException ite) {
+                log("[Noelios Restlet Engine] - Could not instantiate a class using SCHEMA: " +
+                    sch + " and class: " + cName, ite);
+                targetClass = Engine.classForName(className);
+            }
+        } else
+            targetClass = Engine.classForName(className);
+        return targetClass;
     }
 }
