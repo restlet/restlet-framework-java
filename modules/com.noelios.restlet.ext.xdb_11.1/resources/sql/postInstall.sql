089b426d6c68ceed547025fdc844a29a874faf86
Marcelo F. Ochoa <marcelo.ochoa@gmail.com>
2008-03-05 19:45:27 +0000

    register demos to use updated tutorials source

--- modules/com.noelios.restlet.ext.xdb_11.1/resources/sql/postInstall.sql
+++ modules/com.noelios.restlet.ext.xdb_11.1/resources/sql/postInstall.sql
@@ -11,6 +11,8 @@ begin
 end;
 /
 
+rem UserService test
+rem http://localhost:8080/userapp/users/scott/orders/300
 DECLARE
   configxml SYS.XMLType;
 begin
@@ -24,14 +26,44 @@ begin
   XMLType('<init-param xmlns="http://xmlns.oracle.com/xdb/xdbconfig.xsd">
                   <param-name>org.restlet.application</param-name>
                   <param-value>$sch:org.restlet.example.tutorial.Part12</param-value>
-                  <description>REST Application</description>
+                  <description>REST User Application</description>
            </init-param>'),'xmlns="http://xmlns.oracle.com/xdb/xdbconfig.xsd"') INTO configxml
   FROM DUAL;
   -- Update the configuration to use the modified version
   --I got this error at this line :
   dbms_xdb.cfg_update(configxml);
   dbms_xdb.addServletSecRole(SERVNAME => 'UsersRestlet',ROLENAME => 'PUBLIC',ROLELINK => 'PUBLIC');
-  dbms_xdb.addServletMapping('/users/*','UsersRestlet');
+  dbms_xdb.addServletMapping('/userapp/*','UsersRestlet');
+  commit;
+end;
+/
+commit;
+
+rem redirect test
+rem http://localhost:8080/searchapp/search?kwd=myKeyword1+myKeyword2
+rem will be routed to
+rem http://www.google.com/search?q=site:mysite.org+myKeyword1%20myKeyword2
+DECLARE
+  configxml SYS.XMLType;
+begin
+  dbms_xdb.deleteServletMapping('SearchRestlet');
+  dbms_xdb.deleteServlet('SearchRestlet');
+  dbms_xdb.addServlet(name=>'SearchRestlet',language=>'Java',class=>'com.noelios.restlet.ext.xdb.XdbServerServlet',dispname=>'Restlet Servlet',schema=>'PUBLIC');
+  -- Modify the configuration
+  -- Due this servlet provide public access, it can not load 
+  -- '/home/'||USER||'/restlet/UsersRestlet.xml' from XMLDB repository
+  SELECT INSERTCHILDXML(xdburitype('/xdbconfig.xml').getXML(),'/xdbconfig/sysconfig/protocolconfig/httpconfig/webappconfig/servletconfig/servlet-list/servlet[servlet-name="SearchRestlet"]','init-param',
+  XMLType('<init-param xmlns="http://xmlns.oracle.com/xdb/xdbconfig.xsd">
+                  <param-name>org.restlet.application</param-name>
+                  <param-value>$sch:org.restlet.example.tutorial.Part10</param-value>
+                  <description>REST Search Application</description>
+           </init-param>'),'xmlns="http://xmlns.oracle.com/xdb/xdbconfig.xsd"') INTO configxml
+  FROM DUAL;
+  -- Update the configuration to use the modified version
+  --I got this error at this line :
+  dbms_xdb.cfg_update(configxml);
+  dbms_xdb.addServletSecRole(SERVNAME => 'SearchRestlet',ROLENAME => 'PUBLIC',ROLELINK => 'PUBLIC');
+  dbms_xdb.addServletMapping('/searchapp/*','SearchRestlet');
   commit;
 end;
 /
