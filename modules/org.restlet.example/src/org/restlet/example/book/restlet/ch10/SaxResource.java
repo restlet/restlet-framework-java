cbb1ada9011ab157fb5f176da361fad185473780
Thierry Boileau <thboileau@gmail.com>
2008-03-31 13:28:50 +0000

    Removed useless imports.

--- modules/org.restlet.example/src/org/restlet/example/book/restlet/ch10/SaxResource.java
+++ modules/org.restlet.example/src/org/restlet/example/book/restlet/ch10/SaxResource.java
@@ -1,8 +1,5 @@
 package org.restlet.example.book.restlet.ch10;
 
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
 import org.restlet.Context;
 import org.restlet.data.MediaType;
 import org.restlet.data.Request;
@@ -13,8 +10,6 @@ import org.restlet.resource.ResourceException;
 import org.restlet.resource.SaxRepresentation;
 import org.restlet.resource.Variant;
 import org.restlet.util.XmlWriter;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 import org.xml.sax.SAXException;
 
 public class SaxResource extends Resource {
@@ -28,21 +23,21 @@ public class SaxResource extends Resource {
     public Representation represent(Variant variant) throws ResourceException {
         Representation rep = null;
 
-            XmlWriter w = new XmlWriter();
-            w.setDataFormat(true);
-            w.setIndentStep(2);
-            try {
-                w.startDocument();
-                w.startElement("Person");
-                w.dataElement("name", "Jane Smith");
-                w.dataElement("date-of-birth", "1965-05-23");
-                w.dataElement("citizenship", "US");
-                w.endElement("Person");
-                w.endDocument();
-                rep = new SaxRepresentation(MediaType.TEXT_XML);
-            } catch (SAXException e) {
-                e.printStackTrace();
-            }
+        XmlWriter w = new XmlWriter();
+        w.setDataFormat(true);
+        w.setIndentStep(2);
+        try {
+            w.startDocument();
+            w.startElement("Person");
+            w.dataElement("name", "Jane Smith");
+            w.dataElement("date-of-birth", "1965-05-23");
+            w.dataElement("citizenship", "US");
+            w.endElement("Person");
+            w.endDocument();
+            rep = new SaxRepresentation(MediaType.TEXT_XML);
+        } catch (SAXException e) {
+            e.printStackTrace();
+        }
 
         return rep;
     }
