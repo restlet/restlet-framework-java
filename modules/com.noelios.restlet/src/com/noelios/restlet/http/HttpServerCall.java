c52dffeef8f08764a1a257b31a0beae1950950a4
Jérôme Louvel <jerome.louvel@noelios.com>
2008-04-09 12:21:53 +0000

    - Fixed NPE with internal HTTP server when no reason phrase is available for the response status. Reported by Stephan Koops.  - Updated Grizzly to 1.7.3.  - Fixed Jetty version in manifest and libraries.properties.

--- modules/com.noelios.restlet/src/com/noelios/restlet/http/HttpServerCall.java
+++ modules/com.noelios.restlet/src/com/noelios/restlet/http/HttpServerCall.java
@@ -49,79 +49,78 @@ import com.noelios.restlet.util.HeaderReader;
  * @author Jerome Louvel (contact@noelios.com)
  */
 public abstract class HttpServerCall extends HttpCall {
-    
+
     /** Cache of SSL key sizes for various cipher suites. */
     private final static ConcurrentMap<String, Integer> keySizesCache = new ConcurrentHashMap<String, Integer>();
-    
+
     /**
-     * Extract the SSL key size of a given cipher suite. 
+     * Extract the SSL key size of a given cipher suite.
      * 
-     * @param sslCipherSuite The SSL cipher suite.
+     * @param sslCipherSuite
+     *                The SSL cipher suite.
      * @return The SSL key size.
      */
     protected static Integer extractKeySize(String sslCipherSuite) {
-    	Integer keySize = keySizesCache.get(sslCipherSuite);
-    	
-    	if (keySize == null) {
-    		int encAlgorithmIndex = sslCipherSuite.indexOf("WITH_");
-    		if (encAlgorithmIndex >= 0) {
-    			String encAlgorithm = sslCipherSuite.substring(encAlgorithmIndex+5);
+        Integer keySize = keySizesCache.get(sslCipherSuite);
 
-    			/* 
-    			 * (Encryption algorithms and key sizes, quoted from RFC 2246) 
-    			 *  
-    			 *                        Key      Expanded   Effective   IV    Block
-    			 *  Cipher       Type  Material Key Material  Key Bits  Size   Size
-    			 *
-    			 *  NULL       * Stream   0          0           0        0     N/A
-    			 *  IDEA_CBC     Block   16         16         128        8      8
-    			 *  RC2_CBC_40 * Block    5         16          40        8      8
-    			 *  RC4_40     * Stream   5         16          40        0     N/A
-    			 *  RC4_128      Stream  16         16         128        0     N/A
-    			 *  DES40_CBC  * Block    5          8          40        8      8
-    			 *  DES_CBC      Block    8          8          56        8      8
-    			 *  3DES_EDE_CBC Block   24         24         168        8      8
-    			 */
-    			if (encAlgorithm != null) {
-	    			if (encAlgorithm.startsWith("NULL_")) {
-	    				keySize = Integer.valueOf(0);
-	    			} else if (encAlgorithm.startsWith("IDEA_CBC_")) {
-	    				keySize = Integer.valueOf(128);
-	    			} else if (encAlgorithm.startsWith("RC2_CBC_40_")) {
-	    				keySize = Integer.valueOf(40);
-	    			} else if (encAlgorithm.startsWith("RC4_40_")) {
-	    				keySize = Integer.valueOf(40);
-	    			} else if (encAlgorithm.startsWith("RC4_128_")) {
-	    				keySize = Integer.valueOf(128);
-	    			} else if (encAlgorithm.startsWith("DES40_CBC_")) {
-	    				keySize = Integer.valueOf(40);
-	    			} else if (encAlgorithm.startsWith("DES_CBC_")) {
-	    				keySize = Integer.valueOf(56);
-	    			} else if (encAlgorithm.startsWith("3DES_EDE_CBC_")) {
-	    				keySize = Integer.valueOf(168);
-	    			} else {
-			            StringTokenizer st = new StringTokenizer(encAlgorithm, "_");
-			            
-			            while (st.hasMoreTokens()) {
-			                try {
-			                    keySize = Integer.valueOf(st.nextToken());
-			                    break;
-			                } catch (NumberFormatException e) {
-			                    // Tokens that are not integers are ignored.
-			                }
-			            }
-	    			}
-	    			
-	    			if (keySize != null) {
-	    				keySizesCache.put(sslCipherSuite, keySize);
-	    			}
-    			}
-    		}
-    	}
-    	
-    	return keySize;
+        if (keySize == null) {
+            int encAlgorithmIndex = sslCipherSuite.indexOf("WITH_");
+            if (encAlgorithmIndex >= 0) {
+                String encAlgorithm = sslCipherSuite
+                        .substring(encAlgorithmIndex + 5);
+
+                /*
+                 * (Encryption algorithms and key sizes, quoted from RFC 2246)
+                 * 
+                 * Key Expanded Effective IV Block Cipher Type Material Key
+                 * Material Key Bits Size Size
+                 * 
+                 * NULL * Stream 0 0 0 0 N/A IDEA_CBC Block 16 16 128 8 8
+                 * RC2_CBC_40 * Block 5 16 40 8 8 RC4_40 * Stream 5 16 40 0 N/A
+                 * RC4_128 Stream 16 16 128 0 N/A DES40_CBC * Block 5 8 40 8 8
+                 * DES_CBC Block 8 8 56 8 8 3DES_EDE_CBC Block 24 24 168 8 8
+                 */
+                if (encAlgorithm != null) {
+                    if (encAlgorithm.startsWith("NULL_")) {
+                        keySize = Integer.valueOf(0);
+                    } else if (encAlgorithm.startsWith("IDEA_CBC_")) {
+                        keySize = Integer.valueOf(128);
+                    } else if (encAlgorithm.startsWith("RC2_CBC_40_")) {
+                        keySize = Integer.valueOf(40);
+                    } else if (encAlgorithm.startsWith("RC4_40_")) {
+                        keySize = Integer.valueOf(40);
+                    } else if (encAlgorithm.startsWith("RC4_128_")) {
+                        keySize = Integer.valueOf(128);
+                    } else if (encAlgorithm.startsWith("DES40_CBC_")) {
+                        keySize = Integer.valueOf(40);
+                    } else if (encAlgorithm.startsWith("DES_CBC_")) {
+                        keySize = Integer.valueOf(56);
+                    } else if (encAlgorithm.startsWith("3DES_EDE_CBC_")) {
+                        keySize = Integer.valueOf(168);
+                    } else {
+                        StringTokenizer st = new StringTokenizer(encAlgorithm,
+                                "_");
+
+                        while (st.hasMoreTokens()) {
+                            try {
+                                keySize = Integer.valueOf(st.nextToken());
+                                break;
+                            } catch (NumberFormatException e) {
+                                // Tokens that are not integers are ignored.
+                            }
+                        }
+                    }
+
+                    if (keySize != null) {
+                        keySizesCache.put(sslCipherSuite, keySize);
+                    }
+                }
+            }
+        }
+
+        return keySize;
     }
-	
+
     /**
      * Format {@code fileName} as a Content-Disposition header value
      * 
@@ -232,6 +231,7 @@ public abstract class HttpServerCall extends HttpCall {
                     contentLength);
         }
 
+        // TODO result may be null
         result.setSize(contentLength);
 
         // Extract some interesting header values
@@ -340,11 +340,11 @@ public abstract class HttpServerCall extends HttpCall {
     public Integer getSslKeySize() {
         Integer keySize = null;
         String sslCipherSuite = getSslCipherSuite();
-        
+
         if (sslCipherSuite != null) {
             keySize = extractKeySize(sslCipherSuite);
         }
-        
+
         return keySize;
     }
 
@@ -531,6 +531,10 @@ public abstract class HttpServerCall extends HttpCall {
      * 
      * @param entity
      *                The representation to write as entity of the body.
+     * @param responseEntityChannel
+     *                The response entity channel or null if a stream is used.
+     * @param responseEntityStream
+     *                The response entity stream or null if a channel is used.
      * @throws IOException
      */
     public void writeResponseBody(Representation entity,
@@ -572,7 +576,13 @@ public abstract class HttpServerCall extends HttpCall {
         headStream.write(' ');
         headStream.write(Integer.toString(getStatusCode()).getBytes());
         headStream.write(' ');
-        headStream.write(getReasonPhrase().getBytes());
+
+        if (getReasonPhrase() != null) {
+            headStream.write(getReasonPhrase().getBytes());
+        } else {
+            headStream.write(("Status " + getStatusCode()).getBytes());
+        }
+
         headStream.write(13); // CR
         headStream.write(10); // LF
 
