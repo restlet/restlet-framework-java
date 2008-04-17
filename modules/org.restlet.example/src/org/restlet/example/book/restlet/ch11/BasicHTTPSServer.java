b9b49b31df20218442979bf9404df85b7180e5a0
Thierry Boileau <thboileau@gmail.com>
2008-04-17 13:35:24 +0000

    Updated sample code.

--- modules/org.restlet.example/src/org/restlet/example/book/restlet/ch11/BasicHTTPSServer.java
+++ modules/org.restlet.example/src/org/restlet/example/book/restlet/ch11/BasicHTTPSServer.java
@@ -1,4 +1,6 @@
-package org.restlet.example.book.restlet.ch4;
+package org.restlet.example.book.restlet.ch11;
+
+import java.io.File;
 
 import org.restlet.Component;
 import org.restlet.Restlet;
@@ -7,7 +9,7 @@ import org.restlet.data.Protocol;
 import org.restlet.data.Request;
 import org.restlet.data.Response;
 
-public class SimpleHttpServerWithComponent {
+public class BasicHTTPSServer {
     public static void main(String[] args) {
         // Creates a Restlet whose response to each request is "Hello, world".
         Restlet restlet = new Restlet() {
@@ -17,11 +19,20 @@ public class SimpleHttpServerWithComponent {
             }
         };
 
-        // Component declaring only one HTTP server connector.
+        File keystoreFile = new File("d:\\temp\\certificats", "myKeystore");
+
+        // Component declaring only one HTTPS server connector.
         Component component = new Component();
-        component.getServers().add(Protocol.FILE);
-        component.getServers().add(Protocol.HTTP);
+        component.getServers().add(Protocol.HTTPS, 8182);
         component.getDefaultHost().attach("/helloWorld", restlet);
+
+        // Update component's context with keystore parameters.
+        component.getContext().getParameters().add("keystorePath",
+                keystoreFile.toURI().toASCIIString());
+        component.getContext().getParameters().add("keystorePassword",
+                "storepass");
+        component.getContext().getParameters().add("keyPassword", "keypass");
+
         try {
             component.start();
         } catch (Exception e) {
