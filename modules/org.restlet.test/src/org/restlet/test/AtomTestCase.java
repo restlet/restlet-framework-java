b06da9a7f8c69112839f3457ffd125ab9a76d739
Thierry Boileau <thboileau@gmail.com>
2008-04-17 10:06:31 +0000

    Updated support of Atom.

--- modules/org.restlet.test/src/org/restlet/test/AtomTestCase.java
+++ modules/org.restlet.test/src/org/restlet/test/AtomTestCase.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2005-2008 Noelios Consulting.
- * 
+ *
  * The contents of this file are subject to the terms of the Common Development
  * and Distribution License (the "License"). You may not use this file except in
  * compliance with the License.
- * 
+ *
  * You can obtain a copy of the license at
  * http://www.opensource.org/licenses/cddl1.txt See the License for the specific
  * language governing permissions and limitations under the License.
- * 
+ *
  * When distributing Covered Code, include this CDDL HEADER in each file and
  * include the License file at http://www.opensource.org/licenses/cddl1.txt If
  * applicable, add the following below this CDDL HEADER, with the fields
@@ -31,10 +31,10 @@ import org.restlet.resource.FileRepresentation;
 
 /**
  * Unit test case for the Atom extension.
- * 
+ *
  * @author Jerome Louvel (contact@noelios.com)
  */
-public class AtomTest extends TestCase {
+public class AtomTestCase extends TestCase {
 
     public void testAtom() {
         // Create a temporary directory for the tests
@@ -75,7 +75,7 @@ public class AtomTest extends TestCase {
 
     /**
      * Recursively delete a directory.
-     * 
+     *
      * @param dir
      *                The directory to delete.
      */
