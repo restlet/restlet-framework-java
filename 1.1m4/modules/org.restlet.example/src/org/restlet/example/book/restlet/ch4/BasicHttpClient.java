1e9037b01d23052779877ea167de4c3b54f4d414
Thierry Boileau <thboileau@gmail.com>
2008-05-19 15:10:13 +0000

    Removed file/folder

--- 1.1m4/modules/org.restlet.example/src/org/restlet/example/book/restlet/ch4/BasicHttpClient.java
+++
@@ -1,9 +0,0 @@
-package org.restlet.example.book.restlet.ch4;
-import org.restlet.Client;
-import org.restlet.data.Protocol;
-
-public class BasicHttpClient {
-    public static void main(String[] args) {
-        new Client(Protocol.HTTP).get("http://www.w3c.org");
-    }
-}
--- 1.1m4/modules/org.restlet.example/src/org/restlet/example/book/restlet/ch7/jibx/Customer.java
+++
@@ -1,9 +0,0 @@
-package org.restlet.example.book.restlet.ch7.jibx;
-
-public class Customer {
-    public String street;
-    public String city;
-    public String state;
-    public Integer zip;
-    public String phone;
-}
