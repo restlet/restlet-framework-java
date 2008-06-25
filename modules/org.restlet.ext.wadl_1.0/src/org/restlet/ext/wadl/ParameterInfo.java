0c31c13095db1d40e23e8bf4c7c59a24ad90c662
Thierry Boileau <thboileau@gmail.com>
2008-06-25 16:30:07 +0000

    Added WADL generation.

--- modules/org.restlet.ext.wadl_1.0/src/org/restlet/ext/wadl/ParameterInfo.java
+++ modules/org.restlet.ext.wadl_1.0/src/org/restlet/ext/wadl/ParameterInfo.java
@@ -33,36 +33,63 @@ import org.xml.sax.helpers.AttributesImpl;
  * @author Jerome Louvel
  */
 public class ParameterInfo {
-
+    /** Default value of this parameter. */
     private String defaultValue;
 
     /** Doc elements used to document that element. */
     private List<DocumentationInfo> documentations;
 
+    /** Provides a fixed value for the parameter. */
     private String fixed;
 
+    /** Identifier of this parameter element. */
     private String identifier;
 
+    /** Link element. */
     private LinkInfo link;
 
-    private List<OptionInfo> options;
-
-    private String path;
-
-    private boolean repeating;
-
-    private boolean required;
-
-    private ParameterStyle style;
-
+    /** Name of this element. */
     private String name;
 
+    /** List of option elements for that element. */
+    private List<OptionInfo> options;
+
+    /**
+     * Path to the value of this parameter (within a parent representation).
+     */
+    private String path;
+
+    /**
+     * Indicates whether the parameter is single valued or may have multiple
+     * values.
+     */
+    private boolean repeating;
+
+    /**
+     * Indicates whether the parameter is required.
+     */
+    private boolean required;
+
+    /** Parameter style. */
+    private ParameterStyle style;
+
+    /** Parameter type. */
     private String type;
 
+    /**
+     * Returns the default value of this parameter.
+     * 
+     * @return The default value of this parameter.
+     */
     public String getDefaultValue() {
         return defaultValue;
     }
 
+    /**
+     * Returns the list of documentation elements.
+     * 
+     * @return The list of documentation elements.
+     */
     public List<DocumentationInfo> getDocumentations() {
         // Lazy initialization with double-check.
         List<DocumentationInfo> d = this.documentations;
@@ -76,17 +103,50 @@ public class ParameterInfo {
         return d;
     }
 
+    /**
+     * Returns the fixed value for the parameter.
+     * 
+     * @return The fixed value for the parameter.
+     */
     public String getFixed() {
         return fixed;
     }
+
+    /**
+     * Returns the identifier of this parameter element.
+     * 
+     * @return The identifier of this parameter element.
+     */
 
     public String getIdentifier() {
         return identifier;
     }
 
+    /**
+     * Returns the link element.
+     * 
+     * @return The link element.
+     */
+
     public LinkInfo getLink() {
         return link;
     }
+
+    /**
+     * Returns the name of this element.
+     * 
+     * @return The name of this element.
+     */
+
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Returns the list of option elements for that element.
+     * 
+     * @return The list of option elements for that element.
+     */
 
     public List<OptionInfo> getOptions() {
         // Lazy initialization with double-check.
@@ -101,74 +161,178 @@ public class ParameterInfo {
         return o;
     }
 
+    /**
+     * Returns the path to the value of this parameter (within a parent
+     * representation).
+     * 
+     * @return The path to the value of this parameter (within a parent
+     *         representation).
+     */
+
     public String getPath() {
         return path;
     }
+
+    /**
+     * Returns the parameter style.
+     * 
+     * @return The parameter style.
+     */
 
     public ParameterStyle getStyle() {
         return style;
     }
 
-    public String getName() {
-        return name;
-    }
-
+    /**
+     * Returns the parameter type.
+     * 
+     * @return The parameter type.
+     */
     public String getType() {
         return type;
     }
+
+    /**
+     * Returns true if the parameter is single valued or may have multiple
+     * values, false otherwise.
+     * 
+     * @return True if the parameter is single valued or may have multiple
+     *         values, false otherwise.
+     */
 
     public boolean isRepeating() {
         return repeating;
     }
 
+    /**
+     * Indicates whether the parameter is required.
+     * 
+     * @return True if the parameter is required, false otherwise.
+     */
     public boolean isRequired() {
         return required;
     }
 
+    /**
+     * Sets the default value of this parameter.
+     * 
+     * @param defaultValue
+     *                The default value of this parameter.
+     */
     public void setDefaultValue(String defaultValue) {
         this.defaultValue = defaultValue;
     }
 
+    /**
+     * Sets the list of documentation elements.
+     * 
+     * @param doc
+     *                The list of documentation elements.
+     */
     public void setDocumentations(List<DocumentationInfo> doc) {
         this.documentations = doc;
     }
 
+    /**
+     * Sets the fixed value for the parameter.
+     * 
+     * @param fixed
+     *                The fixed value for the parameter.
+     */
     public void setFixed(String fixed) {
         this.fixed = fixed;
     }
 
+    /**
+     * Sets the identifier of this parameter element.
+     * 
+     * @param identifier
+     *                The identifier of this parameter element.
+     */
     public void setIdentifier(String identifier) {
         this.identifier = identifier;
     }
 
+    /**
+     * Sets the link element.
+     * 
+     * @param link
+     *                The link element.
+     */
     public void setLink(LinkInfo link) {
         this.link = link;
     }
 
-    public void setOptions(List<OptionInfo> options) {
-        this.options = options;
-    }
-
-    public void setPath(String path) {
-        this.path = path;
-    }
-
-    public void setRepeating(boolean repeating) {
-        this.repeating = repeating;
-    }
-
-    public void setRequired(boolean required) {
-        this.required = required;
-    }
-
-    public void setStyle(ParameterStyle style) {
-        this.style = style;
-    }
-
+    /**
+     * Sets the name of this element.
+     * 
+     * @param name
+     *                The name of this element.
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Sets the list of option elements for that element.
+     * 
+     * @param options
+     *                The list of option elements for that element.
+     */
+    public void setOptions(List<OptionInfo> options) {
+        this.options = options;
+    }
+
+    /**
+     * Sets the path to the value of this parameter (within a parent
+     * representation).
+     * 
+     * @param path
+     *                The path to the value of this parameter (within a parent
+     *                representation).
+     */
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    /**
+     * Indicates whether the parameter is single valued or may have multiple
+     * values.
+     * 
+     * @param repeating
+     *                True if the parameter is single valued or may have
+     *                multiple values, false otherwise.
+     */
+    public void setRepeating(boolean repeating) {
+        this.repeating = repeating;
+    }
+
+    /**
+     * Indicates whether the parameter is required.
+     * 
+     * @param required
+     *                True if the parameter is required, false otherwise.
+     */
+    public void setRequired(boolean required) {
+        this.required = required;
+    }
+
+    /**
+     * Sets the parameter style.
+     * 
+     * @param style
+     *                The parameter style.
+     */
+    public void setStyle(ParameterStyle style) {
+        this.style = style;
+    }
+
+    /**
+     * Sets the parameter type.
+     * 
+     * @param type
+     *                The parameter type.
+     */
     public void setType(String type) {
         this.type = type;
     }
