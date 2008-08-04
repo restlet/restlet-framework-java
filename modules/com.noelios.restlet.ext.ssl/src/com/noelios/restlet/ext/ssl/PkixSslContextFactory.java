a3426b92552c70fe27031357bb84e84a3d982686
Thierry Boileau <thboileau@gmail.com>
2008-08-04 20:27:56 +0000

    Updated javadocs.

--- modules/com.noelios.restlet.ext.ssl/src/com/noelios/restlet/ext/ssl/PkixSslContextFactory.java
+++ modules/com.noelios.restlet.ext.ssl/src/com/noelios/restlet/ext/ssl/PkixSslContextFactory.java
@@ -1,11 +1,11 @@
 /*
  * Copyright 2005-2008 Noelios Technologies.
  * 
- * The contents of this file are subject to the terms of the following
- * open source licenses: LGPL 3.0 or LGPL 2.1 or CDDL 1.0 (the "Licenses"). 
- * You can select the license that you prefer but you may not use this file 
- * except in compliance with one of these Licenses.
- *
+ * The contents of this file are subject to the terms of the following open
+ * source licenses: LGPL 3.0 or LGPL 2.1 or CDDL 1.0 (the "Licenses"). You can
+ * select the license that you prefer but you may not use this file except in
+ * compliance with one of these Licenses.
+ * 
  * You can obtain a copy of the LGPL 3.0 license at
  * http://www.gnu.org/licenses/lgpl-3.0.html
  * 
@@ -15,13 +15,13 @@
  * You can obtain a copy of the CDDL 1.0 license at
  * http://www.sun.com/cddl/cddl.html
  * 
- * See the Licenses for the specific language governing permissions and 
- * limitations under the Licenses. 
- *
- * Alternatively, you can obtain a royaltee free commercial license with 
- * less limitations, transferable or non-transferable, directly at
+ * See the Licenses for the specific language governing permissions and
+ * limitations under the Licenses.
+ * 
+ * Alternatively, you can obtain a royaltee free commercial license with less
+ * limitations, transferable or non-transferable, directly at
  * http://www.noelios.com/products/restlet-engine/.
- *
+ * 
  * Restlet is a registered trademark of Noelios Technologies.
  */
 
@@ -75,7 +75,8 @@ public class PkixSslContextFactory extends SslContextFactory {
 
     /**
      * Sets the following options according to parameters that may have been set
-     * up directly in the HttpsServerHelper parameters. <table>
+     * up directly in the HttpsServerHelper parameters.
+     * <table>
      * <tr>
      * <th>Parameter name</th>
      * <th>Value type</th>
@@ -165,9 +166,9 @@ public class PkixSslContextFactory extends SslContextFactory {
      * </tr>
      * </table>
      * 
-     * @param helperParameters
-     *                typically, the parameters that would have been obtained
-     *                from HttpsServerHelper.getParameters()
+     * @param parameters
+     *            Typically, the parameters that would have been obtained from
+     *            HttpsServerHelper.getParameters()
      * 
      */
     @Override
