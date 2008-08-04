3e054ca78cd1c40d871ff3f7f3fa67913fe08936
Jérôme Louvel <jerome.louvel@noelios.com>
2008-08-04 10:24:49 +0000

    - Updated copyright notices

--- modules/org.restlet.example/src/org/restlet/example/book/rest/ch2/Example2_1a.java
+++ modules/org.restlet.example/src/org/restlet/example/book/rest/ch2/Example2_1a.java
@@ -1,19 +1,28 @@
 /*
- * Copyright 2005-2007 Noelios Technologies.
+ * Copyright 2005-2008 Noelios Technologies.
  * 
- * The contents of this file are subject to the terms of the Common Development
- * and Distribution License (the "License"). You may not use this file except in
- * compliance with the License.
+ * The contents of this file are subject to the terms of the following
+ * open source licenses: LGPL 3.0 or LGPL 2.1 or CDDL 1.0 (the "Licenses"). 
+ * You can select the license that you prefer but you may not use this file 
+ * except in compliance with one of these Licenses.
+ *
+ * You can obtain a copy of the LGPL 3.0 license at
+ * http://www.gnu.org/licenses/lgpl-3.0.html
  * 
- * You can obtain a copy of the license at
- * http://www.opensource.org/licenses/cddl1.txt See the License for the specific
- * language governing permissions and limitations under the License.
+ * You can obtain a copy of the LGPL 2.1 license at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
  * 
- * When distributing Covered Code, include this CDDL HEADER in each file and
- * include the License file at http://www.opensource.org/licenses/cddl1.txt If
- * applicable, add the following below this CDDL HEADER, with the fields
- * enclosed by brackets "[]" replaced with your own identifying information:
- * Portions Copyright [yyyy] [name of copyright owner]
+ * You can obtain a copy of the CDDL 1.0 license at
+ * http://www.sun.com/cddl/cddl.html
+ * 
+ * See the Licenses for the specific language governing permissions and 
+ * limitations under the Licenses. 
+ *
+ * Alternatively, you can obtain a royaltee free commercial license with 
+ * less limitations, transferable or non-transferable, directly at
+ * http://www.noelios.com/products/restlet-engine/.
+ *
+ * Restlet is a registered trademark of Noelios Technologies.
  */
 
 package org.restlet.example.book.rest.ch2;
