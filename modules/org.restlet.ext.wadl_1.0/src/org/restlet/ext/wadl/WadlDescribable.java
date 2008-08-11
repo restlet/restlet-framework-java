e473a98ad1fc10a592d903bea62df87eadd54d24
Jérôme Louvel <jerome.louvel@noelios.com>
2008-08-11 15:11:04 +0000

    - Made WadlWrapper concrete

--- modules/org.restlet.ext.wadl_1.0/src/org/restlet/ext/wadl/WadlDescribable.java
+++ modules/org.restlet.ext.wadl_1.0/src/org/restlet/ext/wadl/WadlDescribable.java
@@ -1,10 +1,12 @@
 package org.restlet.ext.wadl;
 
+import org.restlet.Directory;
+
 /**
  * Interface that any Restlet can implement in order to provide their own WADL
- * documentation. This is especially useful for subclasses of Directory or other
- * resource finders when the WADL introspection can reach Resource or better
- * WadlResource instances.
+ * documentation. This is especially useful for subclasses of {@link Directory}
+ * or other resource finders when the WADL introspection can reach Resource or
+ * better WadlResource instances.
  * 
  * @author Thierry Boileau
  */
--- modules/org.restlet.ext.wadl_1.0/src/org/restlet/ext/wadl/WadlWrapper.java
+++ modules/org.restlet.ext.wadl_1.0/src/org/restlet/ext/wadl/WadlWrapper.java
@@ -4,23 +4,44 @@ import org.restlet.Restlet;
 import org.restlet.util.WrapperRestlet;
 
 /**
- * Restlet wrapper. Useful for application developer who need to provide the
- * WADL documentation for a Restlet instance.
+ * Restlet WADL wrapper. Useful for application developer who need to provide
+ * the WADL documentation for a Restlet instance like a Directory.
  * 
  * @author Thierry Boileau
  */
 public abstract class WadlWrapper extends WrapperRestlet implements
         WadlDescribable {
 
+    /** The description of the wrapped Restlet. */
+    private ResourceInfo resourceInfo;
+
+    /**
+     * Constructor.
+     * 
+     * @param wrappedRestlet
+     *            The Restlet to wrap.
+     */
     public WadlWrapper(Restlet wrappedRestlet) {
         super(wrappedRestlet);
     }
 
     /**
-     * Provides the data available about the wrapped Restlet.
+     * Returns the description of the wrapped Restlet.
      * 
      * @return The ResourceInfo object of the wrapped Restlet.
      */
-    public abstract ResourceInfo getResourceInfo();
+    public ResourceInfo getResourceInfo() {
+        return this.resourceInfo;
+    }
+
+    /**
+     * Sets the description of the wrapped Restlet.
+     * 
+     * @param resourceInfo
+     *            The ResourceInfo object of the wrapped Restlet.
+     */
+    public void setResourceInfo(ResourceInfo resourceInfo) {
+        this.resourceInfo = resourceInfo;
+    }
 
 }
