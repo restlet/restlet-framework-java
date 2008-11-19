0db97b6e0a3fd453428e33fd87e5ae6c6ae7c586
Jérôme Louvel <jerome.louvel@noelios.com>
2008-11-19 11:27:45 +0000

    - The engine has been moved from "com.noelios.restlet" to    the "org.restlet.engine" package, simplifying the packaging.  - All engine extensions have been moved to "org.restlet.ext"    packages.  - The com.noelios.restlet.ext.spring extension has been merged    with org.restlet.ext.spring.

---
+++ modules/org.restlet.ext.jetty_6.1/src/org/restlet/ext/jetty/AjpServerHelper.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright 2005-2008 Noelios Technologies.
+ * 
+ * The contents of this file are subject to the terms of the following open
+ * source licenses: LGPL 3.0 or LGPL 2.1 or CDDL 1.0 (the "Licenses"). You can
+ * select the license that you prefer but you may not use this file except in
+ * compliance with one of these Licenses.
+ * 
+ * You can obtain a copy of the LGPL 3.0 license at
+ * http://www.gnu.org/licenses/lgpl-3.0.html
+ * 
+ * You can obtain a copy of the LGPL 2.1 license at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ * 
+ * You can obtain a copy of the CDDL 1.0 license at
+ * http://www.sun.com/cddl/cddl.html
+ * 
+ * See the Licenses for the specific language governing permissions and
+ * limitations under the Licenses.
+ * 
+ * Alternatively, you can obtain a royaltee free commercial license with less
+ * limitations, transferable or non-transferable, directly at
+ * http://www.noelios.com/products/restlet-engine
+ * 
+ * Restlet is a registered trademark of Noelios Technologies.
+ */
+
+package org.restlet.ext.jetty;
+
+import org.mortbay.jetty.AbstractConnector;
+import org.mortbay.jetty.ajp.Ajp13SocketConnector;
+import org.restlet.Server;
+import org.restlet.data.Protocol;
+
+/**
+ * Jetty AJP server connector.
+ * 
+ * @see <a href="http://jetty.mortbay.org/jetty6/">Jetty home page</a>
+ * @author Jerome Louvel
+ */
+public class AjpServerHelper extends JettyServerHelper {
+    /**
+     * Constructor.
+     * 
+     * @param server
+     *            The server to help.
+     */
+    public AjpServerHelper(Server server) {
+        super(server);
+        getProtocols().add(Protocol.AJP);
+    }
+
+    /**
+     * Creates a new internal Jetty connector.
+     * 
+     * @return A new internal Jetty connector.
+     */
+    @Override
+    protected AbstractConnector createConnector() {
+        return new Ajp13SocketConnector();
+    }
+
+}
