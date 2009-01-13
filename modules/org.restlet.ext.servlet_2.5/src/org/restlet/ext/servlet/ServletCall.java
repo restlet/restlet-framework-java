d2331e3aaffbe0ae3f2fef7cff26661b7319ad1d
Jérôme Louvel <jerome.louvel@noelios.com>
2009-01-13 16:11:11 +0000

    - The ServerServlet now sets the HTTP headers after the status to prevent issues with Tomcat 5.0 and "Content-Length: 0" headers sending the response immediately. Reported by Eirik Bjorsnos.

--- modules/org.restlet.ext.servlet_2.5/src/org/restlet/ext/servlet/ServletCall.java
+++ modules/org.restlet.ext.servlet_2.5/src/org/restlet/ext/servlet/ServletCall.java
@@ -50,10 +50,10 @@ import org.restlet.data.Request;
 import org.restlet.data.Response;
 import org.restlet.data.Status;
 import org.restlet.engine.http.HttpCall;
+import org.restlet.engine.http.HttpConstants;
 import org.restlet.engine.http.HttpRequest;
 import org.restlet.engine.http.HttpServerCall;
 import org.restlet.util.Series;
-
 
 /**
  * Call that is used by the Servlet HTTP server connector.
@@ -363,19 +363,27 @@ public class ServletCall extends HttpServerCall {
      */
     @Override
     public void sendResponse(Response response) throws IOException {
-        // Add the response headers
-        Parameter header;
-        for (final Iterator<Parameter> iter = getResponseHeaders().iterator(); iter
-                .hasNext();) {
-            header = iter.next();
-            getResponse().addHeader(header.getName(), header.getValue());
-        }
-
         // Set the status code in the response. We do this after adding the
         // headers because when we have to rely on the 'sendError' method,
         // the Servlet containers are expected to commit their response.
         if (Status.isError(getStatusCode()) && (response.getEntity() == null)) {
             try {
+                // Add the response headers
+                Parameter header;
+                for (final Iterator<Parameter> iter = getResponseHeaders()
+                        .iterator(); iter.hasNext();) {
+                    header = iter.next();
+
+                    // We don't need to set the content length, especially
+                    // because it could send the response too early on some
+                    // containers (ex: Tomcat 5.0).
+                    if (!header.getName().equals(
+                            HttpConstants.HEADER_CONTENT_LENGTH)) {
+                        getResponse().addHeader(header.getName(),
+                                header.getValue());
+                    }
+                }
+
                 getResponse().sendError(getStatusCode(), getReasonPhrase());
             } catch (IOException ioe) {
                 getLogger().log(Level.WARNING,
@@ -384,6 +392,17 @@ public class ServletCall extends HttpServerCall {
         } else {
             // Send the response entity
             getResponse().setStatus(getStatusCode());
+
+            // Add the response headers after setting the status because
+            // otherwise some containers (ex: Tomcat 5.0) immediately send
+            // the response if a "Content-Length: 0" header is found.
+            Parameter header;
+            for (final Iterator<Parameter> iter = getResponseHeaders()
+                    .iterator(); iter.hasNext();) {
+                header = iter.next();
+                getResponse().addHeader(header.getName(), header.getValue());
+            }
+
             super.sendResponse(response);
         }
     }
