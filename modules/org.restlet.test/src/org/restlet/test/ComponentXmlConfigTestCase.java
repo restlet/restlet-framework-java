cd680a9e2505b8cec92823add4620c744767d199
Jérôme Louvel <jerome.louvel@noelios.com>
2009-01-24 16:06:08 +0000

    - The default SAX handler has been implemented to log validation information. Contributed by Raif S. Naffah.

--- modules/org.restlet.test/src/org/restlet/test/ComponentXmlConfigTestCase.java
+++ modules/org.restlet.test/src/org/restlet/test/ComponentXmlConfigTestCase.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2005-2008 Noelios Technologies.
+ * Copyright 2005-2009 Noelios Technologies.
  * 
  * The contents of this file are subject to the terms of the following open
  * source licenses: LGPL 3.0 or LGPL 2.1 or CDDL 1.0 (the "Licenses"). You can
@@ -18,9 +18,9 @@
  * See the Licenses for the specific language governing permissions and
  * limitations under the Licenses.
  * 
- * Alternatively, you can obtain a royaltee free commercial license with less
+ * Alternatively, you can obtain a royalty free commercial license with less
  * limitations, transferable or non-transferable, directly at
- * http://www.noelios.com/products/restlet-engine/.
+ * http://www.noelios.com/products/restlet-engine
  * 
  * Restlet is a registered trademark of Noelios Technologies.
  */
@@ -327,7 +327,6 @@ public class ComponentXmlConfigTestCase extends TestCase {
         assertNotNull("Default Host MUST NOT be null", dh);
         final String msg = "[" + HOST + ":" + dh.getHostPort() + "] ";
         final Context ctx = dh.getContext();
-        System.out.println("*** Context parameters = " + ctx.getParameters());
 
         checkNegativeParam(msg, ctx, CON_NAME + "1");
         checkNegativeParam(msg, ctx, CON_NAME + "2");
--- modules/org.restlet/src/org/restlet/engine/util/DefaultSaxHandler.java
+++ modules/org.restlet/src/org/restlet/engine/util/DefaultSaxHandler.java
@@ -27,20 +27,107 @@
 
 package org.restlet.engine.util;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.restlet.Context;
 import org.w3c.dom.ls.LSInput;
 import org.w3c.dom.ls.LSResourceResolver;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
 import org.xml.sax.helpers.DefaultHandler;
 
 /**
- * Default SAX handler.
+ * A Utility class which extends the provided {@link DefaultHandler} and
+ * implements the {@link LSResourceResolver} interface. All the methods of this
+ * class do nothing besides generating log messages.
  * 
+ * @author Raif S. Naffah
  * @author Jerome Louvel
  */
 public class DefaultSaxHandler extends DefaultHandler implements
         LSResourceResolver {
 
     /**
+     * A class field set to {@code true} if the the JAXP debug property is
+     * turned on; {@code false} otherwise. This is used to control the degree of
+     * output generated in the logs.
+     */
+    private static boolean debug;
+
+    static {
+        try {
+            final String debugStr = System.getProperty("jaxp.debug");
+            debug = debugStr != null && !"false".equalsIgnoreCase(debugStr);
+        } catch (SecurityException x) {
+            debug = false;
+        }
+    }
+
+    /**
+     * Set to {@code true} if the current Context's logger is capable of
+     * outputting messages at the CONFIG level; {@code false} otherwise.
+     */
+    private boolean loggable;
+
+    /** The current context JDK {@link Logger} to use for message output. */
+    private Logger logger;
+
+    /**
+     * Trivial constructor.
+     */
+    public DefaultSaxHandler() {
+        super();
+        logger = Context.getCurrentLogger();
+        loggable = logger.isLoggable(Level.CONFIG);
+    }
+
+    @Override
+    public void error(SAXParseException x) throws SAXException {
+        if (loggable) {
+            final String msg = "[ERROR] - Unexpected exception while parsing "
+                    + "an instance of PUBLIC [" + x.getPublicId()
+                    + "], SYSTEM [" + x.getSystemId() + "] - line #"
+                    + x.getLineNumber() + ", column #" + x.getColumnNumber();
+            if (debug) {
+                Context.getCurrentLogger().log(Level.CONFIG, msg, x);
+            } else {
+                logger.config(msg + ": " + x.getLocalizedMessage());
+            }
+        }
+    }
+
+    @Override
+    public void fatalError(SAXParseException x) throws SAXException {
+        if (loggable) {
+            final String msg = "[FATAL] - Unexpected exception while parsing "
+                    + "an instance of PUBLIC [" + x.getPublicId()
+                    + "], SYSTEM [" + x.getSystemId() + "] - line #"
+                    + x.getLineNumber() + ", column #" + x.getColumnNumber();
+            if (debug) {
+                Context.getCurrentLogger().log(Level.CONFIG, msg, x);
+            } else {
+                logger.config(msg + ": " + x.getLocalizedMessage());
+            }
+        }
+    }
+
+    @Override
+    public InputSource resolveEntity(String publicId, String systemId)
+            throws IOException, SAXException {
+        if (loggable) {
+            logger.config("Resolve entity with PUBLIC [" + publicId
+                    + "], and SYSTEM [" + systemId + "]");
+        }
+        return super.resolveEntity(publicId, systemId);
+    }
+
+    /**
      * Allow the application to resolve external resources.
+     * <p>
+     * This implementation <em>always</em> returns a {@code null}.
      * 
      * @param type
      *            The type of the resource being resolved.
@@ -52,11 +139,39 @@ public class DefaultSaxHandler extends DefaultHandler implements
      *            The system identifier.
      * @param baseUri
      *            The absolute base URI of the resource being parsed.
-     * 
+     * @return Always {@code null}.
      */
     public LSInput resolveResource(String type, String namespaceUri,
             String publicId, String systemId, String baseUri) {
+        if (loggable) {
+            logger.config("Resolve resource with type [" + type
+                    + "], namespace URI [" + namespaceUri + "], PUBLIC ["
+                    + publicId + "], SYSTEM [" + systemId + "], and base URI ["
+                    + baseUri + "]");
+        }
         return null;
     }
 
+    @Override
+    public void skippedEntity(String name) throws SAXException {
+        super.skippedEntity(name);
+        if (loggable) {
+            logger.config("Skipped entity named [" + name + "]");
+        }
+    }
+
+    @Override
+    public void warning(SAXParseException x) throws SAXException {
+        if (loggable) {
+            final String msg = "[WARN] - Unexpected exception while parsing "
+                    + "an instance of PUBLIC [" + x.getPublicId()
+                    + "], SYSTEM [" + x.getSystemId() + "] - line #"
+                    + x.getLineNumber() + ", column #" + x.getColumnNumber();
+            if (debug) {
+                Context.getCurrentLogger().log(Level.CONFIG, msg, x);
+            } else {
+                logger.config(msg + ": " + x.getLocalizedMessage());
+            }
+        }
+    }
 }
