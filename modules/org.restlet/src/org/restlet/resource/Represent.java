304882f8cf927d494c196eb38260a00f0fcc4dcb
Jérôme Louvel <jerome.louvel@noelios.com>
2009-02-10 22:21:24 +0000

    - Added Delete, Options, Post, Put and Represent annotations to the org.restlet.resource package. Based on community feed-back and suggestions by Tim Peierls.

---
+++ modules/org.restlet/src/org/restlet/resource/Represent.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright 2005-2008 Noelios Technologies.
+ * 
+ * The contents of this file are subject to the terms of the following open
+ * source licenses: LGPL 3.0 or LGPL 2.1 or CDDL 1.0 (the "Licenses"). You can
+ * select the license that you prefer but you may not use this file except in
+ * compliance with one of these Licenses.
+ * 
+ * You can obtain a copy of the LGPL 3.0 license at
+ * http://www.gnu.org/licenses/lgpl-3.0.html
+ * 
+ * You can obtain a copy of the LGPL 2.1 license at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ * 
+ * You can obtain a copy of the CDDL 1.0 license at
+ * http://www.sun.com/cddl/cddl.html
+ * 
+ * See the Licenses for the specific language governing permissions and
+ * limitations under the Licenses.
+ * 
+ * Alternatively, you can obtain a royaltee free commercial license with less
+ * limitations, transferable or non-transferable, directly at
+ * http://www.noelios.com/products/restlet-engine/.
+ * 
+ * Restlet is a registered trademark of Noelios Technologies.
+ */
+
+package org.restlet.resource;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotates method that represents a resource. Used for GET and HEAD handling.
+ * 
+ * @author Jerome Louvel
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Represent {
+
+    /**
+     * Representable media types specified either via extension names or full
+     * MIME type.
+     */
+    String[] value() default "*/*";
+
+}
