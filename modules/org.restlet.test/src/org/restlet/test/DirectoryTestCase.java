1e69f7e730b4d780b0181648d0b59d3b701f02f5
Thierry Boileau <thboileau@gmail.com>
2009-02-20 15:52:40 +0000

    Uncommented right instructions in DirectoryTestCase.

--- modules/org.restlet.test/src/org/restlet/test/DirectoryTestCase.java
+++ modules/org.restlet.test/src/org/restlet/test/DirectoryTestCase.java
@@ -255,8 +255,7 @@ public class DirectoryTestCase extends RestletTestCase {
         response = handle(application, this.webSiteURL, this.webSiteURL.concat(
                 "dir/subDir/").concat(testFile.getName()), Method.GET, null,
                 "deep access 2");
-        //assertEquals(Status.SUCCESS_OK, response.getStatus());
-        assertEquals(Status.CLIENT_ERROR_NOT_FOUND, response.getStatus());
+        assertEquals(Status.SUCCESS_OK, response.getStatus());
 
         directory.setDeeplyAccessible(false);
         response = handle(application, this.webSiteURL, this.webSiteURL
