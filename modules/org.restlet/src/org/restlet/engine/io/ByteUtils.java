fec829fc1596b6529abf3e3a289cf352926e96ef
Jérôme Louvel <jerome.louvel@noelios.com>
2009-03-11 17:52:37 +0000

    - Updated licensing information to mention addition of the           EPL 1.0 licensing option (Eclipse Public License 1.0).

--- modules/org.restlet/src/org/restlet/engine/io/ByteUtils.java
+++ modules/org.restlet/src/org/restlet/engine/io/ByteUtils.java
@@ -1,19 +1,22 @@
 /**
  * Copyright 2005-2009 Noelios Technologies.
  * 
- * The contents of this file are subject to the terms of the following open
- * source licenses: LGPL 3.0 or LGPL 2.1 or CDDL 1.0 (the "Licenses"). You can
- * select the license that you prefer but you may not use this file except in
- * compliance with one of these Licenses.
+ * The contents of this file are subject to the terms of one of the following
+ * open source licenses: LGPL 3.0 or LGPL 2.1 or CDDL 1.0 or CDL 1.0 (the
+ * "Licenses"). You can select the license that you prefer but you may not use
+ * this file except in compliance with one of these Licenses.
  * 
  * You can obtain a copy of the LGPL 3.0 license at
- * http://www.gnu.org/licenses/lgpl-3.0.html
+ * http://www.opensource.org/licenses/lgpl-3.0.html
  * 
  * You can obtain a copy of the LGPL 2.1 license at
- * http://www.gnu.org/licenses/lgpl-2.1.html
+ * http://www.opensource.org/licenses/lgpl-2.1.php
  * 
  * You can obtain a copy of the CDDL 1.0 license at
- * http://www.sun.com/cddl/cddl.html
+ * http://www.opensource.org/licenses/cddl1.php
+ * 
+ * You can obtain a copy of the EPL 1.0 license at
+ * http://www.opensource.org/licenses/eclipse-1.0.php
  * 
  * See the Licenses for the specific language governing permissions and
  * limitations under the Licenses.
