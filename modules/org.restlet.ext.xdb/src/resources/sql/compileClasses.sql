1dfd378c211bae12e53ff301791a803e5a83c174
Marcelo F. Ochoa <marcelo.ochoa@gmail.com>
2009-04-03 12:02:32 +0000

    update file due package refactoring

--- modules/org.restlet.ext.xdb/src/resources/sql/compileClasses.sql
+++ modules/org.restlet.ext.xdb/src/resources/sql/compileClasses.sql
@@ -1,4 +1,4 @@
-rem Compiled valid classes under RESTLET schema in batch to reduce memory usage
+rem Compiled valid classes under RESTLET schema
 declare
   res NUMBER;
 begin
@@ -6,18 +6,6 @@ begin
             FROM user_objects
             WHERE object_type = 'JAVA CLASS' AND status = 'VALID'
             AND dbms_java.longname(object_name) like 'org/restlet/%') loop
-    res := dbms_java.compile_class(c.cName);
-  end loop;
-end;
-/
-
-declare
-  res NUMBER;
-begin
-  for c in (SELECT dbms_java.longname(object_name) cName
-            FROM user_objects
-            WHERE object_type = 'JAVA CLASS' AND status = 'VALID'
-            AND dbms_java.longname(object_name) like 'com/noelios/restlet/%') loop
     res := dbms_java.compile_class(c.cName);
   end loop;
 end;
