c96fa2f0337f9a8df676ca3a22900b489bdac12d
Thierry Boileau <thboileau@gmail.com>
2009-04-03 13:03:47 +0000

    Added support of the parsing and writing of RDF documents expressed in N-Triples notation.

--- modules/org.restlet/src/org/restlet/data/MediaType.java
+++ modules/org.restlet/src/org/restlet/data/MediaType.java
@@ -39,7 +39,7 @@ import org.restlet.util.Series;
 /**
  * Media type used in representations and preferences.
  * 
- * @see <a href="http://en.wikipedia.org/wiki/MIME">MIME types on Wikipedia</a>
+ * @see <a href="http://en.wikipedia.org/wiki/MIME">MIME types on Wikipedia< /a>
  * @author Jerome Louvel
  */
 public final class MediaType extends Metadata {
