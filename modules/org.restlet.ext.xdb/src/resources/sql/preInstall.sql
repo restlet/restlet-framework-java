c00ce3a80e51cc4089b5c0f78558dbee6ee20d5d
Marcelo F. Ochoa <marcelo.ochoa@gmail.com>
2009-04-08 12:19:37 +0000

    added required grants for new functionality using JAX-RS

--- modules/org.restlet.ext.xdb/src/resources/sql/preInstall.sql
+++ modules/org.restlet.ext.xdb/src/resources/sql/preInstall.sql
@@ -24,6 +24,10 @@ begin
   -- required grants for using JDK1.4 logging
   dbms_java.grant_permission( UPPER('$sch'), 'SYS:java.lang.RuntimePermission', 'getClassLoader', '' );
   dbms_java.grant_permission( UPPER('$sch'), 'SYS:java.util.logging.LoggingPermission', 'control', '' );
+  dbms_java.grant_permission( UPPER('$sch'), 'SYS:java.lang.RuntimePermission', 'accessDeclaredMembers', '' )
+  dbms_java.grant_permission( 'ANONYMOUS', 'SYS:java.lang.RuntimePermission', 'getClassLoader', '' );
+  dbms_java.grant_permission( 'ANONYMOUS', 'SYS:java.util.logging.LoggingPermission', 'control', '' );
+  dbms_java.grant_permission( 'ANONYMOUS', 'SYS:java.lang.RuntimePermission', 'accessDeclaredMembers', '' )
   -- required for remote debugging
   -- uncomment this if you need remote debugging
   -- dbms_java.grant_permission( UPPER('$sch'), 'SYS:java.net.SocketPermission','localhost:4000', 'connect,resolve' );
