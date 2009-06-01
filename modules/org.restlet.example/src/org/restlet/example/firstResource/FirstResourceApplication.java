133ff46a6180beaac9318b08df8a88713808c42b
Jérôme Louvel <jerome.louvel@noelios.com>
2009-06-01 17:19:02 +0000

    - Removed unecessary synchronization

--- modules/org.restlet.example/src/org/restlet/example/firstResource/FirstResourceApplication.java
+++ modules/org.restlet.example/src/org/restlet/example/firstResource/FirstResourceApplication.java
@@ -16,7 +16,7 @@ public class FirstResourceApplication extends Application {
      * Creates a root Restlet that will receive all incoming calls.
      */
     @Override
-    public synchronized Restlet createRoot() {
+    public Restlet createRoot() {
         // Create a router Restlet that defines routes.
         Router router = new Router(getContext());
 
--- modules/org.restlet.example/src/org/restlet/example/firstSteps/FirstStepsApplication.java
+++ modules/org.restlet.example/src/org/restlet/example/firstSteps/FirstStepsApplication.java
@@ -10,7 +10,7 @@ public class FirstStepsApplication extends Application {
      * Creates a root Restlet that will receive all incoming calls.
      */
     @Override
-    public synchronized Restlet createRoot() {
+    public Restlet createRoot() {
         // Create a router Restlet that routes each call to a
         // new instance of HelloWorldResource.
         Router router = new Router(getContext());
