eaeb9f7bf347c30117532fa9d4c2799ca4900dc3
Jérôme Louvel <jerome.louvel@noelios.com>
2009-06-19 21:11:49 +0000

    - Added org.restlet.util.Couple generic class to store two           related objects. Inspired by Tim Peierls's Relation idea.

---
+++ modules/org.restlet/src/org/restlet/util/Couple.java
@@ -0,0 +1,122 @@
+/**
+ * Copyright 2005-2009 Noelios Technologies.
+ * 
+ * The contents of this file are subject to the terms of one of the following
+ * open source licenses: LGPL 3.0 or LGPL 2.1 or CDDL 1.0 or EPL 1.0 (the
+ * "Licenses"). You can select the license that you prefer but you may not use
+ * this file except in compliance with one of these Licenses.
+ * 
+ * You can obtain a copy of the LGPL 3.0 license at
+ * http://www.opensource.org/licenses/lgpl-3.0.html
+ * 
+ * You can obtain a copy of the LGPL 2.1 license at
+ * http://www.opensource.org/licenses/lgpl-2.1.php
+ * 
+ * You can obtain a copy of the CDDL 1.0 license at
+ * http://www.opensource.org/licenses/cddl1.php
+ * 
+ * You can obtain a copy of the EPL 1.0 license at
+ * http://www.opensource.org/licenses/eclipse-1.0.php
+ * 
+ * See the Licenses for the specific language governing permissions and
+ * limitations under the Licenses.
+ * 
+ * Alternatively, you can obtain a royalty free commercial license with less
+ * limitations, transferable or non-transferable, directly at
+ * http://www.noelios.com/products/restlet-engine
+ * 
+ * Restlet is a registered trademark of Noelios Technologies.
+ */
+
+package org.restlet.util;
+
+/**
+ * Relationship between two typed objects.
+ * 
+ * @author Jerome Louvel
+ * 
+ * @param <T>
+ * @param <U>
+ */
+public class Couple<T, U> {
+
+    /** The first object. */
+    private volatile T first;
+
+    /** The second object. */
+    private volatile U second;
+
+    /**
+     * Constructor.
+     * 
+     * @param first
+     *            The first object.
+     * @param second
+     *            The second object.
+     */
+    public Couple(T first, U second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public boolean equals(Object other) {
+        boolean result = false;
+
+        if (other instanceof Couple) {
+            Couple couple = (Couple) other;
+
+            if ((couple.getFirst() == null) && (getFirst() == null)) {
+                if ((couple.getSecond() == null) && (getSecond() == null)) {
+                    result = true;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * Returns the first object.
+     * 
+     * @return The first object.
+     */
+    public T getFirst() {
+        return first;
+    }
+
+    /**
+     * Returns the second object.
+     * 
+     * @return The second object.
+     */
+    public U getSecond() {
+        return second;
+    }
+
+    /**
+     * Sets the first object.
+     * 
+     * @param first
+     *            The first object.
+     */
+    public void setFirst(T first) {
+        this.first = first;
+    }
+
+    /**
+     * Sets the second object.
+     * 
+     * @param second
+     *            The second object.
+     */
+    public void setSecond(U second) {
+        this.second = second;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getFirst() + "," + getSecond() + "]";
+    }
+}
