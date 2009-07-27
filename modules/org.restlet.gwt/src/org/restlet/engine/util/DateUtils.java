6ab9740f92d77327277d877376ff0523b23c098f
Thierry Boileau <thboileau@gmail.com>
2009-07-27 13:52:07 +0000

    Updated source code for generation of editions.

--- modules/org.restlet.gwt/src/org/restlet/engine/util/DateUtils.java
+++ modules/org.restlet.gwt/src/org/restlet/engine/util/DateUtils.java
@@ -54,7 +54,8 @@ public final class DateUtils {
      * 
      * @author Piyush Purang (ppurang@gmail.com)
      * @see java.util.Date
-     * @see <a href="http://discuss.fogcreek.com/joelonsoftware3/default.asp?cmd=show&ixPost=73959&ixReplies=24"
+     * @see <a
+     *      href="http://discuss.fogcreek.com/joelonsoftware3/default.asp?cmd=show&ixPost=73959&ixReplies=24"
      *      >Immutable Date</a>
      */
     private static final class ImmutableDate extends Date {
