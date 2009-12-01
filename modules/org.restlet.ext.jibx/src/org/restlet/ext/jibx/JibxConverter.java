e3e14761f04b4d25adc1b73a796770f463988d03
Thierry Boileau <thboileau@gmail.com>
2009-12-01 11:50:47 +0000

    Added converter of objects using the JiBX extension.

---
+++ modules/org.restlet.ext.jibx/src/org/restlet/ext/jibx/JibxConverter.java
@@ -0,0 +1,193 @@
+/**
+ * Copyright 2005-2009 Noelios Technologies.
+ * 
+ * The contents of this file are subject to the terms of one of the following
+ * open source licenses: LGPL 3.0 or LGPL 2.1 or CDDL 1.0 or EPL 1.0 (the
+ * "Licenses"). You can select the license that you prefer but you may not use
+ * this file except in compliance with one of these Licenses.
+ * 
+ * You can obtain a copy of the LGPL 3.0 license at
+ * http://www.opensource.org/licenses/lgpl-3.0.html
+ * 
+ * You can obtain a copy of the LGPL 2.1 license at
+ * http://www.opensource.org/licenses/lgpl-2.1.php
+ * 
+ * You can obtain a copy of the CDDL 1.0 license at
+ * http://www.opensource.org/licenses/cddl1.php
+ * 
+ * You can obtain a copy of the EPL 1.0 license at
+ * http://www.opensource.org/licenses/eclipse-1.0.php
+ * 
+ * See the Licenses for the specific language governing permissions and
+ * limitations under the Licenses.
+ * 
+ * Alternatively, you can obtain a royalty free commercial license with less
+ * limitations, transferable or non-transferable, directly at
+ * http://www.noelios.com/products/restlet-engine
+ * 
+ * Restlet is a registered trademark of Noelios Technologies.
+ */
+
+package org.restlet.ext.jibx;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.jibx.runtime.BindingDirectory;
+import org.jibx.runtime.JiBXException;
+import org.restlet.Context;
+import org.restlet.data.MediaType;
+import org.restlet.engine.converter.ConverterHelper;
+import org.restlet.engine.resource.VariantInfo;
+import org.restlet.representation.Representation;
+import org.restlet.representation.Variant;
+import org.restlet.resource.UniformResource;
+
+/**
+ * A Jibx converter helper to convert from Jibx objects to JibxRepresentation
+ * and vice versa. It only supports objects that are not bound several times
+ * using several binding names.
+ * 
+ * @author Thierry Boileau
+ */
+public class JibxConverter extends ConverterHelper {
+
+    private static final VariantInfo VARIANT_APPLICATION_ALL_XML = new VariantInfo(
+            MediaType.APPLICATION_ALL_XML);
+
+    private static final VariantInfo VARIANT_APPLICATION_XML = new VariantInfo(
+            MediaType.APPLICATION_XML);
+
+    private static final VariantInfo VARIANT_TEXT_XML = new VariantInfo(
+            MediaType.TEXT_XML);
+
+    @Override
+    public List<Class<?>> getObjectClasses(Variant source) {
+        List<Class<?>> result = null;
+
+        if (VARIANT_APPLICATION_ALL_XML.isCompatible(source)
+                || VARIANT_APPLICATION_XML.isCompatible(source)
+                || VARIANT_TEXT_XML.isCompatible(source)) {
+            result = addObjectClass(result, JibxRepresentation.class);
+        }
+
+        return result;
+    }
+
+    /**
+     * Indicates if the class is bound by a Jibx factory.
+     * 
+     * @param source
+     *            The class to test.
+     * @return True if the class is bound by a Jibx factory.
+     */
+    private boolean isJibxBoundClass(Class<?> source) {
+        boolean result = false;
+
+        try {
+            if ((source != null)
+                    && (BindingDirectory.getFactory(source) != null)) {
+                result = true;
+            }
+        } catch (JiBXException e) {
+            // This may be caused by the fact that the source class is bound
+            // several times which requires the knowledge of the binding name.
+            Context.getCurrentLogger().info(
+                    "Cannot find binding class " + source + " due to: "
+                            + e.getMessage());
+        }
+
+        return result;
+    }
+
+    @Override
+    public List<VariantInfo> getVariants(Class<?> source) {
+        List<VariantInfo> result = null;
+
+        if (isJibxBoundClass(source)
+                || JibxRepresentation.class.isAssignableFrom(source)) {
+            result = addVariant(result, VARIANT_APPLICATION_ALL_XML);
+            result = addVariant(result, VARIANT_APPLICATION_XML);
+            result = addVariant(result, VARIANT_TEXT_XML);
+        }
+
+        return result;
+    }
+
+    @Override
+    public <T> float score(Representation source, Class<T> target,
+            UniformResource resource) {
+        float result = -1.0F;
+
+        if ((source != null)
+                && (isJibxBoundClass(target) || JibxRepresentation.class
+                        .isAssignableFrom(source.getClass()))) {
+            result = 1.0F;
+        } else if (JibxRepresentation.class.isAssignableFrom(target)) {
+            result = 1.0F;
+        }
+
+        return result;
+    }
+
+    @Override
+    public float score(Object source, Variant target, UniformResource resource) {
+        float result = -1.0F;
+
+        if (source != null
+                && (source instanceof JibxRepresentation<?> || isJibxBoundClass(source
+                        .getClass()))) {
+            if (target == null) {
+                // Can Happen when using ClientResource toRepresentation();
+                result = 1.0F;
+            } else if (MediaType.APPLICATION_ALL_XML.isCompatible(target
+                    .getMediaType())) {
+                result = 1.0F;
+            } else if (MediaType.APPLICATION_XML.isCompatible(target
+                    .getMediaType())) {
+                result = 1.0F;
+            } else if (MediaType.TEXT_XML.isCompatible(target.getMediaType())) {
+                result = 1.0F;
+            } else {
+                // Allow for JAXB object to be used for JSON and other
+                // representations
+                result = 0.5F;
+            }
+        }
+
+        return result;
+    }
+
+    @Override
+    public <T> T toObject(Representation source, Class<T> target,
+            UniformResource resource) throws IOException {
+        Object result = null;
+
+        if (isJibxBoundClass(target)) {
+            try {
+                result = new JibxRepresentation<T>(source, target).getObject();
+            } catch (JiBXException e) {
+                throw new IOException(
+                        "Cannot convert the given representation to  an object of this class using Jibx converter "
+                                + target + " due to " + e.getMessage());
+            }
+        }
+
+        return target.cast(result);
+    }
+
+    @Override
+    public Representation toRepresentation(Object source, Variant target,
+            UniformResource resource) {
+        Representation result = null;
+
+        if (isJibxBoundClass(source.getClass())) {
+            result = new JibxRepresentation<Object>(target.getMediaType(),
+                    source);
+        } else if (source instanceof JibxRepresentation<?>) {
+            result = (Representation) source;
+        }
+
+        return result;
+    }
+}
--- modules/org.restlet.ext.jibx/src/org/restlet/ext/jibx/JibxRepresentation.java
+++ modules/org.restlet.ext.jibx/src/org/restlet/ext/jibx/JibxRepresentation.java
@@ -61,220 +61,220 @@ import org.xml.sax.InputSource;
  */
 public class JibxRepresentation<T> extends XmlRepresentation {
 
-	/**
-	 * Improves performance by caching contexts which are expensive to create.
-	 * (All binding factory instances are guaranteed to be threadsafe and
-	 * reusable.)
-	 */
-	private final static Map<String, IBindingFactory> bindingFactories = new TreeMap<String, IBindingFactory>();
+    /**
+     * Improves performance by caching contexts which are expensive to create.
+     * (All binding factory instances are guaranteed to be threadsafe and
+     * reusable.)
+     */
+    private final static Map<String, IBindingFactory> bindingFactories = new TreeMap<String, IBindingFactory>();
 
-	/**
-	 * Get a cached binding factory.
-	 * 
-	 * @param bindingName
-	 *            The name of the binding to use.
-	 * @param bindingClass
-	 *            Target class for binding.
-	 * @return A binding factory.
-	 * @throws JiBXException
-	 */
-	@SuppressWarnings("unchecked")
-	private static synchronized IBindingFactory getBindingFactory(
-			String bindingName, Class bindingClass) throws JiBXException {
+    /**
+     * Get a cached binding factory.
+     * 
+     * @param bindingName
+     *            The name of the binding to use.
+     * @param bindingClass
+     *            Target class for binding.
+     * @return A binding factory.
+     * @throws JiBXException
+     */
+    @SuppressWarnings("unchecked")
+    private static synchronized IBindingFactory getBindingFactory(
+            String bindingName, Class bindingClass) throws JiBXException {
 
-		// All binding factory instances are guaranteed to be threadsafe and
-		// reusable.
+        // All binding factory instances are guaranteed to be threadsafe and
+        // reusable.
 
-		IBindingFactory jibxBFact = bindingFactories.get(bindingName
-				+ bindingClass.toString());
+        IBindingFactory jibxBFact = bindingFactories.get(bindingName
+                + bindingClass.toString());
 
-		if (jibxBFact == null) {
-			if (bindingName == null) {
-				jibxBFact = BindingDirectory.getFactory(bindingClass);
-			} else {
-				jibxBFact = BindingDirectory.getFactory(bindingName,
-						bindingClass);
-			}
+        if (jibxBFact == null) {
+            if (bindingName == null) {
+                jibxBFact = BindingDirectory.getFactory(bindingClass);
+            } else {
+                jibxBFact = BindingDirectory.getFactory(bindingName,
+                        bindingClass);
+            }
 
-			bindingFactories.put(bindingName + bindingClass.toString(),
-					jibxBFact);
-		}
+            bindingFactories.put(bindingName + bindingClass.toString(),
+                    jibxBFact);
+        }
 
-		return jibxBFact;
-	}
+        return jibxBFact;
+    }
 
-	/** Class for target binding. */
-	@SuppressWarnings("unchecked")
-	private volatile Class bindingClass;
+    /** Class for target binding. */
+    @SuppressWarnings("unchecked")
+    private volatile Class bindingClass;
 
-	/** The binding name to use. */
-	private volatile String bindingName;
+    /** The binding name to use. */
+    private volatile String bindingName;
 
-	/** The document encoding to use for marshalling (default is UTF-8). */
-	private volatile String encoding = "UTF-8";
+    /** The document encoding to use for marshalling (default is UTF-8). */
+    private volatile String encoding = "UTF-8";
 
-	/** The wrapped Java object. */
-	private volatile T object;
+    /** The wrapped Java object. */
+    private volatile T object;
 
-	/** The source XML representation. */
-	private volatile Representation xmlRepresentation;
+    /** The source XML representation. */
+    private volatile Representation xmlRepresentation;
 
-	/**
-	 * Creates a JIBX representation from an existing Java object. This allows a
-	 * translation from a Java object to a XML representation.
-	 * 
-	 * @param mediaType
-	 *            The representation's media type (usually
-	 *            MediaType.APPLICATION_XML or MediaType.TEXT_XML).
-	 * @param object
-	 *            The Java object.
-	 */
-	public JibxRepresentation(MediaType mediaType, T object) {
-		this(mediaType, object, null);
-	}
+    /**
+     * Creates a JIBX representation from an existing Java object. This allows a
+     * translation from a Java object to a XML representation.
+     * 
+     * @param mediaType
+     *            The representation's media type (usually
+     *            MediaType.APPLICATION_XML or MediaType.TEXT_XML).
+     * @param object
+     *            The Java object.
+     */
+    public JibxRepresentation(MediaType mediaType, T object) {
+        this(mediaType, object, null);
+    }
 
-	/**
-	 * Creates a JIBX representation from an existing Java object. This allows a
-	 * translation from a Java object to a XML representation.
-	 * 
-	 * @param mediaType
-	 *            The representation's media type (usually
-	 *            MediaType.APPLICATION_XML or MediaType.TEXT_XML).
-	 * @param object
-	 *            The Java object.
-	 * @param bindingName
-	 *            The name of the JIBX binding to use.
-	 */
-	public JibxRepresentation(MediaType mediaType, T object, String bindingName) {
-		super(mediaType);
-		this.object = object;
-		this.bindingClass = object.getClass();
-		this.bindingName = bindingName;
-	}
+    /**
+     * Creates a JIBX representation from an existing Java object. This allows a
+     * translation from a Java object to a XML representation.
+     * 
+     * @param mediaType
+     *            The representation's media type (usually
+     *            MediaType.APPLICATION_XML or MediaType.TEXT_XML).
+     * @param object
+     *            The Java object.
+     * @param bindingName
+     *            The name of the JIBX binding to use.
+     */
+    public JibxRepresentation(MediaType mediaType, T object, String bindingName) {
+        super(mediaType);
+        this.object = object;
+        this.bindingClass = object.getClass();
+        this.bindingName = bindingName;
+    }
 
-	/**
-	 * Creates a new JIBX representation, that can be used to convert the input
-	 * XML into a Java object. The XML is not validated.
-	 * 
-	 * @param xmlRepresentation
-	 *            The XML wrapped in a representation.
-	 * @param bindingClass
-	 *            The Target Java class for binding.
-	 */
-	@SuppressWarnings("unchecked")
-	public JibxRepresentation(Representation xmlRepresentation,
-			Class bindingClass) {
-		this(xmlRepresentation, bindingClass, null);
-	}
+    /**
+     * Creates a new JIBX representation, that can be used to convert the input
+     * XML into a Java object. The XML is not validated.
+     * 
+     * @param xmlRepresentation
+     *            The XML wrapped in a representation.
+     * @param bindingClass
+     *            The Target Java class for binding.
+     */
+    @SuppressWarnings("unchecked")
+    public JibxRepresentation(Representation xmlRepresentation,
+            Class bindingClass) {
+        this(xmlRepresentation, bindingClass, null);
+    }
 
-	/**
-	 * Creates a new JIBX representation, that can be used to convert the input
-	 * XML into a Java object. The XML is not validated.
-	 * 
-	 * @param xmlRepresentation
-	 *            The XML wrapped in a representation.
-	 * @param bindingClass
-	 *            The Target Java class for binding.
-	 * @param bindingName
-	 *            The name of the JIBX binding to use.
-	 */
-	@SuppressWarnings("unchecked")
-	public JibxRepresentation(Representation xmlRepresentation,
-			Class bindingClass, String bindingName) {
-		super(xmlRepresentation.getMediaType());
-		this.xmlRepresentation = xmlRepresentation;
-		this.bindingClass = bindingClass;
-		this.bindingName = bindingName;
-	}
+    /**
+     * Creates a new JIBX representation, that can be used to convert the input
+     * XML into a Java object. The XML is not validated.
+     * 
+     * @param xmlRepresentation
+     *            The XML wrapped in a representation.
+     * @param bindingClass
+     *            The Target Java class for binding.
+     * @param bindingName
+     *            The name of the JIBX binding to use.
+     */
+    @SuppressWarnings("unchecked")
+    public JibxRepresentation(Representation xmlRepresentation,
+            Class bindingClass, String bindingName) {
+        super(xmlRepresentation.getMediaType());
+        this.xmlRepresentation = xmlRepresentation;
+        this.bindingClass = bindingClass;
+        this.bindingName = bindingName;
+    }
 
-	@Override
-	public Object evaluate(String expression, QName returnType)
-			throws Exception {
-		Object result = null;
-		final XPath xpath = XPathFactory.newInstance().newXPath();
-		xpath.setNamespaceContext(this);
+    @Override
+    public Object evaluate(String expression, QName returnType)
+            throws Exception {
+        Object result = null;
+        final XPath xpath = XPathFactory.newInstance().newXPath();
+        xpath.setNamespaceContext(this);
 
-		final Document xmlDocument = getDocumentBuilder().parse(
-				new InputSource(this.xmlRepresentation.getReader()));
+        final Document xmlDocument = getDocumentBuilder().parse(
+                new InputSource(this.xmlRepresentation.getReader()));
 
-		if (xmlDocument != null) {
-			result = xpath.evaluate(expression, xmlDocument, returnType);
-		} else {
-			throw new Exception(
-					"Unable to obtain a DOM document for the XML representation. "
-							+ "XPath evaluation cancelled.");
-		}
-		return result;
-	}
+        if (xmlDocument != null) {
+            result = xpath.evaluate(expression, xmlDocument, returnType);
+        } else {
+            throw new Exception(
+                    "Unable to obtain a DOM document for the XML representation. "
+                            + "XPath evaluation cancelled.");
+        }
+        return result;
+    }
 
-	/**
-	 * Returns the document encoding to use for marshalling. The default value
-	 * is UTF-8.
-	 * 
-	 * @return The document encoding to use for marshalling.
-	 */
-	public String getEncoding() {
-		return this.encoding;
-	}
+    /**
+     * Returns the document encoding to use for marshalling. The default value
+     * is UTF-8.
+     * 
+     * @return The document encoding to use for marshalling.
+     */
+    public String getEncoding() {
+        return this.encoding;
+    }
 
-	/**
-	 * Returns the wrapped Java object.
-	 * 
-	 * @return The wrapped Java object.
-	 * @throws JiBXException
-	 *             If unmarshalling XML with JIBX fails.
-	 * @throws IOException
-	 *             If any error occurs attempting to get the stream of the
-	 *             xmlRepresentation.
-	 */
-	@SuppressWarnings("unchecked")
-	public T getObject() throws JiBXException, IOException {
-		if ((this.object == null) && (this.xmlRepresentation != null)) {
-			// Try to unmarshal the wrapped XML representation
-			final IBindingFactory jibxBFact = JibxRepresentation
-					.getBindingFactory(this.bindingName, this.bindingClass);
-			final IUnmarshallingContext uctx = jibxBFact
-					.createUnmarshallingContext();
-			return (T) uctx.unmarshalDocument(this.xmlRepresentation
-					.getStream(), null);
-		}
-		return this.object;
-	}
+    /**
+     * Returns the wrapped Java object.
+     * 
+     * @return The wrapped Java object.
+     * @throws JiBXException
+     *             If unmarshalling XML with JIBX fails.
+     * @throws IOException
+     *             If any error occurs attempting to get the stream of the
+     *             xmlRepresentation.
+     */
+    @SuppressWarnings("unchecked")
+    public T getObject() throws JiBXException, IOException {
+        if ((this.object == null) && (this.xmlRepresentation != null)) {
+            // Try to unmarshal the wrapped XML representation
+            final IBindingFactory jibxBFact = JibxRepresentation
+                    .getBindingFactory(this.bindingName, this.bindingClass);
+            final IUnmarshallingContext uctx = jibxBFact
+                    .createUnmarshallingContext();
+            return (T) uctx.unmarshalDocument(this.xmlRepresentation
+                    .getStream(), null);
+        }
+        return this.object;
+    }
 
-	public void setEncoding(String encoding) {
-		this.encoding = encoding;
-	}
+    public void setEncoding(String encoding) {
+        this.encoding = encoding;
+    }
 
-	/**
-	 * Sets the wrapped Java object.
-	 * 
-	 * @param object
-	 *            The Java object to set.
-	 */
-	public void setObject(T object) {
-		this.object = object;
-	}
+    /**
+     * Sets the wrapped Java object.
+     * 
+     * @param object
+     *            The Java object to set.
+     */
+    public void setObject(T object) {
+        this.object = object;
+    }
 
-	/**
-	 * Marshals the document and writes the representation to a byte stream.
-	 * 
-	 * @param outputStream
-	 *            The output stream.
-	 * @throws IOException
-	 *             If any error occurs attempting to write the stream.
-	 */
-	@Override
-	public void write(OutputStream outputStream) throws IOException {
-		try {
-			final IBindingFactory jibxBFact = JibxRepresentation
-					.getBindingFactory(this.bindingName, this.bindingClass);
-			final IMarshallingContext mctx = jibxBFact
-					.createMarshallingContext();
-			mctx
-					.marshalDocument(getObject(), this.encoding, null,
-							outputStream);
-		} catch (JiBXException e) {
-			throw new IOException(e.getMessage());
-		}
-	}
+    /**
+     * Marshals the document and writes the representation to a byte stream.
+     * 
+     * @param outputStream
+     *            The output stream.
+     * @throws IOException
+     *             If any error occurs attempting to write the stream.
+     */
+    @Override
+    public void write(OutputStream outputStream) throws IOException {
+        try {
+            final IBindingFactory jibxBFact = JibxRepresentation
+                    .getBindingFactory(this.bindingName, this.bindingClass);
+            final IMarshallingContext mctx = jibxBFact
+                    .createMarshallingContext();
+            mctx
+                    .marshalDocument(getObject(), this.encoding, null,
+                            outputStream);
+        } catch (JiBXException e) {
+            throw new IOException(e.getMessage());
+        }
+    }
 }
