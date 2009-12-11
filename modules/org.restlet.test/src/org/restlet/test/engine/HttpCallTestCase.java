4ed8d7f75f58fda7fc7c94b449f5c66e874d8022
Thierry Boileau <thboileau@gmail.com>
2009-12-11 11:08:50 +0000

    Fixed test case.

--- modules/org.restlet.test/src/org/restlet/test/engine/HttpCallTestCase.java
+++ modules/org.restlet.test/src/org/restlet/test/engine/HttpCallTestCase.java
@@ -73,6 +73,7 @@ public class HttpCallTestCase extends RestletTestCase {
         c.set(Calendar.YEAR, 2009);
         c.set(Calendar.MONTH, 10);
         c.set(Calendar.DAY_OF_MONTH, 11);
+        c.set(Calendar.AM_PM, Calendar.AM);
         c.set(Calendar.HOUR, 10);
         c.set(Calendar.MINUTE, 11);
         c.set(Calendar.SECOND, 12);
