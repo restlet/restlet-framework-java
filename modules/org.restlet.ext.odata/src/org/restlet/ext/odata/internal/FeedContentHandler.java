066d92ef7e6ae2e7959751c27f23ae50eea0a892
Thierry Boileau <thboileau@gmail.com>
2010-02-19 15:51:26 +0000

    Updated javadocs.

--- modules/org.restlet.ext.odata/src/org/restlet/ext/odata/internal/FeedContentHandler.java
+++ modules/org.restlet.ext.odata/src/org/restlet/ext/odata/internal/FeedContentHandler.java
@@ -1,3 +1,33 @@
+/**
+ * Copyright 2005-2010 Noelios Technologies.
+ * 
+ * The contents of this file are subject to the terms of one of the following
+ * open source licenses: LGPL 3.0 or LGPL 2.1 or CDDL 1.0 or EPL 1.0 (the
+ * "Licenses"). You can select the license that you prefer but you may not use
+ * this file except in compliance with one of these Licenses.
+ * 
+ * You can obtain a copy of the LGPL 3.0 license at
+ * http://www.opensource.org/licenses/lgpl-3.0.html
+ * 
+ * You can obtain a copy of the LGPL 2.1 license at
+ * http://www.opensource.org/licenses/lgpl-2.1.php
+ * 
+ * You can obtain a copy of the CDDL 1.0 license at
+ * http://www.opensource.org/licenses/cddl1.php
+ * 
+ * You can obtain a copy of the EPL 1.0 license at
+ * http://www.opensource.org/licenses/eclipse-1.0.php
+ * 
+ * See the Licenses for the specific language governing permissions and
+ * limitations under the Licenses.
+ * 
+ * Alternatively, you can obtain a royalty free commercial license with less
+ * limitations, transferable or non-transferable, directly at
+ * http://www.noelios.com/products/restlet-engine
+ * 
+ * Restlet is a registered trademark of Noelios Technologies.
+ */
+
 package org.restlet.ext.odata.internal;
 
 import org.restlet.ext.odata.Service;
@@ -5,14 +35,27 @@ import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
+/**
+ * Content handler for Atom Feed that takes care of odata specific needs, such
+ * as parsing XML content from other namespaces than Atom.
+ * 
+ * @author Thierry Boileau
+ * 
+ */
 public class FeedContentHandler extends DefaultHandler {
 
+    /** True if we are parsing the count tag. */
     private boolean bCount = false;
 
+    /** The value retrieved from the "count" tag. */
     private int count = -1;
 
+    /** Gleans text content. */
     StringBuilder sb = null;
 
+    /**
+     * Constructor.
+     */
     public FeedContentHandler() {
         super();
     }
@@ -34,6 +77,17 @@ public class FeedContentHandler extends DefaultHandler {
         }
     }
 
+    /**
+     * Returns the value of the "count" tag, that is to say the size of the
+     * current entity set.
+     * 
+     * @return The size of the current entity set, as specified by the Atom
+     *         document.
+     */
+    public int getCount() {
+        return count;
+    }
+
     @Override
     public void startElement(String uri, String localName, String qName,
             Attributes attributes) throws SAXException {
@@ -42,10 +96,6 @@ public class FeedContentHandler extends DefaultHandler {
             sb = new StringBuilder();
             bCount = true;
         }
-    }
-
-    public int getCount() {
-        return count;
     }
 
 }
