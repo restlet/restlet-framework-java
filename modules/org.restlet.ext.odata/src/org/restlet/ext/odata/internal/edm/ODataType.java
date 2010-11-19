c7821a005a338dc7cf7c312aaf778b3dcede66a0
Thierry Boileau <thboileau@gmail.com>
2010-11-19 10:58:54 +0000

    Fixed parsing of complex types. Reported by Alex Bass.

--- modules/org.restlet.ext.odata/src/org/restlet/ext/odata/internal/edm/ODataType.java
+++ modules/org.restlet.ext.odata/src/org/restlet/ext/odata/internal/edm/ODataType.java
@@ -42,212 +42,212 @@ import java.util.TreeSet;
  */
 public class ODataType extends NamedObject implements Comparable<ODataType> {
 
-    /** Is this type abstract? */
-    private boolean abstractType;
+	/** Is this type abstract? */
+	private boolean abstractType;
 
-    /** The parent type this type inherits from. */
-    private ODataType baseType;
+	/** The parent type this type inherits from. */
+	private ODataType baseType;
 
-    /** The list of complex properties. */
-    private List<ComplexProperty> complexProperties;
+	/** The list of complex properties. */
+	private List<ComplexProperty> complexProperties;
 
-    /** The list of properties. */
-    private List<Property> properties;
+	/** The list of properties. */
+	private List<Property> properties;
 
-    /** The schema. */
-    private Schema schema;
+	/** The schema. */
+	private Schema schema;
 
-    /**
-     * Constructor.
-     * 
-     * @param name
-     *            The name of this entity type.
-     */
-    public ODataType(String name) {
-        super(name);
-    }
+	/**
+	 * Constructor.
+	 * 
+	 * @param name
+	 *            The name of this entity type.
+	 */
+	public ODataType(String name) {
+		super(name);
+	}
 
-    /**
-     * Compares this object with the specified object for order. The comparison
-     * is based on the computed full class name
-     */
-    public int compareTo(ODataType o) {
-        if (o == null) {
-            return 1;
-        }
-        int result = 0;
+	/**
+	 * Compares this object with the specified object for order. The comparison
+	 * is based on the computed full class name
+	 */
+	public int compareTo(ODataType o) {
+		if (o == null) {
+			return 1;
+		}
+		int result = 0;
 
-        String s1 = getFullClassName();
-        String s2 = o.getFullClassName();
-        if (s1 != null) {
-            result = s1.compareTo(s2);
-        } else if (s2 != null) {
-            result = -1 * s2.compareTo(s1);
-        }
-        return result;
-    }
+		String s1 = getFullClassName();
+		String s2 = o.getFullClassName();
+		if (s1 != null) {
+			result = s1.compareTo(s2);
+		} else if (s2 != null) {
+			result = -1 * s2.compareTo(s1);
+		}
+		return result;
+	}
 
-    /**
-     * Returns the parent type this type inherits from.
-     * 
-     * @return The parent type this type inherits from.
-     */
-    public ODataType getBaseType() {
-        return baseType;
-    }
+	/**
+	 * Returns the parent type this type inherits from.
+	 * 
+	 * @return The parent type this type inherits from.
+	 */
+	public ODataType getBaseType() {
+		return baseType;
+	}
 
-    /**
-     * Returns the Java class name related to this entity type.
-     * 
-     * @return The Java class name related to this entity type.
-     */
-    public String getClassName() {
-        return getNormalizedName().substring(0, 1).toUpperCase()
-                + getNormalizedName().substring(1);
-    }
+	/**
+	 * Returns the Java class name related to this entity type.
+	 * 
+	 * @return The Java class name related to this entity type.
+	 */
+	public String getClassName() {
+		return getNormalizedName().substring(0, 1).toUpperCase()
+				+ getNormalizedName().substring(1);
+	}
 
-    /**
-     * Returns the list of complex properties.
-     * 
-     * @return The list of complex properties.
-     */
-    public List<ComplexProperty> getComplexProperties() {
-        if (complexProperties == null) {
-            complexProperties = new ArrayList<ComplexProperty>();
-        }
-        return complexProperties;
-    }
+	/**
+	 * Returns the list of complex properties.
+	 * 
+	 * @return The list of complex properties.
+	 */
+	public List<ComplexProperty> getComplexProperties() {
+		if (complexProperties == null) {
+			complexProperties = new ArrayList<ComplexProperty>();
+		}
+		return complexProperties;
+	}
 
-    /**
-     * Returns the package name related to this entity type.
-     * 
-     * @return The package name related to this entity type.
-     */
-    public String getFullClassName() {
-        return getPackageName() + "." + getClassName();
-    }
+	/**
+	 * Returns the package name related to this entity type.
+	 * 
+	 * @return The package name related to this entity type.
+	 */
+	public String getFullClassName() {
+		return getPackageName() + "." + getClassName();
+	}
 
-    /**
-     * Returns the set of imported Java classes.
-     * 
-     * @return The set of imported Java classes.
-     */
-    public Set<String> getImportedJavaClasses() {
-        Set<String> result = new TreeSet<String>();
+	/**
+	 * Returns the set of imported Java classes.
+	 * 
+	 * @return The set of imported Java classes.
+	 */
+	public Set<String> getImportedJavaClasses() {
+		Set<String> result = new TreeSet<String>();
 
-        for (Property property : getProperties()) {
-            if (property.getType() != null) {
-                result.addAll(property.getType().getImportedJavaClasses());
-            }
-        }
+		for (Property property : getProperties()) {
+			if (property.getType() != null) {
+				result.addAll(property.getType().getImportedJavaClasses());
+			}
+		}
 
-        for (ComplexProperty property : getComplexProperties()) {
-            if (property.getComplexType() != null) {
-                if (!property.getComplexType().getSchema().equals(getSchema())) {
-                    result.add(property.getComplexType().getFullClassName());
-                }
-            }
-        }
+		for (ComplexProperty property : getComplexProperties()) {
+			if (property.getComplexType() != null) {
+				if (!property.getComplexType().getSchema().equals(getSchema())) {
+					result.add(property.getComplexType().getFullClassName());
+				}
+			}
+		}
 
-        return result;
-    }
+		return result;
+	}
 
-    /**
-     * Returns the set of imported entity types. By default, returns an empty
-     * set.
-     * 
-     * @return The set of imported entity types.
-     */
-    public Set<ODataType> getImportedTypes() {
-        return new TreeSet<ODataType>();
-    }
+	/**
+	 * Returns the set of imported entity types. By default, returns an empty
+	 * set.
+	 * 
+	 * @return The set of imported entity types.
+	 */
+	public Set<ODataType> getImportedTypes() {
+		return new TreeSet<ODataType>();
+	}
 
-    /**
-     * Returns the package name related to this entity type.
-     * 
-     * @return The package name related to this entity type.
-     */
-    public String getPackageName() {
-        return TypeUtils.getPackageName(getSchema());
-    }
+	/**
+	 * Returns the package name related to this entity type.
+	 * 
+	 * @return The package name related to this entity type.
+	 */
+	public String getPackageName() {
+		return TypeUtils.getPackageName(getSchema());
+	}
 
-    /**
-     * Returns the list of properties.
-     * 
-     * @return The list of properties.
-     */
-    public List<Property> getProperties() {
-        if (properties == null) {
-            properties = new ArrayList<Property>();
-        }
-        return properties;
-    }
+	/**
+	 * Returns the list of properties.
+	 * 
+	 * @return The list of properties.
+	 */
+	public List<Property> getProperties() {
+		if (properties == null) {
+			properties = new ArrayList<Property>();
+		}
+		return properties;
+	}
 
-    /**
-     * Returns the schema.
-     * 
-     * @return The schema.
-     */
-    public Schema getSchema() {
-        return schema;
-    }
+	/**
+	 * Returns the schema.
+	 * 
+	 * @return The schema.
+	 */
+	public Schema getSchema() {
+		return schema;
+	}
 
-    /**
-     * Returns true if this type is abstract.
-     * 
-     * @return True if this type is abstract
-     */
-    public boolean isAbstractType() {
-        return abstractType;
-    }
+	/**
+	 * Returns true if this type is abstract.
+	 * 
+	 * @return True if this type is abstract
+	 */
+	public boolean isAbstractType() {
+		return abstractType;
+	}
 
-    /**
-     * Indicates if this type is abstract
-     * 
-     * @param abstractType
-     *            True if this type is abstract
-     */
-    public void setAbstractType(boolean abstractType) {
-        this.abstractType = abstractType;
-    }
+	/**
+	 * Indicates if this type is abstract
+	 * 
+	 * @param abstractType
+	 *            True if this type is abstract
+	 */
+	public void setAbstractType(boolean abstractType) {
+		this.abstractType = abstractType;
+	}
 
-    /**
-     * Sets the parent type this type inherits from.
-     * 
-     * @param baseType
-     *            The parent type this type inherits from.
-     */
-    public void setBaseType(ODataType baseType) {
-        this.baseType = baseType;
-    }
+	/**
+	 * Sets the parent type this type inherits from.
+	 * 
+	 * @param baseType
+	 *            The parent type this type inherits from.
+	 */
+	public void setBaseType(ODataType baseType) {
+		this.baseType = baseType;
+	}
 
-    /**
-     * Sets the list of complex properties.
-     * 
-     * @param complexProperties
-     *            The list of complex properties.
-     */
-    public void setComplexProperties(List<ComplexProperty> complexProperties) {
-        this.complexProperties = complexProperties;
-    }
+	/**
+	 * Sets the list of complex properties.
+	 * 
+	 * @param complexProperties
+	 *            The list of complex properties.
+	 */
+	public void setComplexProperties(List<ComplexProperty> complexProperties) {
+		this.complexProperties = complexProperties;
+	}
 
-    /**
-     * Sets the list of properties.
-     * 
-     * @param properties
-     *            The list of properties.
-     */
-    public void setProperties(List<Property> properties) {
-        this.properties = properties;
-    }
+	/**
+	 * Sets the list of properties.
+	 * 
+	 * @param properties
+	 *            The list of properties.
+	 */
+	public void setProperties(List<Property> properties) {
+		this.properties = properties;
+	}
 
-    /**
-     * Sets the schema.
-     * 
-     * @param schema
-     *            The schema.
-     */
-    public void setSchema(Schema schema) {
-        this.schema = schema;
-    }
+	/**
+	 * Sets the schema.
+	 * 
+	 * @param schema
+	 *            The schema.
+	 */
+	public void setSchema(Schema schema) {
+		this.schema = schema;
+	}
 }
