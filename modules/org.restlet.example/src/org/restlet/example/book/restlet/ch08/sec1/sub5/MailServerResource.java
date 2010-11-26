86702405a9293582ef1abed682d30d0301c51a6e
Jérôme Louvel <jerome.louvel@noelios.com>
2010-11-26 17:23:31 +0000

    - Continued chapter 8 examples

---
+++ modules/org.restlet.example/src/org/restlet/example/book/restlet/ch08/sec1/sub5/MailServerResource.java
@@ -0,0 +1,68 @@
+package org.restlet.example.book.restlet.ch08.sec1.sub5;
+
+import java.util.List;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.restlet.data.LocalReference;
+import org.restlet.data.MediaType;
+import org.restlet.data.Reference;
+import org.restlet.ext.fileupload.RestletFileUpload;
+import org.restlet.ext.freemarker.TemplateRepresentation;
+import org.restlet.representation.InputRepresentation;
+import org.restlet.representation.Representation;
+import org.restlet.representation.StringRepresentation;
+import org.restlet.resource.ClientResource;
+import org.restlet.resource.ResourceException;
+import org.restlet.resource.ServerResource;
+
+/**
+ * Resource corresponding to a mail received or sent with the parent mail
+ * account. Leverages FreeMarker template engine.
+ */
+public class MailServerResource extends ServerResource {
+
+    @Override
+    protected Representation get() throws ResourceException {
+        // Create the mail bean
+        Mail mail = new Mail();
+        mail.setStatus("received");
+        mail.setSubject("Message to self");
+        mail.setContent("Doh!");
+        mail.setAccountRef(new Reference(getReference(), "..").getTargetRef()
+                .toString());
+
+        // Load the FreeMarker template
+        Representation mailFtl = new ClientResource(
+                LocalReference.createClapReference(getClass().getPackage())
+                        + "/Mail.ftl").get();
+
+        // Wraps the bean with a FreeMarker representation
+        return new TemplateRepresentation(mailFtl, mail, MediaType.TEXT_HTML);
+    }
+
+    @Override
+    protected Representation put(Representation input) {
+        try {
+            // Create a factory for disk-based file items
+            RestletFileUpload fileUpload = new RestletFileUpload(
+                    new DiskFileItemFactory());
+            List<FileItem> fileItems = fileUpload.parseRepresentation(input);
+
+            for (FileItem fileItem : fileItems) {
+                if (fileItem.isFormField()) {
+                    System.out.println(fileItem.getFieldName() + "="
+                            + fileItem.getString());
+                } else {
+                    Representation attachment = new InputRepresentation(
+                            fileItem.getInputStream());
+                    attachment.write(System.out);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return new StringRepresentation("Mail updated!");
+    }
+}
