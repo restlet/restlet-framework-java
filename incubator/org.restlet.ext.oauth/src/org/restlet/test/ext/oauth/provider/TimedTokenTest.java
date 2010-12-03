87b7e46c07ae631a99de2dc5a86622fa316f1e4d
Jérôme Louvel <jerome.louvel@noelios.com>
2010-12-03 17:11:28 +0000

    - Added OAuth 2.0 extension and OpenID 2.0 extension to the incubator. Contributed by Ericsson Research

---
+++ incubator/org.restlet.ext.oauth/src/org/restlet/test/ext/oauth/provider/TimedTokenTest.java
@@ -0,0 +1,194 @@
+package org.restlet.test.ext.oauth.provider;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.restlet.Component;
+import org.restlet.Context;
+import org.restlet.Server;
+import org.restlet.data.MediaType;
+import org.restlet.data.Parameter;
+import org.restlet.data.Protocol;
+import org.restlet.data.Reference;
+import org.restlet.data.Status;
+import org.restlet.engine.Engine;
+import org.restlet.engine.security.AuthenticatorHelper;
+import org.restlet.ext.oauth.provider.OAuthUser;
+import org.restlet.ext.oauth.util.OAuthUtils;
+import org.restlet.ext.oauth.util.OAuthAuthenticationHelper;
+import org.restlet.representation.Representation;
+import org.restlet.resource.ClientResource;
+import org.restlet.resource.ResourceException;
+import org.restlet.test.ext.oauth.test.resources.OauthClientTestApplication;
+import org.restlet.test.ext.oauth.test.resources.OauthProtectedTestApplication;
+import org.restlet.test.ext.oauth.test.resources.OauthTestApplication;
+import org.restlet.util.Series;
+
+public class TimedTokenTest {
+    public static Component component;
+
+    // Use for http test when debugging
+    public static int serverPort = 8080;
+
+    public static final String prot = "http";
+
+    // public static int serverPort = 8443;
+    // public static final String prot = "https";
+
+    public static OauthClientTestApplication client = new OauthClientTestApplication();
+
+    @BeforeClass
+    public static void startServer() throws Exception {
+        Logger log = Context.getCurrentLogger();
+        log.info("Starting timed server test!");
+
+        // SSL global configuration
+        String keystore = ClassLoader.getSystemResource("localhost.jks")
+                .getPath();
+        System.setProperty("javax.net.ssl.trustStore", keystore);
+        System.setProperty("javax.net.ssl.trustStoreType", "JKS");
+        System.setProperty("javax.net.ssl.trustStorePassword", "testpass");
+
+        // Server server = new Server(Protocol.HTTPS, serverPort);
+        Server server = new Server(Protocol.HTTP, serverPort);
+        component = new Component();
+        component.getServers().add(server);
+        component.getClients().add(Protocol.HTTP);
+        component.getClients().add(Protocol.HTTPS);
+        // component.getClients().add(Protocol.CLAP);
+        component.getClients().add(Protocol.RIAP);
+        component.getDefaultHost().attach("/oauth",
+                new OauthTestApplication(20)); // limited token life
+        component.getDefaultHost().attach("/client", client);
+        component.getDefaultHost().attach("/server",
+                new OauthProtectedTestApplication());
+        // server.getContext().getParameters().add("maxThreads", "30");
+
+        // Setup TLS
+        Series<Parameter> parameters = server.getContext().getParameters();
+        parameters.add("keystorePath", keystore);
+        parameters.add("keystorePassword", "testpass");
+        parameters.add("keyPassword", "testpass");
+        parameters.add("keystoreType", "JKS");
+        parameters.add("sslServerAlias", "localhost");
+
+        component.start();
+
+        List<AuthenticatorHelper> authenticators = Engine.getInstance()
+                .getRegisteredAuthenticators();
+        for (AuthenticatorHelper helper : authenticators) {
+            System.out.println("Found default auth helper : " + helper);
+        }
+        authenticators.add(new OAuthAuthenticationHelper());
+
+        System.out.println(Engine.getInstance().getRegisteredClients().get(0));
+    }
+
+    @AfterClass
+    public static void stopServer() throws Exception {
+        component.stop();
+    }
+
+    @Test
+    public void testTimedTokens() throws Exception {
+
+        assertNull(client.getToken());
+        ClientResource cr = new ClientResource(prot + "://localhost:"
+                + serverPort + "/client/webclient");
+        Representation r = cr.get();
+        assertNotNull(r);
+        String text = r.getText();
+        assertEquals("Response text test", text, "TestSuccessful");
+        assertEquals("Response content type test", r.getMediaType(),
+                MediaType.TEXT_HTML);
+        assertNotNull(client.getToken());
+        cr.release();
+        // Query test
+        assertNotNull(client.getToken());
+        Reference ref = new Reference(prot + "://localhost:" + serverPort
+                + "/server/protected");
+        ref.addQueryParameter("oauth_token", client.getToken());
+        cr = new ClientResource(ref);
+        r = cr.get();
+        assertNotNull(r);
+        text = r.getText();
+        assertEquals("Response text test", text, "TestSuccessful");
+        assertEquals("Response content type test", r.getMediaType(),
+                MediaType.TEXT_HTML);
+        cr.release();
+        Thread.sleep(20000);
+        // Another query test...
+        assertNotNull(client.getToken());
+        ref.addQueryParameter("oauth_token", client.getToken());
+        cr = new ClientResource(ref);
+        try {
+            r = cr.get();
+        } catch (ResourceException re) {
+            assertEquals(Status.CLIENT_ERROR_FORBIDDEN, re.getStatus());
+        }
+        cr.release();
+        assertNotNull(r);
+    }
+
+    @Test
+    public void testRefresh() throws Exception {
+        OAuthUser user = client.getUser();
+        assertNotNull(user);
+        OAuthUser refreshed = OAuthUtils
+                .refreshToken(client.getOauthParameters(), client.getUser()
+                        .getRefreshToken());
+        assertNotNull(user);
+        String wrongToken = refreshed.getAccessToken();
+        assertNotNull(wrongToken);
+
+        // Back to back test
+        refreshed = OAuthUtils.refreshToken(client.getOauthParameters(),
+                client.getUser().getRefreshToken());
+        String newToken = refreshed.getAccessToken();
+        assertNotNull(newToken);
+
+        // Query test
+        Reference ref = new Reference(prot + "://localhost:" + serverPort
+                + "/server/protected");
+        ref.addQueryParameter("oauth_token", newToken);
+        ClientResource cr = new ClientResource(ref);
+        Representation r = cr.get();
+        assertNotNull(r);
+        String text = r.getText();
+        assertEquals("Response text test", text, "TestSuccessful");
+        assertEquals("Response content type test", r.getMediaType(),
+                MediaType.TEXT_HTML);
+        cr.release();
+        // Check the token we got before
+        ref = new Reference(prot + "://localhost:" + serverPort
+                + "/server/protected");
+        ref.addQueryParameter("oauth_token", wrongToken);
+        cr = new ClientResource(ref);
+        try {
+            r = cr.get();
+        } catch (ResourceException re) { // Should be invalidated
+            assertEquals(Status.CLIENT_ERROR_FORBIDDEN, re.getStatus());
+        }
+        cr.release();
+        Thread.sleep(20000);
+        // Another query test...
+        assertNotNull(client.getToken());
+        ref.addQueryParameter("oauth_token", client.getToken());
+        cr = new ClientResource(ref);
+        try {
+            r = cr.get();
+        } catch (ResourceException re) {
+            assertEquals(Status.CLIENT_ERROR_FORBIDDEN, re.getStatus());
+        }
+        cr.release();
+        assertNotNull(r);
+    }
+
+}
---
+++ incubator/org.restlet.ext.openid/src/org/restlet/ext/openid/OpenIdProvider.java
@@ -0,0 +1,205 @@
+/**
+ * Copyright 2005-2010 Noelios Technologies.
+ * 
+ * The contents of this file are subject to the terms of one of the following
+ * open source licenses: LGPL 3.0 or LGPL 2.1 or CDDL 1.0 or EPL 1.0 (the
+ * "Licenses"). You can select the license that you prefer but you may not use
+ * this file except in compliance with one of these Licenses.
+ * 
+ * You can obtain a copy of the LGPL 3.0 license at
+ * http://www.opensource.org/licenses/lgpl-3.0.html
+ * 
+ * You can obtain a copy of the LGPL 2.1 license at
+ * http://www.opensource.org/licenses/lgpl-2.1.php
+ * 
+ * You can obtain a copy of the CDDL 1.0 license at
+ * http://www.opensource.org/licenses/cddl1.php
+ * 
+ * You can obtain a copy of the EPL 1.0 license at
+ * http://www.opensource.org/licenses/eclipse-1.0.php
+ * 
+ * See the Licenses for the specific language governing permissions and
+ * limitations under the Licenses.
+ * 
+ * Alternatively, you can obtain a royalty free commercial license with less
+ * limitations, transferable or non-transferable, directly at
+ * http://www.noelios.com/products/restlet-engine
+ * 
+ * Restlet is a registered trademark of Noelios Technologies.
+ */
+
+package org.restlet.ext.openid;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentMap;
+import java.util.logging.Logger;
+
+import org.openid4java.message.DirectError;
+import org.openid4java.message.Message;
+import org.openid4java.message.ParameterList;
+import org.openid4java.server.ServerManager;
+import org.restlet.data.Form;
+import org.restlet.data.Method;
+import org.restlet.representation.EmptyRepresentation;
+import org.restlet.representation.Representation;
+import org.restlet.representation.StringRepresentation;
+import org.restlet.representation.Variant;
+import org.restlet.resource.Get;
+import org.restlet.resource.Post;
+import org.restlet.resource.ResourceException;
+import org.restlet.resource.ServerResource;
+
+/**
+ * OpenID provider representation implementing an open identity IdP. At the
+ * moment it is only used for testing.
+ * 
+ * User backend is not specified and if is only returning one user 'foo'
+ * 
+ * @author Kristoffer Gronowski
+ */
+public class OpenIdProvider extends ServerResource {
+
+    public OpenIdProvider() {
+        System.out.println("OpenID CREATED NEW PROVIDER");
+    }
+
+    @Override
+    protected Representation head() throws ResourceException {
+        getLogger().info("IN head()");
+
+        sendXRDSLocation();
+
+        return new EmptyRepresentation();
+    }
+
+    @Override
+    protected Representation head(Variant variant) throws ResourceException {
+        getLogger().info("IN head(variant) : " + variant);
+        return super.head(variant);
+    }
+
+    @Post("form")
+    public Representation represent(Representation input) {
+        Form f = new Form(input);
+        return handle(new ParameterList(f.getValuesMap()));
+    }
+
+    @Get("form")
+    public Representation represent() {
+
+        return handle(new ParameterList(getQuery().getValuesMap()));
+    }
+
+    private Representation handle(ParameterList request) {
+        Logger log = getLogger();
+        log.info("Handle on OP");
+        ConcurrentMap<String, Object> attribs = getContext().getAttributes();
+        ServerManager manager = (ServerManager) attribs.get("openid_manager");
+        log.info("OP endpoint = " + manager.getOPEndpointUrl());
+
+        String mode = request.hasParameter("openid.mode") ? request
+                .getParameterValue("openid.mode") : null;
+
+        Message response;
+        String responseText;
+
+        if ("associate".equals(mode)) {
+            // --- process an association request ---
+            response = manager.associationResponse(request);
+            responseText = response.keyValueFormEncoding();
+        } else if ("checkid_setup".equals(mode)
+                || "checkid_immediate".equals(mode)) {
+            // interact with the user and obtain data needed to continue
+            List<?> userData = userInteraction(request,
+                    manager.getOPEndpointUrl());
+
+            String userSelectedId = (String) userData.get(0);
+            String userSelectedClaimedId = (String) userData.get(1);
+            Boolean authenticatedAndApproved = (Boolean) userData.get(2);
+
+            // --- process an authentication request ---
+            response = manager.authResponse(request, userSelectedId,
+                    userSelectedClaimedId,
+                    authenticatedAndApproved.booleanValue());
+
+            if (response instanceof DirectError) {
+                Form f = new Form();
+                @SuppressWarnings("unchecked")
+                Map<String, String> m = (Map<String, String>) response
+                        .getParameterMap();
+                for (String key : m.keySet()) {
+                    f.add(key, m.get(key));
+                }
+                return f.getWebRepresentation();
+            } else {
+                // caller will need to decide which of the following to use:
+
+                // option1: GET HTTP-redirect to the return_to URL
+                // return new
+                // StringRepresentation(response.getDestinationUrl(true));
+                redirectTemporary(response.getDestinationUrl(true));
+                return new EmptyRepresentation();
+
+                // option2: HTML FORM Redirection
+                // RequestDispatcher dispatcher =
+                // getServletContext().getRequestDispatcher("formredirection.jsp");
+                // httpReq.setAttribute("prameterMap",
+                // response.getParameterMap());
+                // httpReq.setAttribute("destinationUrl",
+                // response.getDestinationUrl(false));
+                // dispatcher.forward(request, response);
+                // return null;
+            }
+        } else if ("check_authentication".equals(mode)) {
+            // --- processing a verification request ---
+            response = manager.verify(request);
+            log.info("OpenID : " + response.keyValueFormEncoding());
+            responseText = response.keyValueFormEncoding();
+        } else if (Method.GET.equals(getRequest().getMethod())) {
+            // Could be a discovery request
+            sendXRDSLocation();
+            return new StringRepresentation("XRDS Discovery Information");
+        } else {
+            // --- error response ---
+            response = DirectError.createDirectError("Unknown request");
+            responseText = response.keyValueFormEncoding();
+        }
+
+        // return the result to the user
+        return new StringRepresentation(responseText);
+    }
+
+    private void sendXRDSLocation() {
+        ConcurrentMap<String, Object> attribs = getContext().getAttributes();
+        String id = getQuery().getFirstValue("id");
+        String xrds = attribs.get("xrds").toString();
+        String location = (id != null) ? xrds + "?id=" + id : xrds;
+        getLogger().info("XRDS endpoint = " + xrds);
+        Form headers = (Form) getResponse().getAttributes().get(
+                "org.restlet.http.headers");
+        if (headers == null) {
+            headers = new Form();
+            headers.add("X-XRDS-Location", location);
+            getResponse().getAttributes().put("org.restlet.http.headers",
+                    headers);
+        } else {
+            headers.add("X-XRDS-Location", location);
+        }
+        getLogger().info("Sending empty representation.");
+    }
+
+    private List<Object> userInteraction(ParameterList request, String endpoint) {
+        StringBuilder id = new StringBuilder();
+        id.append(endpoint);
+        id.append("?id=");
+        id.append("foo");
+        List<Object> result = new ArrayList<Object>();
+        result.add(0, id.toString());
+        result.add(1, id.toString());
+        result.add(2, Boolean.TRUE);
+        return result;
+    }
+
+}
