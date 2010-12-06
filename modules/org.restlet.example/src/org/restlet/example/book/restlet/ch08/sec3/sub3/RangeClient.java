a9bd88ecd09d325247281e46bf6db5f72a07fffb
Jérôme Louvel <jerome.louvel@noelios.com>
2010-12-06 09:24:41 +0000

    - Updated book examples

--- modules/org.restlet.example/src/org/restlet/example/book/restlet/ch08/sec3/sub3/RangeClient.java
+++ modules/org.restlet.example/src/org/restlet/example/book/restlet/ch08/sec3/sub3/RangeClient.java
@@ -5,10 +5,11 @@ import org.restlet.resource.ClientResource;
 
 public class RangeClient {
     public static void main(String[] args) throws Exception {
-        ClientResource resource = new ClientResource(
-                "http://localhost:8082/");
+        ClientResource resource = new ClientResource("http://localhost:8111/");
+
         // Requesting the first five characters.
         resource.getRanges().add(new Range(0, 5));
+
         // Get the representation of the resource
         resource.get().write(System.out);
     }
--- modules/org.restlet.example/src/org/restlet/example/book/restlet/ch08/sec3/sub3/RangeServer.java
+++ modules/org.restlet.example/src/org/restlet/example/book/restlet/ch08/sec3/sub3/RangeServer.java
@@ -5,8 +5,8 @@ import org.restlet.Server;
 import org.restlet.data.Protocol;
 import org.restlet.example.book.restlet.ch01.HelloServerResource;
 
-
 public class RangeServer {
+
     public static void main(String[] args) throws Exception {
         // Instantiating the Application providing the Range Service
         Application app = new Application();
@@ -15,6 +15,7 @@ public class RangeServer {
         app.setInboundRoot(HelloServerResource.class);
 
         // Instantiating the HTTP server and listening on port 8082
-        new Server(Protocol.HTTP, 8082, app).start();
+        new Server(Protocol.HTTP, 8111, app).start();
     }
+
 }
