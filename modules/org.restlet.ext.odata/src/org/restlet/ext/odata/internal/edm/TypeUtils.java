fb94d39b8c391ebb083fa20eebaeaa85656c7063
Thierry Boileau <thboileau@gmail.com>
2011-01-06 17:16:20 +0000

    Fixed handling of EDM Single properties. Reported by Andreas Taube.

--- modules/org.restlet.ext.odata/src/org/restlet/ext/odata/internal/edm/TypeUtils.java
+++ modules/org.restlet.ext.odata/src/org/restlet/ext/odata/internal/edm/TypeUtils.java
@@ -287,6 +287,8 @@ public class TypeUtils {
         } else if (adoNetType.endsWith("Single")) {
             if ((Float.class).isAssignableFrom(value.getClass())) {
                 result = toEdmSingle((Float) value);
+            } else if ((Double.class).isAssignableFrom(value.getClass())) {
+                result = toEdmSingle((Double) value);
             }
         } else if (adoNetType.endsWith("Double")) {
             if ((Double.class).isAssignableFrom(value.getClass())) {
@@ -472,6 +474,8 @@ public class TypeUtils {
         } else if (adoNetType.endsWith("Single")) {
             if ((Float.class).isAssignableFrom(value.getClass())) {
                 result = toEdmSingle((Float) value);
+            } else if ((Double.class).isAssignableFrom(value.getClass())) {
+                result = toEdmSingle((Double) value);
             }
         } else if (adoNetType.endsWith("Double")) {
             if ((Double.class).isAssignableFrom(value.getClass())) {
@@ -514,6 +518,18 @@ public class TypeUtils {
      *            The value to convert.
      * @return The value converted as String object.
      */
+    public static String toEdmSingle(double value) {
+        return singleFormat.format(value);
+    }
+
+    /**
+     * Convert the given value to the String representation of a EDM Single
+     * value.
+     * 
+     * @param value
+     *            The value to convert.
+     * @return The value converted as String object.
+     */
     public static String toEdmSingle(float value) {
         return singleFormat.format(value);
     }
@@ -527,6 +543,90 @@ public class TypeUtils {
      */
     public static String toEdmTime(long value) {
         return timeFormat.format(value);
+    }
+
+    /**
+     * Returns the corresponding Java class or scalar type.
+     * 
+     * @param edmTypeName
+     *            The type name.
+     * @return The corresponding Java class or scalar type.
+     */
+    public static Class<?> toJavaClass(String edmTypeName) {
+        Class<?> result = Object.class;
+        if (edmTypeName.endsWith("Binary")) {
+            result = byte[].class;
+        } else if (edmTypeName.endsWith("Boolean")) {
+            result = Boolean.class;
+        } else if (edmTypeName.endsWith("DateTime")) {
+            result = Date.class;
+        } else if (edmTypeName.endsWith("DateTimeOffset")) {
+            result = Date.class;
+        } else if (edmTypeName.endsWith("Time")) {
+            result = Long.class;
+        } else if (edmTypeName.endsWith("Decimal")) {
+            result = Double.class;
+        } else if (edmTypeName.endsWith("Single")) {
+            result = Double.class;
+        } else if (edmTypeName.endsWith("Double")) {
+            result = Double.class;
+        } else if (edmTypeName.endsWith("Guid")) {
+            result = String.class;
+        } else if (edmTypeName.endsWith("Int16")) {
+            result = Short.class;
+        } else if (edmTypeName.endsWith("Int32")) {
+            result = Integer.class;
+        } else if (edmTypeName.endsWith("Int64")) {
+            result = Long.class;
+        } else if (edmTypeName.endsWith("Byte")) {
+            result = Byte.class;
+        } else if (edmTypeName.endsWith("String")) {
+            result = String.class;
+        }
+
+        return result;
+    }
+
+    /**
+     * Returns the name of the corresponding Java class or scalar type.
+     * 
+     * @param edmTypeName
+     *            The type name.
+     * @return The name of the corresponding Java class or scalar type.
+     */
+    public static String toJavaTypeName(String edmTypeName) {
+        String result = "Object";
+        if (edmTypeName.endsWith("Binary")) {
+            result = "byte[]";
+        } else if (edmTypeName.endsWith("Boolean")) {
+            result = "boolean";
+        } else if (edmTypeName.endsWith("DateTime")) {
+            result = "Date";
+        } else if (edmTypeName.endsWith("DateTimeOffset")) {
+            result = "Date";
+        } else if (edmTypeName.endsWith("Time")) {
+            result = "long";
+        } else if (edmTypeName.endsWith("Decimal")) {
+            result = "double";
+        } else if (edmTypeName.endsWith("Single")) {
+            result = "double";
+        } else if (edmTypeName.endsWith("Double")) {
+            result = "double";
+        } else if (edmTypeName.endsWith("Guid")) {
+            result = "String";
+        } else if (edmTypeName.endsWith("Int16")) {
+            result = "short";
+        } else if (edmTypeName.endsWith("Int32")) {
+            result = "int";
+        } else if (edmTypeName.endsWith("Int64")) {
+            result = "long";
+        } else if (edmTypeName.endsWith("Byte")) {
+            result = "byte";
+        } else if (edmTypeName.endsWith("String")) {
+            result = "String";
+        }
+
+        return result;
     }
 
 }
