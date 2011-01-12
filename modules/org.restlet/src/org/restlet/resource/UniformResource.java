a21cbd58279f2f2b3d650369c089cde72698f1c3
Thierry Boileau <thboileau@gmail.com>
2011-01-12 14:25:30 +0000

    Fixed ClientResource#put(Object,MediaType) and #post(Object,MediaType) methods of the GWT edition. Reported by Charlie Mason.

--- modules/org.restlet/src/org/restlet/resource/UniformResource.java
+++ modules/org.restlet/src/org/restlet/resource/UniformResource.java
@@ -726,6 +726,11 @@ public abstract class UniformResource {
             org.restlet.service.ConverterService cs = getConverterService();
             result = cs.toRepresentation(source, target, this);
             // [enddef]
+            // [ifdef gwt] uncomment
+            // if (source instanceof Representation) {
+            // result = (Representation) source;
+            // }
+            // [enddef]
         }
 
         return result;
