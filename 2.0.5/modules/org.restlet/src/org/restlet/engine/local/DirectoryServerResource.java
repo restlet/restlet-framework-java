8f64bf66c58aaa7ced624f68e48d87987945754c
forge Restlet <forger@noelios.com>
2011-02-09 16:53:28 +0000

    tagged restlet 2.0.5

--- 2.0.5/modules/org.restlet/src/org/restlet/engine/local/DirectoryServerResource.java
+++ 2.0.5/modules/org.restlet/src/org/restlet/engine/local/DirectoryServerResource.java
@@ -575,7 +575,7 @@ public class DirectoryServerResource extends ServerResource {
      *            The related method.
      * @return The list of variants for the given method.
      */
-    public List<Variant> getVariants(Method method) {
+    protected List<Variant> getVariants(Method method) {
         List<Variant> result = null;
 
         if ((Method.GET.equals(method) || Method.HEAD.equals(method))) {
