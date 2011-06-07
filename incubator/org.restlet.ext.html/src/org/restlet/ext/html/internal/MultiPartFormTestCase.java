8986dd1a7a019ede34c65f90ab811adf4339ba73
Thierry Boileau <thboileau@gmail.com>
2011-06-07 10:14:29 +0000

    Fixed test case.

--- incubator/org.restlet.ext.html/src/org/restlet/ext/html/internal/MultiPartFormTestCase.java
+++ incubator/org.restlet.ext.html/src/org/restlet/ext/html/internal/MultiPartFormTestCase.java
@@ -16,28 +16,34 @@ public class MultiPartFormTestCase extends RestletTestCase {
         Representation file = new EmptyRepresentation();
         file.setMediaType(MediaType.APPLICATION_OCTET_STREAM);
 
-        MultiPartForm form = new MultiPartForm(
-                "-----------------------------1294919323195");
+        String boundary = "-----------------------------1294919323195";
+        String boundaryBis = "--" + boundary;
+        MultiPartForm form;
+        String expected;
+
+        form = new MultiPartForm(boundary);
         form.getData().add(new MultiPartData("number", "5555555555"));
         form.getData().add(new MultiPartData("clip", "rickroll"));
         form.getData().add(new MultiPartData("upload_file", file));
         form.getData().add(new MultiPartData("tos", "agree"));
 
-        String expected = "-----------------------------1294919323195\r\n"
+        expected = boundaryBis
+                + "\r\n"
                 + "Content-Disposition: form-data; name=\"number\"\r\n"
                 + "\r\n"
                 + "5555555555\r\n"
-                + "-----------------------------1294919323195\r\n"
+                + boundaryBis
+                + "\r\n"
                 + "Content-Disposition: form-data; name=\"clip\"\r\n"
                 + "\r\n"
                 + "rickroll\r\n"
-                + "-----------------------------1294919323195\r\n"
+                + boundaryBis
+                + "\r\n"
                 + "Content-Disposition: form-data; name=\"upload_file\"; filename=\"\"\r\n"
-                + "Content-Type: application/octet-stream\r\n" + "\r\n" + "\r\n"
-                + "-----------------------------1294919323195\r\n"
+                + "Content-Type: application/octet-stream\r\n" + "\r\n"
+                + "\r\n" + boundaryBis + "\r\n"
                 + "Content-Disposition: form-data; name=\"tos\"\r\n" + "\r\n"
-                + "agree\r\n" + "-----------------------------1294919323195--\r\n";
-
-        assertEquals("Value", expected, form.getText());
+                + "agree\r\n" + boundaryBis + "--\r\n";
+        assertEquals("Value 1", expected, form.getText());
     }
 }
