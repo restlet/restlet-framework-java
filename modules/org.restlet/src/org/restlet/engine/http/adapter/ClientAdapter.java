329378b5afd0ddb1d0a6c986deb8686bc1a6385c
Thierry Boileau <thboileau@gmail.com>
2011-08-19 13:31:44 +0000

    Added warning message when an unexpected error happen during the invocation of an asynchronous callback.

--- modules/org.restlet/src/org/restlet/engine/http/adapter/ClientAdapter.java
+++ modules/org.restlet/src/org/restlet/engine/http/adapter/ClientAdapter.java
@@ -54,175 +54,179 @@ import org.restlet.util.Series;
  */
 public class ClientAdapter extends Adapter {
 
-    /**
-     * Constructor.
-     * 
-     * @param context
-     *            The context to use.
-     */
-    public ClientAdapter(Context context) {
-        super(context);
-    }
+	/**
+	 * Constructor.
+	 * 
+	 * @param context
+	 *            The context to use.
+	 */
+	public ClientAdapter(Context context) {
+		super(context);
+	}
 
-    /**
-     * Commits the changes to a handled HTTP client call back into the original
-     * uniform call. The default implementation first invokes the
-     * "addResponseHeaders" then asks the "htppCall" to send the response back
-     * to the client.
-     * 
-     * @param httpCall
-     *            The original HTTP call.
-     * @param request
-     *            The high-level request.
-     * @param response
-     *            The high-level response.
-     * @throws Exception
-     */
-    public void commit(final ClientCall httpCall, Request request,
-            Response response) throws Exception {
-        if (httpCall != null) {
-            // Check if the call is asynchronous
-            if (request.getOnResponse() != null) {
-                final Uniform userCallback = request.getOnResponse();
+	/**
+	 * Commits the changes to a handled HTTP client call back into the original
+	 * uniform call. The default implementation first invokes the
+	 * "addResponseHeaders" then asks the "htppCall" to send the response back
+	 * to the client.
+	 * 
+	 * @param httpCall
+	 *            The original HTTP call.
+	 * @param request
+	 *            The high-level request.
+	 * @param response
+	 *            The high-level response.
+	 * @throws Exception
+	 */
+	public void commit(final ClientCall httpCall, Request request,
+			Response response) throws Exception {
+		if (httpCall != null) {
+			// Check if the call is asynchronous
+			if (request.getOnResponse() != null) {
+				final Uniform userCallback = request.getOnResponse();
 
-                // Send the request to the client
-                httpCall.sendRequest(request, response, new Uniform() {
-                    public void handle(Request request, Response response) {
-                        try {
-                            updateResponse(response,
-                                    new Status(httpCall.getStatusCode(), null,
-                                            httpCall.getReasonPhrase(), null),
-                                    httpCall);
-                            userCallback.handle(request, response);
-                        } catch (Exception e) {
-                            // Unexpected exception occurred
-                            if ((response.getStatus() == null)
-                                    || !response.getStatus().isError()) {
-                                response.setStatus(
-                                        Status.CONNECTOR_ERROR_INTERNAL, e);
-                                userCallback.handle(request, response);
-                            }
-                        }
-                    }
-                });
-            } else {
-                if (Edition.CURRENT == Edition.GWT) {
-                    System.err
-                            .println("HTTP client calls must have a callback in the GWT edition");
-                } else {
-                    // [ifndef gwt]
-                    updateResponse(response, httpCall.sendRequest(request),
-                            httpCall);
-                    // [enddef]
-                }
-            }
-        }
-    }
+				// Send the request to the client
+				httpCall.sendRequest(request, response, new Uniform() {
+					public void handle(Request request, Response response) {
+						try {
+							updateResponse(response,
+									new Status(httpCall.getStatusCode(), null,
+											httpCall.getReasonPhrase(), null),
+									httpCall);
+							userCallback.handle(request, response);
+						} catch (Throwable t) {
+							getLogger()
+									.log(Level.WARNING,
+											"Unexpected error or exception inside the user call back",
+											t);
+							// Unexpected exception occurred
+							if ((response.getStatus() == null)
+									|| !response.getStatus().isError()) {
+								response.setStatus(
+										Status.CONNECTOR_ERROR_INTERNAL, t);
+								userCallback.handle(request, response);
+							}
+						}
+					}
+				});
+			} else {
+				if (Edition.CURRENT == Edition.GWT) {
+					System.err
+							.println("HTTP client calls must have a callback in the GWT edition");
+				} else {
+					// [ifndef gwt]
+					updateResponse(response, httpCall.sendRequest(request),
+							httpCall);
+					// [enddef]
+				}
+			}
+		}
+	}
 
-    /**
-     * Reads the response headers of a handled HTTP client call to update the
-     * original uniform call.
-     * 
-     * @param httpCall
-     *            The handled HTTP client call.
-     * @param response
-     *            The high-level response to update.
-     */
-    protected void readResponseHeaders(ClientCall httpCall, Response response) {
-        try {
-            Series<Parameter> responseHeaders = httpCall.getResponseHeaders();
+	/**
+	 * Reads the response headers of a handled HTTP client call to update the
+	 * original uniform call.
+	 * 
+	 * @param httpCall
+	 *            The handled HTTP client call.
+	 * @param response
+	 *            The high-level response to update.
+	 */
+	protected void readResponseHeaders(ClientCall httpCall, Response response) {
+		try {
+			Series<Parameter> responseHeaders = httpCall.getResponseHeaders();
 
-            // Put the response headers in the call's attributes map
-            response.getAttributes().put(HeaderConstants.ATTRIBUTE_HEADERS,
-                    responseHeaders);
+			// Put the response headers in the call's attributes map
+			response.getAttributes().put(HeaderConstants.ATTRIBUTE_HEADERS,
+					responseHeaders);
 
-            HeaderUtils.copyResponseTransportHeaders(responseHeaders, response);
-        } catch (Exception e) {
-            e.printStackTrace();
-            getLogger()
-                    .log(Level.FINE,
-                            "An error occured during the processing of the HTTP response.",
-                            e);
-            response.setStatus(Status.CONNECTOR_ERROR_INTERNAL, e);
-        }
-    }
+			HeaderUtils.copyResponseTransportHeaders(responseHeaders, response);
+		} catch (Exception e) {
+			e.printStackTrace();
+			getLogger()
+					.log(Level.FINE,
+							"An error occured during the processing of the HTTP response.",
+							e);
+			response.setStatus(Status.CONNECTOR_ERROR_INTERNAL, e);
+		}
+	}
 
-    /**
-     * Converts a low-level HTTP call into a high-level uniform call.
-     * 
-     * @param client
-     *            The HTTP client that will handle the call.
-     * @param request
-     *            The high-level request.
-     * @return A new high-level uniform call.
-     */
-    public ClientCall toSpecific(HttpClientHelper client, Request request) {
-        // Create the low-level HTTP client call
-        ClientCall result = client.create(request);
+	/**
+	 * Converts a low-level HTTP call into a high-level uniform call.
+	 * 
+	 * @param client
+	 *            The HTTP client that will handle the call.
+	 * @param request
+	 *            The high-level request.
+	 * @return A new high-level uniform call.
+	 */
+	public ClientCall toSpecific(HttpClientHelper client, Request request) {
+		// Create the low-level HTTP client call
+		ClientCall result = client.create(request);
 
-        // Add the headers
-        if (result != null) {
-            HeaderUtils.addGeneralHeaders(request, result.getRequestHeaders());
+		// Add the headers
+		if (result != null) {
+			HeaderUtils.addGeneralHeaders(request, result.getRequestHeaders());
 
-            if (request.getEntity() != null) {
-                HeaderUtils.addEntityHeaders(request.getEntity(),
-                        result.getRequestHeaders());
-            }
+			if (request.getEntity() != null) {
+				HeaderUtils.addEntityHeaders(request.getEntity(),
+						result.getRequestHeaders());
+			}
 
-            // NOTE: This must stay at the end because the AWS challenge
-            // scheme requires access to all HTTP headers
-            HeaderUtils.addRequestHeaders(request, result.getRequestHeaders());
-        }
+			// NOTE: This must stay at the end because the AWS challenge
+			// scheme requires access to all HTTP headers
+			HeaderUtils.addRequestHeaders(request, result.getRequestHeaders());
+		}
 
-        return result;
-    }
+		return result;
+	}
 
-    /**
-     * Updates the response with information from the lower-level HTTP client
-     * call.
-     * 
-     * @param response
-     *            The response to update.
-     * @param status
-     *            The response status to apply.
-     * @param httpCall
-     *            The source HTTP client call.
-     * @throws IOException
-     */
-    public void updateResponse(Response response, Status status,
-            ClientCall httpCall) {
-        // Send the request to the client
-        response.setStatus(status);
+	/**
+	 * Updates the response with information from the lower-level HTTP client
+	 * call.
+	 * 
+	 * @param response
+	 *            The response to update.
+	 * @param status
+	 *            The response status to apply.
+	 * @param httpCall
+	 *            The source HTTP client call.
+	 * @throws IOException
+	 */
+	public void updateResponse(Response response, Status status,
+			ClientCall httpCall) {
+		// Send the request to the client
+		response.setStatus(status);
 
-        // Get the server address
-        response.getServerInfo().setAddress(httpCall.getServerAddress());
-        response.getServerInfo().setPort(httpCall.getServerPort());
+		// Get the server address
+		response.getServerInfo().setAddress(httpCall.getServerAddress());
+		response.getServerInfo().setPort(httpCall.getServerPort());
 
-        // Read the response headers
-        readResponseHeaders(httpCall, response);
+		// Read the response headers
+		readResponseHeaders(httpCall, response);
 
-        // Set the entity
-        response.setEntity(httpCall.getResponseEntity(response));
+		// Set the entity
+		response.setEntity(httpCall.getResponseEntity(response));
 
-        // Release the representation's content for some obvious cases
-        if (response.getEntity() != null) {
-            if (response.getEntity().getSize() == 0) {
-                response.getEntity().release();
-            } else if (response.getRequest().getMethod().equals(Method.HEAD)) {
-                response.getEntity().release();
-            } else if (response.getStatus().equals(Status.SUCCESS_NO_CONTENT)) {
-                response.getEntity().release();
-            } else if (response.getStatus()
-                    .equals(Status.SUCCESS_RESET_CONTENT)) {
-                response.getEntity().release();
-                response.setEntity(null);
-            } else if (response.getStatus().equals(
-                    Status.REDIRECTION_NOT_MODIFIED)) {
-                response.getEntity().release();
-            } else if (response.getStatus().isInformational()) {
-                response.getEntity().release();
-                response.setEntity(null);
-            }
-        }
-    }
+		// Release the representation's content for some obvious cases
+		if (response.getEntity() != null) {
+			if (response.getEntity().getSize() == 0) {
+				response.getEntity().release();
+			} else if (response.getRequest().getMethod().equals(Method.HEAD)) {
+				response.getEntity().release();
+			} else if (response.getStatus().equals(Status.SUCCESS_NO_CONTENT)) {
+				response.getEntity().release();
+			} else if (response.getStatus()
+					.equals(Status.SUCCESS_RESET_CONTENT)) {
+				response.getEntity().release();
+				response.setEntity(null);
+			} else if (response.getStatus().equals(
+					Status.REDIRECTION_NOT_MODIFIED)) {
+				response.getEntity().release();
+			} else if (response.getStatus().isInformational()) {
+				response.getEntity().release();
+				response.setEntity(null);
+			}
+		}
+	}
 }
