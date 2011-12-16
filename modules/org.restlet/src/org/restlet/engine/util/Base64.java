3f28a5ba0e384656a8fbb4379060fd24bf7d1e08
Thierry Boileau <thboileau@gmail.com>
2011-12-16 10:44:21 +0000

    Fix the GWT edition, client requests using HTTP_BASIC authentication scheme did not generate Authorization header. Reported by Koen Maes.

--- modules/org.restlet/src/org/restlet/engine/util/Base64.java
+++ modules/org.restlet/src/org/restlet/engine/util/Base64.java
@@ -232,7 +232,8 @@ public class Base64 {
         }
         return new String(output, 0, pos);
     }
-
+    
+    // [ifndef gwt] method
     /**
      * Encodes an entire chars array into a Base64 string, with optional
      * newlines after every 76 characters.
@@ -247,6 +248,7 @@ public class Base64 {
         return encode(BioUtils.toByteArray(chars), newlines);
     }
 
+    // [ifndef gwt] method
     /**
      * Encodes an entire chars array into a Base64 string, with optional
      * newlines after every 76 characters.
