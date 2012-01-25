405b6f6c8399278c43c4a52684063d9eac2ae92a
Thierry Boileau <thierry.boileau@noelios.com>
2012-01-25 10:53:37 +0100

    Updated copyrights.

--- modules/org.restlet.test/src/org/restlet/test/jaxrs/services/others/package-info.java
+++ modules/org.restlet.test/src/org/restlet/test/jaxrs/services/others/package-info.java
@@ -1,43 +1,34 @@
 /**
- * Copyright 2005-2011 Noelios Technologies.
+ * Copyright 2005-2012 Restlet S.A.S.
  * 
  * The contents of this file are subject to the terms of one of the following
- * open source licenses: LGPL 3.0 or LGPL 2.1 or CDDL 1.0 or EPL 1.0 (the
- * "Licenses"). You can select the license that you prefer but you may not use
- * this file except in compliance with one of these Licenses.
+ * open source licenses: Apache 2.0 or LGPL 3.0 or LGPL 2.1 or CDDL 1.0 or EPL
+ * 1.0 (the "Licenses"). You can select the license that you prefer but you may
+ * not use this file except in compliance with one of these Licenses.
+ * 
+ * You can obtain a copy of the Apache 2.0 license at
+ * http://www.opensource.org/licenses/apache-2.0
  * 
  * You can obtain a copy of the LGPL 3.0 license at
- * http://www.opensource.org/licenses/lgpl-3.0.html
+ * http://www.opensource.org/licenses/lgpl-3.0
  * 
  * You can obtain a copy of the LGPL 2.1 license at
- * http://www.opensource.org/licenses/lgpl-2.1.php
+ * http://www.opensource.org/licenses/lgpl-2.1
  * 
  * You can obtain a copy of the CDDL 1.0 license at
- * http://www.opensource.org/licenses/cddl1.php
+ * http://www.opensource.org/licenses/cddl1
  * 
  * You can obtain a copy of the EPL 1.0 license at
- * http://www.opensource.org/licenses/eclipse-1.0.php
+ * http://www.opensource.org/licenses/eclipse-1.0
  * 
  * See the Licenses for the specific language governing permissions and
  * limitations under the Licenses.
  * 
  * Alternatively, you can obtain a royalty free commercial license with less
  * limitations, transferable or non-transferable, directly at
- * http://www.noelios.com/products/restlet-engine
+ * http://www.restlet.com/products/restlet-framework
  * 
- * Restlet is a registered trademark of Noelios Technologies.
+ * Restlet is a registered trademark of Restlet S.A.S.
  */
 
-/**
- * <p>
- * Other classes for the test resources in package
- * {@link org.restlet.ext.jaxrs.services.resource}.
- * </p>
- * <p>
- * This extension as well as the JAX-RS specification are currently under
- * development. You should only use this extension for experimental purpose.
- * </p>
- * 
- * @author Stephan Koops
- */
 package org.restlet.test.jaxrs.services.others;
--- modules/org.restlet.test/src/org/restlet/test/jaxrs/services/resources/PathParamTestResource3.java
+++ modules/org.restlet.test/src/org/restlet/test/jaxrs/services/resources/PathParamTestResource3.java
@@ -1,24 +1,36 @@
-/*
- * Copyright 2005-2008 Noelios Consulting.
- *
- * The contents of this file are subject to the terms
- * of the Common Development and Distribution License
- * (the "License").  You may not use this file except
- * in compliance with the License.
- *
- * You can obtain a copy of the license at
- * http://www.opensource.org/licenses/cddl1.txt
- * See the License for the specific language governing
- * permissions and limitations under the License.
- *
- * When distributing Covered Code, include this CDDL
- * HEADER in each file and include the License file at
- * http://www.opensource.org/licenses/cddl1.txt
- * If applicable, add the following below this CDDL
- * HEADER, with the fields enclosed by brackets "[]"
- * replaced with your own identifying information:
- * Portions Copyright [yyyy] [name of copyright owner]
+/**
+ * Copyright 2005-2012 Restlet S.A.S.
+ * 
+ * The contents of this file are subject to the terms of one of the following
+ * open source licenses: Apache 2.0 or LGPL 3.0 or LGPL 2.1 or CDDL 1.0 or EPL
+ * 1.0 (the "Licenses"). You can select the license that you prefer but you may
+ * not use this file except in compliance with one of these Licenses.
+ * 
+ * You can obtain a copy of the Apache 2.0 license at
+ * http://www.opensource.org/licenses/apache-2.0
+ * 
+ * You can obtain a copy of the LGPL 3.0 license at
+ * http://www.opensource.org/licenses/lgpl-3.0
+ * 
+ * You can obtain a copy of the LGPL 2.1 license at
+ * http://www.opensource.org/licenses/lgpl-2.1
+ * 
+ * You can obtain a copy of the CDDL 1.0 license at
+ * http://www.opensource.org/licenses/cddl1
+ * 
+ * You can obtain a copy of the EPL 1.0 license at
+ * http://www.opensource.org/licenses/eclipse-1.0
+ * 
+ * See the Licenses for the specific language governing permissions and
+ * limitations under the Licenses.
+ * 
+ * Alternatively, you can obtain a royalty free commercial license with less
+ * limitations, transferable or non-transferable, directly at
+ * http://www.restlet.com/products/restlet-framework
+ * 
+ * Restlet is a registered trademark of Restlet S.A.S.
  */
+
 package org.restlet.test.jaxrs.services.resources;
 
 import javax.ws.rs.GET;
