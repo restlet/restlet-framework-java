e3e75d42f827fc6dd21da8d244c35262dfcda988
Thierry Boileau <thierry.boileau@noelios.com>
2012-03-28 09:04:20 +0200

    modules/org.restlet.test/src/org/restlet/test/resource/MyServerResource18.java

---
+++ modules/org.restlet.test/src/org/restlet/test/resource/GenericServerResource17.java
@@ -0,0 +1,11 @@
+package org.restlet.test.resource;
+
+import org.restlet.resource.ServerResource;
+
+public class GenericServerResource17<E> extends ServerResource implements MyResource17<E> {
+
+    public E add(E rep) {
+        return rep;
+    };
+
+}
