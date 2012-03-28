76d7af9d0727d91462275375ddd6e71fc4009709
Thierry Boileau <thierry.boileau@noelios.com>
2012-03-28 16:26:34 +0200

    Fixed potential NPE.

--- modules/org.restlet/src/org/restlet/data/ClientInfo.java
+++ modules/org.restlet/src/org/restlet/data/ClientInfo.java
@@ -36,6 +36,7 @@ package org.restlet.data;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -225,6 +226,9 @@ public final class ClientInfo {
                         }
                     }
                     ClientInfo.userAgentTemplates = u;
+                }
+                if (u == null) {
+                    u = new ArrayList<String>();
                 }
             }
         }
