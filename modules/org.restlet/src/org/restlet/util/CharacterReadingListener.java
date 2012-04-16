2bc16c28e7113ff3a4387056508a85d975c4cf5e
Jerome Louvel <jerome.louvel@restlet.com>
2012-04-16 23:01:11 +0200

    - Bug fixed 	   - Fixed user agent parsing for Chrome and Safari. 	- API changes 	   - Renamed ReadableListener into ReadingListener 	   - Renamed InputListener into ByteReadingListener 	   - Renamed ReaderListener into CharacterReadingListener 	   - Renamed StringListener into StringReadingListener

--- modules/org.restlet/src/org/restlet/util/CharacterReadingListener.java
+++ modules/org.restlet/src/org/restlet/util/CharacterReadingListener.java
@@ -33,20 +33,25 @@
 
 package org.restlet.util;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.ByteBuffer;
+import java.io.InputStreamReader;
+import java.io.Reader;
 
+import org.restlet.data.CharacterSet;
 import org.restlet.engine.io.IoUtils;
 import org.restlet.representation.Representation;
 
 /**
- * Selection listener notifying new content as an {@link InputStream}.
+ * Selection listener notifying new content as a {@link Reader}. It relies on
+ * the representation's character set for proper character decoding.
  * 
  * @author Jerome Louvel
  */
-public abstract class InputListener extends ReadableListener {
+public abstract class CharacterReadingListener extends ByteReadingListener {
+
+    /** The character set of the associated representation. */
+    private final CharacterSet characterSet;
 
     /**
      * Default constructor. Uses a byte buffer of {@link IoUtils#BUFFER_SIZE}
@@ -56,41 +61,38 @@ public abstract class InputListener extends ReadableListener {
      *            The source representation.
      * @throws IOException
      */
-    public InputListener(Representation source) throws IOException {
-        super(source);
+    public CharacterReadingListener(Representation source) throws IOException {
+        this(source, IoUtils.BUFFER_SIZE);
     }
 
     /**
      * Constructor. Uses a byte buffer of a given size.
      * 
      * @param source
-     *            The source byte channel.
+     *            The source representation.
      * @param bufferSize
      *            The byte buffer to use.
      * @throws IOException
      */
-    public InputListener(Representation source, int bufferSize)
+    public CharacterReadingListener(Representation source, int bufferSize)
             throws IOException {
         super(source, bufferSize);
+        this.characterSet = source.getCharacterSet();
+    }
+
+    @Override
+    protected final void onContent(InputStream inputStream) {
+        InputStreamReader isr = new InputStreamReader(inputStream,
+                this.characterSet.toCharset());
+        onContent(isr);
     }
 
     /**
      * Callback invoked when new content is available.
      * 
-     * @param byteBuffer
-     *            The byte buffer filled with the new content (correctly flip).
+     * @param reader
+     *            The reader allowing to retrieve the new content.
      */
-    protected final void onContent(ByteBuffer byteBuffer) {
-        onContent(new ByteArrayInputStream(byteBuffer.array(),
-                byteBuffer.arrayOffset(), byteBuffer.remaining()));
-    }
-
-    /**
-     * Callback invoked when new content is available.
-     * 
-     * @param inputStream
-     *            The input stream allowing to retrieve the new content.
-     */
-    protected abstract void onContent(InputStream inputStream);
+    protected abstract void onContent(Reader reader);
 
 }
