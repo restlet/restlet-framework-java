3487ec7e1fdac25b1ceb9b242997bdeea54361ba
Thierry Boileau <thierry.boileau@restlet.com>
2012-07-25 12:44:48 +0200

    Removed test cases using the internal connectors.

--- modules/org.restlet.test/src/org/restlet/test/ext/ssl/SslBaseConnectorsTestCase.java
+++ modules/org.restlet.test/src/org/restlet/test/ext/ssl/SslBaseConnectorsTestCase.java
@@ -67,11 +67,11 @@ public abstract class SslBaseConnectorsTestCase extends RestletTestCase {
 
     private final boolean enabledClientApache = true;
 
-    private final boolean enabledClientInternal = true;
+    private final boolean enabledClientInternal = false;
 
     private final boolean enabledClientJdkNet = true;
 
-    private final boolean enabledServerInternal = true;
+    private final boolean enabledServerInternal = false;
 
     private final boolean enabledServerJetty = true;
 
