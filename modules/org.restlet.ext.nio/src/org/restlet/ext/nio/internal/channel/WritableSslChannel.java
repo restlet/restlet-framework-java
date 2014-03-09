e704f93c16da127780722bc1abba214a680a1dd9
Jonathan Keljo <jk@cs.stanford.edu>
2014-03-09 18:03:28 -0400

    Fix issues #852 and #862.
    - Issue #852: Android NIO server SSL handshake was hanging due to SSLEngine behaving differently on Android than on J2SE
    - Issue #862: Android and J2SE SSL server was hanging on close-notify due to SSLEngine going directly from NEED_WRAP to NOT_HANDSHAKING

--- modules/org.restlet.ext.nio/src/org/restlet/ext/nio/internal/channel/WritableSslChannel.java
+++ modules/org.restlet.ext.nio/src/org/restlet/ext/nio/internal/channel/WritableSslChannel.java
@@ -113,8 +113,22 @@ public class WritableSslChannel extends WritableBufferedChannel implements
     public int onFill(Buffer buffer, Object... args) throws IOException {
         int srcSize = buffer.remaining();
         ByteBuffer applicationBuffer = (ByteBuffer) args[0];
-        SSLEngineResult sslResult = getConnection().getSslEngine().wrap(
-                applicationBuffer, buffer.getBytes());
+
+        HandshakeStatus handshakeStatus = getConnection().getSslHandshakeStatus();
+        SSLEngineResult sslResult;
+
+        // Empty buffers should generally only be passed to SSLEngine during the handshaking process,
+        // when SSLEngine will be generating handshake packets itself. The behavior of SSLEngine when
+        // an empty buffer is passed during normal operation varies across platforms. To avoid problems
+        // due to this inconsistency, we avoid calling SSLEngine with empty buffers when not handshaking,
+        // and return what J2SE's SSLEngine would have in that case. See the following issue for more details:
+        // https://github.com/restlet/restlet-framework-java/issues/852
+        if (applicationBuffer.hasRemaining() || handshakeStatus != HandshakeStatus.NOT_HANDSHAKING) {
+            sslResult = getConnection().getSslEngine().wrap(applicationBuffer, buffer.getBytes());
+        } else {
+            sslResult = new SSLEngineResult(Status.BUFFER_OVERFLOW, handshakeStatus, 0, 0);
+        }
+
         getConnection().setSslResult(sslResult);
         return srcSize - buffer.remaining();
     }
