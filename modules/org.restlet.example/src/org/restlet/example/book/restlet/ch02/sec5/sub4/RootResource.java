bb6c848b7470a04a086bd2a512d304e33d56753d
Ubuntu <ubuntu@ip-10-43-157-52.ec2.internal>
2014-03-19 07:44:33 +0000

    Updated copyright

--- modules/org.restlet.example/src/org/restlet/example/book/restlet/ch02/sec5/sub4/RootResource.java
+++ modules/org.restlet.example/src/org/restlet/example/book/restlet/ch02/sec5/sub4/RootResource.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2005-2013 Restlet S.A.S.
+ * Copyright 2005-2014 Restlet
  * 
  * The contents of this file are subject to the terms of one of the following
  * open source licenses: Apache 2.0 or LGPL 3.0 or LGPL 2.1 or CDDL 1.0 or EPL
@@ -28,7 +28,7 @@
  * limitations, transferable or non-transferable, directly at
  * http://www.restlet.com/products/restlet-framework
  * 
- * Restlet is a registered trademark of Restlet S.A.S.
+ * Restlet is a registered trademark of Restlet
  */
 
 package org.restlet.example.book.restlet.ch02.sec5.sub4;
