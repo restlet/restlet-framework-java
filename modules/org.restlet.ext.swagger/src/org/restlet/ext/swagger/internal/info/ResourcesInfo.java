5df848570d1da67674a30efc84bd90784105b0e7
Thierry Boileau <tboileau@restlet.com>
2014-07-13 07:49:30 +0200

    added support of list - first effort

---
+++ modules/org.restlet.ext.swagger/src/org/restlet/ext/swagger/internal/info/ResourcesInfo.java
@@ -0,0 +1,138 @@
+/**
+ * Copyright 2005-2014 Restlet
+ * 
+ * The contents of this file are subject to the terms of one of the following
+ * open source licenses: Apache 2.0 or LGPL 3.0 or LGPL 2.1 or CDDL 1.0 or EPL
+ * 1.0 (the "Licenses"). You can select the license that you prefer but you may
+ * not use this file except in compliance with one of these Licenses.
+ * 
+ * You can obtain a copy of the Apache 2.0 license at
+ * http://www.opensource.org/licenses/apache-2.0
+ * 
+ * You can obtain a copy of the LGPL 3.0 license at
+ * http://www.opensource.org/licenses/lgpl-3.0
+ * 
+ * You can obtain a copy of the LGPL 2.1 license at
+ * http://www.opensource.org/licenses/lgpl-2.1
+ * 
+ * You can obtain a copy of the CDDL 1.0 license at
+ * http://www.opensource.org/licenses/cddl1
+ * 
+ * You can obtain a copy of the EPL 1.0 license at
+ * http://www.opensource.org/licenses/eclipse-1.0
+ * 
+ * See the Licenses for the specific language governing permissions and
+ * limitations under the Licenses.
+ * 
+ * Alternatively, you can obtain a royalty free commercial license with less
+ * limitations, transferable or non-transferable, directly at
+ * http://restlet.com/products/restlet-framework
+ * 
+ * Restlet is a registered trademark of Restlet S.A.S.
+ */
+
+package org.restlet.ext.swagger.internal.info;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.restlet.data.Reference;
+
+/**
+ * Describes the root resources of an application.
+ * 
+ * @author Jerome Louvel
+ */
+public class ResourcesInfo extends DocumentedInfo {
+    /** Base URI for each child resource identifier. */
+    private Reference baseRef;
+
+    /** List of child resources. */
+    private List<ResourceInfo> resources;
+
+    /**
+     * Constructor.
+     */
+    public ResourcesInfo() {
+        super();
+    }
+
+    /**
+     * Constructor with a single documentation element.
+     * 
+     * @param documentation
+     *            A single documentation element.
+     */
+    public ResourcesInfo(DocumentationInfo documentation) {
+        super(documentation);
+    }
+
+    /**
+     * Constructor with a list of documentation elements.
+     * 
+     * @param documentations
+     *            The list of documentation elements.
+     */
+    public ResourcesInfo(List<DocumentationInfo> documentations) {
+        super(documentations);
+    }
+
+    /**
+     * Constructor with a single documentation element.
+     * 
+     * @param documentation
+     *            A single documentation element.
+     */
+    public ResourcesInfo(String documentation) {
+        super(documentation);
+    }
+
+    /**
+     * Returns the base URI for each child resource identifier.
+     * 
+     * @return The base URI for each child resource identifier.
+     */
+    public Reference getBaseRef() {
+        return this.baseRef;
+    }
+
+    /**
+     * Returns the list of child resources.
+     * 
+     * @return The list of child resources.
+     */
+    public List<ResourceInfo> getResources() {
+        // Lazy initialization with double-check.
+        List<ResourceInfo> r = this.resources;
+        if (r == null) {
+            synchronized (this) {
+                r = this.resources;
+                if (r == null) {
+                    this.resources = r = new ArrayList<ResourceInfo>();
+                }
+            }
+        }
+        return r;
+    }
+
+    /**
+     * Sets the base URI for each child resource identifier.
+     * 
+     * @param baseRef
+     *            The base URI for each child resource identifier.
+     */
+    public void setBaseRef(Reference baseRef) {
+        this.baseRef = baseRef;
+    }
+
+    /**
+     * Sets the list of child resources.
+     * 
+     * @param resources
+     *            The list of child resources.
+     */
+    public void setResources(List<ResourceInfo> resources) {
+        this.resources = resources;
+    }
+
+}
