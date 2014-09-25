1d6928113f8c65a980d737aa2d608da81155e562
quilicicf <quilicicf@gmail.com>
2014-09-25 12:44:24 +0200

    Enhanced SwaggerUtils (https://github.com/restlet/apispark/issues/1520)

--- modules/org.restlet.ext.apispark/src/org/restlet/ext/apispark/internal/conversion/SwaggerUtils.java
+++ modules/org.restlet.ext.apispark/src/org/restlet/ext/apispark/internal/conversion/SwaggerUtils.java
@@ -132,6 +132,59 @@ public abstract class SwaggerUtils {
     }
 
     /**
+     * Converts Java types to Swagger types
+     * 
+     * @param dataType
+     *            The Java type
+     * @return The corresponding Swagger type
+     */
+    public static SwaggerTypeFormat convertType(String dataType) {
+        if ("String".equals(dataType)) {
+            return new SwaggerTypeFormat("string");
+        } else if ("Integer".equals(dataType)) {
+            return new SwaggerTypeFormat("integer", "int32");
+        } else if ("Long".equals(dataType)) {
+            return new SwaggerTypeFormat("integer", "int64");
+        } else if ("Float".equals(dataType)) {
+            return new SwaggerTypeFormat("number", "float");
+        } else if ("Double".equals(dataType)) {
+            return new SwaggerTypeFormat("number", "double");
+        } else if ("Boolean".equals(dataType)) {
+            return new SwaggerTypeFormat("boolean");
+        } else if ("Date".equals(dataType)) {
+            return new SwaggerTypeFormat("string", "date");
+        } else {
+            return new SwaggerTypeFormat(dataType);
+        }
+    }
+
+    /**
+     * Internal class representing a Swagger type
+     */
+    public static class SwaggerTypeFormat {
+        private String type;
+
+        private String format;
+
+        public SwaggerTypeFormat(String type) {
+            this(type, null);
+        }
+
+        public SwaggerTypeFormat(String type, String format) {
+            this.type = type;
+            this.format = format;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public String getFormat() {
+            return format;
+        }
+    }
+
+    /**
      * Indicates if the given velue is either null or empty.
      * 
      * @param value
