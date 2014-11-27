05ad4a0d5dbc30fd032efe94ec80eae9f263b707
thboileau <thboileau@gmail.com>
2014-11-27 15:12:00 +0100

    Removed apispark extension.

--- modules/org.restlet.ext.apispark/src/org/restlet/ext/apispark/internal/model/swagger/AuthorizationType.java
+++
@@ -1,9 +0,0 @@
-package org.restlet.ext.apispark.internal.model.swagger;
-
-public abstract class AuthorizationType {
-    public final static String BASIC_AUTH = "basicAuth";
-
-    public final static String API_KEY = "apiKey";
-
-    public final static String OAUTH2 = "oauth2";
-}
--- modules/org.restlet.ext.swagger/src/org/restlet/ext/swagger/internal/model/AuthorizationType.java
+++
@@ -1,9 +0,0 @@
-package org.restlet.ext.swagger.internal.model;
-
-public abstract class AuthorizationType {
-    public final static String BASIC_AUTH = "basicAuth";
-
-    public final static String API_KEY = "apiKey";
-
-    public final static String OAUTH2 = "oauth2";
-}
