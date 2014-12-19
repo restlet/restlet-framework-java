811651eb25b262af3457624330b53b014fae3b27
thboileau <thboileau@gmail.com>
2014-12-19 21:11:56 +0100

    Reintroduced registration by default of the internal HTTP server connectors in JEE edition.

--- modules/org.restlet/src/org/restlet/engine/Engine.java
+++ modules/org.restlet/src/org/restlet/engine/Engine.java
@@ -893,7 +893,7 @@ public class Engine {
                 new org.restlet.engine.local.RiapServerHelper(null));
         // [enddef]
 
-        // [ifndef android, gae, gwt, jee]
+        // [ifndef android, gae, gwt]
         getRegisteredServers().add(
                 new org.restlet.engine.connector.HttpServerHelper(null));
         getRegisteredServers().add(
