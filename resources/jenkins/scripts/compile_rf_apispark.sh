43401f6af5021fa23ced0578fad881fb51fc8f5f
Guillaume Blondeau <gblondeau@restlet.com>
2015-02-09 17:51:36 +0100

    Create ci script with unit tests

---
+++ resources/jenkins/scripts/compile_rf_apispark.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+# Restlet Framework source dir: first argument of the script, given by Jenkins, matches to jenkins workspace
+RF_SOURCE_DIR=$1
+
+# Release type (snapshot, milestone, rc)
+RELEASE_TYPE=$2
+
+# Version (eg: 2.3)
+VERSION=$3
+
+# Build tag: will be used to create bundles artifact
+BUILD_TAG=$5
+
+# Clean build directories
+rm -rf build/editions
+
+cd build
+
+# Build RF OSGI version for APISpark 
+ant -Deditions=osgi -Dverify=false -Djavadoc=false -Dmaven=false -Dnsis=false -Dpackage=false -Declipse-pde=true -Declipse-pde-optional-dependencies=true -Dp2=true
+
+# Build  RF JSE version for agent and create maven artefacts
+ant -Deditions=jse -Dverify=false -Djavadoc=false -Dmaven=true -Dnsis=false -Dpackage=false -Declipse-pde=true -Declipse-pde-optional-dependencies=true -Dp2=true
+
+# Run test suite
+ant -Deditions=jse -Dverify=true 
