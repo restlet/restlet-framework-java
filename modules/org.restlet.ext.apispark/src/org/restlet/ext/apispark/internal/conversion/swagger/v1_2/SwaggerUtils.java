c6d5d5cd24702dba8f108fc1536f765f5f81a4ce
quilicicf <cquilici@restlet.com>
2015-06-01 11:33:23 +0200

    Add SwaggerUtils for Swagger 2.0 (restlet/apispark#3793)

--- modules/org.restlet.ext.apispark/src/org/restlet/ext/apispark/internal/conversion/swagger/v1_2/SwaggerUtils.java
+++ modules/org.restlet.ext.apispark/src/org/restlet/ext/apispark/internal/conversion/swagger/v1_2/SwaggerUtils.java
@@ -26,29 +26,18 @@ package org.restlet.ext.apispark.internal.conversion.swagger.v1_2;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.regex.Pattern;
 
-import org.restlet.Client;
-import org.restlet.Context;
-import org.restlet.data.ChallengeScheme;
-import org.restlet.data.MediaType;
-import org.restlet.data.Protocol;
-import org.restlet.engine.util.StringUtils;
+import org.restlet.ext.apispark.internal.conversion.ImportUtils;
 import org.restlet.ext.apispark.internal.conversion.TranslationException;
 import org.restlet.ext.apispark.internal.conversion.swagger.v1_2.model.ApiDeclaration;
 import org.restlet.ext.apispark.internal.conversion.swagger.v1_2.model.ResourceListing;
 import org.restlet.ext.apispark.internal.conversion.swagger.v1_2.model.ResourceListingApi;
 import org.restlet.ext.apispark.internal.model.Definition;
-import org.restlet.ext.jackson.JacksonRepresentation;
-import org.restlet.representation.Representation;
-import org.restlet.resource.ClientResource;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
@@ -59,58 +48,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public abstract class SwaggerUtils {
 
     /** Internal logger. */
-    protected static Logger LOGGER = Logger.getLogger(SwaggerUtils.class
-            .getName());
+    protected static Logger LOGGER = Logger.getLogger(SwaggerUtils.class.getName());
 
-    private static Client client = new Client(Arrays.asList(Protocol.HTTP, Protocol.HTTPS));
-
-    static {
-        client.setContext(new Context());
-    }
-
-    private static ClientResource createAuthenticatedClientResource(String url,
-            String userName, String password) {
-        ClientResource cr = new ClientResource(url);
-        cr.setNext(client);
-        cr.accept(MediaType.APPLICATION_JSON);
-        if (!StringUtils.isNullOrEmpty(userName)
-                && !StringUtils.isNullOrEmpty(password)) {
-            cr.setChallengeResponse(ChallengeScheme.HTTP_BASIC, userName,
-                    password);
-        }
-        return cr;
-    }
-    
     /**
-     * Downloads the content served at the given URL and de-serializes it in the given Java class.
-     * Forces the representation's media type to JSON to avoid de-serialization errors.
-     * 
-     * @param url
-     *            The URL on which the content is served
-     * @param userName
-     *            The login for basic HTTP authentication.
-     * @param password
-     *            The password for basic HTTP authentication.
-     * @param clazz
-     *            The Java class in which to de-serialize.
-     * @return The de-serialized content.
-     * @throws TranslationException
+     * Private constructor to ensure that the class acts as a true utility class
+     * i.e. it isn't instantiable and extensible.
      */
-    private static <T> T getAndDeserialize(String url, String userName, String password,
-            Class<T> clazz) throws TranslationException {
-        try {
-            ClientResource cr = createAuthenticatedClientResource(url, userName, password);
-            Representation representation = cr.get();
-
-            representation.setMediaType(MediaType.APPLICATION_JSON);
-            JacksonRepresentation<T> jacksonRepresentation = new JacksonRepresentation<T>(representation, clazz);
-            jacksonRepresentation.getObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-
-            return jacksonRepresentation.getObject();
-        } catch (Exception e) {
-            throw new TranslationException("file", "Could not deserialize object at " + url +
-                    " as " + clazz.getName(), e);
-        }
+    private SwaggerUtils() {
     }
 
     /**
@@ -133,26 +77,19 @@ public abstract class SwaggerUtils {
         if (swaggerUrl == null) {
             throw new TranslationException("url", "You did not provide any URL");
         }
-        Pattern p = Pattern
-                .compile("^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]");
-        boolean remote = p.matcher(swaggerUrl).matches();
+
         ResourceListing resourceListing;
         Map<String, ApiDeclaration> apis = new HashMap<String, ApiDeclaration>();
-        if (remote) {
+        if (ImportUtils.isRemoteUrl(swaggerUrl)) {
             LOGGER.log(Level.FINE, "Reading file: " + swaggerUrl);
-            resourceListing = getAndDeserialize(swaggerUrl, userName, password, ResourceListing.class);
+            resourceListing = ImportUtils.getAndDeserialize(swaggerUrl, userName, password, ResourceListing.class);
             for (ResourceListingApi api : resourceListing.getApis()) {
                 LOGGER.log(Level.FINE,
                         "Reading file: " + swaggerUrl + api.getPath());
                 apis.put(
                         api.getPath(),
-                        getAndDeserialize(swaggerUrl + api.getPath(), userName, password, ApiDeclaration.class));
-            }
-            try {
-                // FIXME: remove when jetty client is fixed. See issue #1077
-                client.stop();
-            } catch (Exception e) {
-                LOGGER.warning("Connection to " + swaggerUrl + "was not closed.");
+                        ImportUtils.getAndDeserialize(swaggerUrl + api.getPath(), userName, password,
+                                ApiDeclaration.class));
             }
         } else {
             File resourceListingFile = new File(swaggerUrl);
@@ -173,12 +110,5 @@ public abstract class SwaggerUtils {
             }
         }
         return SwaggerTranslator.translate(resourceListing, apis);
-    }
-
-    /**
-     * Private constructor to ensure that the class acts as a true utility class
-     * i.e. it isn't instantiable and extensible.
-     */
-    private SwaggerUtils() {
     }
 }
