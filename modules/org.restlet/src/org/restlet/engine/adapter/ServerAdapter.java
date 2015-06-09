0ba601c03685f05346c0a083af08f0e5f8f2b01f
alt250 <alt250@gmail.com>
2015-06-09 17:58:15 +0200

    Properly detect Tomcat and Jetty connection aborted exceptions in Call.isBroken()

--- modules/org.restlet/src/org/restlet/engine/adapter/ServerAdapter.java
+++ modules/org.restlet/src/org/restlet/engine/adapter/ServerAdapter.java
@@ -188,10 +188,8 @@ public class ServerAdapter extends Adapter {
         } catch (Throwable t) {
             // [ifndef gae]
             if (response.getHttpCall().isConnectionBroken(t)) {
-                getLogger()
-                        .log(Level.INFO,
-                                "The connection was broken. It was probably closed by the client.",
-                                t);
+                // output a single log line for this common case to avoid filling servers logs
+                getLogger().log(Level.INFO, "The connection was broken. It was probably closed by the client. Reason: " + t.getMessage());
             } else
             // [enddef]
             {
