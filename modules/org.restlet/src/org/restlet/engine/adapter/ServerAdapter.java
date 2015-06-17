d2b87c228baccef11604d2a2c006be01739a02a5
alt250 <alt250@gmail.com>
2015-06-17 12:02:46 +0200

    Minor change to trigger a new pull request build

--- modules/org.restlet/src/org/restlet/engine/adapter/ServerAdapter.java
+++ modules/org.restlet/src/org/restlet/engine/adapter/ServerAdapter.java
@@ -188,7 +188,7 @@ public class ServerAdapter extends Adapter {
         } catch (Throwable t) {
             // [ifndef gae]
             if (response.getHttpCall().isConnectionBroken(t)) {
-                // output a single log line for this common case to avoid filling servers logs
+                // output a single log line for this common case to avoid filling logs
                 getLogger().log(Level.INFO, "The connection was broken. It was probably closed by the client. Reason: " + t.getMessage());
             } else
             // [enddef]
