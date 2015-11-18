a32a7da21789d3c4e59a475aef3c3d32bb848eb7
Gasrataliev, Timur <Timur.Gasrataliev@billing.ru>
2015-11-18 15:00:50 +0300

    #1167
    added comments

--- modules/org.restlet.test/src/org/restlet/test/engine/header/HeaderReaderTestCase.java
+++ modules/org.restlet.test/src/org/restlet/test/engine/header/HeaderReaderTestCase.java
@@ -7,8 +7,16 @@ import org.restlet.test.RestletTestCase;
 
 import java.io.IOException;
 
+/**
+ * Test case for reading headers
+ *
+ * @author Timur.Gasrataliev
+ */
 public class HeaderReaderTestCase extends RestletTestCase {
 
+    /**
+     * Reading normal header
+     */
     public void testRreadHeader() throws IOException {
         Header result = HeaderReader.readHeader("My-Header: my-header-value");
         Assert.assertNotNull(result);
@@ -16,6 +24,10 @@ public class HeaderReaderTestCase extends RestletTestCase {
         Assert.assertEquals("my-header-value", result.getValue());
     }
 
+    /**
+     * Reading empty header
+     * Should not fail with StringIndexOutOfBoundsException (issue #1167)
+     */
     public void testRreadHeaderEmptyValue() throws IOException {
         Header result = HeaderReader.readHeader("My-Header: ");
         Assert.assertNotNull(result);
