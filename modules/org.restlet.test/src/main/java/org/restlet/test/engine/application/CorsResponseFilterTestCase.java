21b5e718fe2ee62e12e8f02e074b2e50e77436da
thboileau <thboileau@gmail.com>
2017-10-24 11:48:14 +0200

    Added Cors Response filter test case.

---
+++ modules/org.restlet.test/src/main/java/org/restlet/test/engine/application/CorsResponseFilterTestCase.java
@@ -0,0 +1,186 @@
+ /**
+ * Copyright 2005-2017 Restlet
+ * 
+ * The contents of this file are subject to the terms of one of the following
+ * open source licenses: Apache 2.0 or or EPL 1.0 (the "Licenses"). You can
+ * select the license that you prefer but you may not use this file except in
+ * compliance with one of these Licenses.
+ * 
+ * You can obtain a copy of the Apache 2.0 license at
+ * http://www.opensource.org/licenses/apache-2.0
+ * 
+ * You can obtain a copy of the EPL 1.0 license at
+ * http://www.opensource.org/licenses/eclipse-1.0
+ * 
+ * See the Licenses for the specific language governing permissions and
+ * limitations under the Licenses.
+ * 
+ * Alternatively, you can obtain a royalty free commercial license with less
+ * limitations, transferable or non-transferable, directly at
+ * http://restlet.com/products/restlet-framework
+ * 
+ * Restlet is a registered trademark of Restlet S.A.S.
+ */
+
+package org.restlet.test.engine.application;
+
+import org.restlet.Request;
+import org.restlet.Response;
+import org.restlet.data.Method;
+import org.restlet.engine.application.CorsFilter;
+import org.restlet.resource.Get;
+import org.restlet.resource.Options;
+import org.restlet.resource.ServerResource;
+import org.restlet.test.RestletTestCase;
+
+import java.util.Collection;
+
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Manuel Boillod
+ */
+public class CorsResponseFilterTestCase extends RestletTestCase {
+
+    private CorsFilter corsFilter;
+
+    public static class DummyServerResource extends ServerResource {
+        @Options
+        public void doOption(){}
+        @Get
+        public void doGet(){}
+    }
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        corsFilter = new CorsFilter();
+        corsFilter.setNext(DummyServerResource.class);
+    }
+
+    // INVALID CORS REQUESTS
+
+    public void testGet_withoutOrigin() {
+        Request request = new Request();
+        request.setMethod(Method.GET);
+        Response response = corsFilter.handle(request);
+        assertNoCorsHeaders(response);
+    }
+
+    public void testOption_withoutOrigin() {
+        Request request = new Request();
+        request.setMethod(Method.OPTIONS);
+        Response response = corsFilter.handle(request);
+        assertNoCorsHeaders(response);
+    }
+
+    public void testOption_withoutRequestMethod() {
+        Request request = new Request();
+        request.setMethod(Method.OPTIONS);
+        request.getHeaders().set("Origin", "localhost");
+        Response response = corsFilter.handle(request);
+        assertNoCorsHeaders(response);
+    }
+
+    // VALID CORS REQUESTS
+
+    public void testGet() {
+        Request request = new Request();
+        request.setMethod(Method.GET);
+        request.getHeaders().set("Origin", "localhost");
+        Response response = corsFilter.handle(request);
+        assertEquals("*", response.getAccessControlAllowOrigin());
+        assertNull(response.getAccessControlAllowCredentials());
+        assertIsEmpty(response.getAccessControlAllowHeaders());
+        assertIsEmpty(response.getAccessControlAllowMethods());
+        assertIsEmpty(response.getAccessControlExposeHeaders());
+    }
+
+    public void testGet_withAuthenticationAllowed() {
+        corsFilter.setAllowedCredentials(true);
+
+        Request request = new Request();
+        request.setMethod(Method.GET);
+        request.getHeaders().set("Origin", "localhost");
+        Response response = corsFilter.handle(request);
+        assertEquals("localhost", response.getAccessControlAllowOrigin());
+        assertEquals(Boolean.TRUE, response.getAccessControlAllowCredentials());
+        assertIsEmpty(response.getAccessControlAllowHeaders());
+        assertIsEmpty(response.getAccessControlAllowMethods());
+        assertIsEmpty(response.getAccessControlExposeHeaders());
+    }
+
+    public void testOption_requestGet() {
+        Request request = new Request();
+        request.setMethod(Method.OPTIONS);
+        request.getHeaders().set("Origin", "localhost");
+        request.setAccessControlRequestMethod(Method.GET);
+        Response response = corsFilter.handle(request);
+        assertEquals("*", response.getAccessControlAllowOrigin());
+        assertNull(response.getAccessControlAllowCredentials());
+        assertIsEmpty(response.getAccessControlAllowHeaders());
+        assertThat(response.getAccessControlAllowMethods(), contains(Method.GET, Method.OPTIONS));
+        assertIsEmpty(response.getAccessControlExposeHeaders());
+    }
+
+    public void testOption_requestGet_skippingResource() {
+        corsFilter.setSkippingResourceForCorsOptions(true);
+
+        Request request = new Request();
+        request.setMethod(Method.OPTIONS);
+        request.getHeaders().set("Origin", "localhost");
+        request.setAccessControlRequestMethod(Method.GET);
+        Response response = corsFilter.handle(request);
+        assertEquals("*", response.getAccessControlAllowOrigin());
+        assertNull(response.getAccessControlAllowCredentials());
+        assertIsEmpty(response.getAccessControlAllowHeaders());
+        assertThat(response.getAccessControlAllowMethods(), containsInAnyOrder(Method.GET, Method.POST, Method.PUT, Method.DELETE, Method.PATCH));
+        assertIsEmpty(response.getAccessControlExposeHeaders());
+    }
+
+    public void testOption_requestPost_skippingResource() {
+        corsFilter.setSkippingResourceForCorsOptions(true);
+
+        Request request = new Request();
+        request.setMethod(Method.OPTIONS);
+        request.getHeaders().set("Origin", "localhost");
+        request.setAccessControlRequestMethod(Method.POST);
+        Response response = corsFilter.handle(request);
+        assertEquals("*", response.getAccessControlAllowOrigin());
+        assertNull(response.getAccessControlAllowCredentials());
+        assertIsEmpty(response.getAccessControlAllowHeaders());
+        assertThat(response.getAccessControlAllowMethods(), containsInAnyOrder(Method.GET, Method.POST, Method.PUT, Method.DELETE, Method.PATCH));
+        assertIsEmpty(response.getAccessControlExposeHeaders());
+    }
+
+    public void testOption_requestGet_withAuthenticationAllowed() {
+        corsFilter.setAllowedCredentials(true);
+
+        Request request = new Request();
+        request.setMethod(Method.OPTIONS);
+        request.getHeaders().set("Origin", "localhost");
+        request.setAccessControlRequestMethod(Method.GET);
+        Response response = corsFilter.handle(request);
+        assertEquals("localhost", response.getAccessControlAllowOrigin());
+        assertEquals(Boolean.TRUE, response.getAccessControlAllowCredentials());
+        assertIsEmpty(response.getAccessControlAllowHeaders());
+        assertThat(response.getAccessControlAllowMethods(), contains(Method.GET, Method.OPTIONS));
+        assertIsEmpty(response.getAccessControlExposeHeaders());
+    }
+
+    private void assertIsEmpty(Collection<?> collection) {
+        assertNotNull(collection);
+        assertTrue(collection.isEmpty());
+    }
+
+    public void assertNoCorsHeaders(Response response) {
+        assertNull(response.getAccessControlAllowOrigin());
+        assertNull(response.getAccessControlAllowCredentials());
+        assertIsEmpty(response.getAccessControlAllowHeaders());
+        assertIsEmpty(response.getAccessControlAllowMethods());
+        assertIsEmpty(response.getAccessControlExposeHeaders());
+    }
+
+}
