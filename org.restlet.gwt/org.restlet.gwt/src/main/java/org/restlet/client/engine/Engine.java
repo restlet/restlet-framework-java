9900f0e83ab395bab32e50f7215411dde0be8327
Thierry Boileau <thboileau@gmaiil.com>
2025-03-02 10:25:31 +0100

    Bumped version 2.6.0-m2

--- org.restlet.gwt/org.restlet.gwt/src/main/java/org/restlet/client/engine/Engine.java
+++ org.restlet.gwt/org.restlet.gwt/src/main/java/org/restlet/client/engine/Engine.java
@@ -72,7 +72,7 @@ public class Engine {
     public static final String MINOR_NUMBER = "6";
 
     /** Release number. */
-    public static final String RELEASE_NUMBER = "-SNAPSHOT";
+    public static final String RELEASE_NUMBER = ".0-m2";
 
     /** Complete version. */
     public static final String VERSION = MAJOR_NUMBER + '.' + MINOR_NUMBER
