b24ec678bd40e5bedc817cb64f29187e063e84ba
Thierry Boileau <thboileau@gmaiil.com>
2025-03-16 12:31:28 +0100

    HttpBasic test refacto

--- org.restlet.java/org.restlet/src/test/java/org/restlet/security/HttpBasicTestCase.java
+++ org.restlet.java/org.restlet/src/test/java/org/restlet/security/HttpBasicTestCase.java
@@ -9,15 +9,34 @@
 
 package org.restlet.security;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.restlet.*;
-import org.restlet.data.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 import java.util.Arrays;
+import java.util.Base64;
+import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.restlet.Application;
+import org.restlet.Client;
+import org.restlet.Component;
+import org.restlet.Request;
+import org.restlet.Response;
+import org.restlet.Restlet;
+import org.restlet.Server;
+import org.restlet.data.ChallengeResponse;
+import org.restlet.data.ChallengeScheme;
+import org.restlet.data.MediaType;
+import org.restlet.data.Method;
+import org.restlet.data.Protocol;
+import org.restlet.data.Status;
+import org.restlet.engine.io.IoUtils;
 
 /**
  * Restlet unit tests for HTTP Basic authentication client/server.
@@ -27,244 +46,145 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  */
 public class HttpBasicTestCase {
 
-    public static class AuthenticatedRestlet extends Restlet {
-        @Override
-        public void handle(Request request, Response response) {
-            response.setEntity(AUTHENTICATED_MSG, MediaType.TEXT_PLAIN);
-        }
-    }
+	public static class AuthenticatedRestlet extends Restlet {
+		@Override
+		public void handle(Request request, Response response) {
+			response.setEntity(AUTHENTICATED_MSG, MediaType.TEXT_PLAIN);
+		}
+	}
 
-    public static class TestVerifier extends MapVerifier {
-        public TestVerifier() {
-            getLocalSecrets().put(SHORT_USERNAME, SHORT_PASSWORD.toCharArray());
-            getLocalSecrets().put(LONG_USERNAME, LONG_PASSWORD.toCharArray());
-        }
+	public static class TestVerifier extends MapVerifier {
+		public TestVerifier() {
+			getLocalSecrets().put(SHORT_USERNAME, SHORT_PASSWORD.toCharArray());
+			getLocalSecrets().put(LONG_USERNAME, LONG_PASSWORD.toCharArray());
+		}
 
-        @Override
-        public int verify(String identifier, char[] inputSecret) {
-            // NOTE: Allocating Strings are not really secure treatment of passwords
-            String almostSecret = new String(inputSecret);
+		@Override
+		public int verify(String identifier, char[] inputSecret) {
+			// NOTE: Allocating Strings are not really secure treatment of passwords
+			String almostSecret = new String(inputSecret);
 
-            try {
-                return super.verify(identifier, inputSecret);
-            } finally {
-                // Clear secret from memory as soon as possible (This is better
-                // treatment, but useless due to our almostSecret
-                // copy)
-                Arrays.fill(inputSecret, '\000');
-            }
-        }
-    }
+			try {
+				return super.verify(identifier, inputSecret);
+			} finally {
+				// Clear secret from memory as soon as possible (This is better
+				// treatment, but useless due to our almostSecret copy)
+				Arrays.fill(inputSecret, '\000');
+			}
+		}
+	}
 
-    public static final String AUTHENTICATED_MSG = "You are authenticated";
+	public static class BlockerVerifier implements Verifier {
+		@Override
+		public int verify(Request request, Response response) {
+			return RESULT_INVALID;
+		}
+	}
 
-    public static final String LONG_PASSWORD = "thisLongPasswordIsExtremelySecure";
+	public static final String AUTHENTICATED_MSG = "You are authenticated";
 
-    public static final String LONG_USERNAME = "aVeryLongUsernameIsIndeedRequiredForThisTest";
+	public static final String LONG_PASSWORD = "thisLongPasswordIsExtremelySecure";
 
-    public static final String SHORT_PASSWORD = "pw15";
+	public static final String LONG_USERNAME = "aVeryLongUsernameIsIndeedRequiredForThisTest";
 
-    public static final String SHORT_USERNAME = "user13";
+	public static final String SHORT_PASSWORD = "pw15";
 
-    public static final String WRONG_USERNAME = "wrongUser";
+	public static final String SHORT_USERNAME = "user13";
 
-    private ChallengeAuthenticator authenticator;
+	public static final String WRONG_USERNAME = "wrongUser";
 
-    private Component component;
+	static Stream<Arguments> invalidCredentials() {
+		return Stream.of(arguments(LONG_USERNAME, SHORT_PASSWORD), arguments(SHORT_USERNAME, LONG_PASSWORD),
+				arguments(WRONG_USERNAME, SHORT_PASSWORD));
+	}
 
-    private String uri;
+	static Stream<Arguments> validCredentials() {
+		return Stream.of(arguments(LONG_USERNAME, LONG_PASSWORD), arguments(SHORT_USERNAME, SHORT_PASSWORD));
+	}
 
-    private MapVerifier verifier;
+	@Nested
+	class TestMapVerifier {
 
-    @Test
-    public void guardLong() {
-        assertEquals(
-                Verifier.RESULT_VALID,
-                this.verifier.verify(LONG_USERNAME, LONG_PASSWORD.toCharArray()),
-                "Didn't authenticate short user/pwd"
-        );
-    }
+		private final MapVerifier verifier = new TestVerifier();
 
-    @Test
-    public void guardLongWrong() {
-        assertEquals(
-                Verifier.RESULT_INVALID,
-                this.verifier.verify(LONG_USERNAME, SHORT_PASSWORD.toCharArray()),
-                "Authenticated long username with wrong password"
-        );
-    }
+		@ParameterizedTest
+		@MethodSource("org.restlet.security.HttpBasicTestCase#invalidCredentials")
+		void testInvalidCredentials(final String login, final String password) {
+			assertEquals(Verifier.RESULT_INVALID, this.verifier.verify(login, password.toCharArray()));
+		}
 
-    // Test our guard.checkSecret() stand-alone
-    @Test
-    public void guardShort() {
-        assertEquals(
-                Verifier.RESULT_VALID,
-                this.verifier.verify(SHORT_USERNAME, SHORT_PASSWORD.toCharArray()),
-                "Didn't authenticate short user/pwd"
-        );
-    }
+		@ParameterizedTest
+		@MethodSource("org.restlet.security.HttpBasicTestCase#validCredentials")
+		void testValidCredentials(final String login, final String password) {
+			assertEquals(Verifier.RESULT_VALID, this.verifier.verify(login, password.toCharArray()));
+		}
 
-    @Test
-    public void guardShortWrong() {
-        assertEquals(
-                Verifier.RESULT_INVALID,
-                this.verifier.verify(SHORT_USERNAME, LONG_PASSWORD.toCharArray()),
-                "Authenticated short username with wrong password"
-        );
-    }
+	}
 
-    @Test
-    public void guardWrongUser() {
-        assertEquals(
-                Verifier.RESULT_INVALID,
-                this.verifier.verify(WRONG_USERNAME, SHORT_PASSWORD.toCharArray()),
-                "Authenticated wrong username"
-        );
-    }
+	@Nested
+	class TestHttpBasicServer {
+		private Component component;
+		private Request request;
+		private Client client;
 
-    public void HttpBasicLong() throws Exception {
-        Request request = new Request(Method.GET, this.uri);
-        Client client = new Client(Protocol.HTTP);
+		@Test
+		public void HttpBasicNone() throws Exception {
+			final Response response = client.handle(request);
+			assertEquals(Status.CLIENT_ERROR_UNAUTHORIZED, response.getStatus());
+		}
 
-        ChallengeResponse authentication = new ChallengeResponse(
-                ChallengeScheme.HTTP_BASIC, LONG_USERNAME, LONG_PASSWORD);
-        request.setChallengeResponse(authentication);
+		@ParameterizedTest
+		@MethodSource("org.restlet.security.HttpBasicTestCase#invalidCredentials")
+		void testInvalidCredentials(final String login, final String password) throws Exception {
+			ChallengeResponse authentication = new ChallengeResponse(ChallengeScheme.HTTP_BASIC, login, password);
+			request.setChallengeResponse(authentication);
 
-        final Response response = client.handle(request);
-        assertEquals(
-                Status.SUCCESS_OK, response.getStatus(),
-                "Long username did not return 200 OK"
-        );
-        assertEquals(AUTHENTICATED_MSG, response.getEntity().getText());
+			final Response response = client.handle(request);
+			assertEquals(Status.CLIENT_ERROR_UNAUTHORIZED, response.getStatus());
+		}
 
-        client.stop();
-    }
+		@ParameterizedTest
+		@MethodSource("org.restlet.security.HttpBasicTestCase#validCredentials")
+		void testValidCredentials(final String login, final String password) throws Exception {
+			ChallengeResponse authentication = new ChallengeResponse(ChallengeScheme.HTTP_BASIC, login, password);
+			request.setChallengeResponse(authentication);
 
-    public void HttpBasicLongWrong() throws Exception {
-        final Request request = new Request(Method.GET, this.uri);
-        final Client client = new Client(Protocol.HTTP);
+			final Response response = client.handle(request);
+			assertEquals(Status.SUCCESS_OK, response.getStatus());
+			assertEquals(AUTHENTICATED_MSG, response.getEntity().getText());
+		}
 
-        final ChallengeResponse authentication = new ChallengeResponse(
-                ChallengeScheme.HTTP_BASIC, LONG_USERNAME, SHORT_PASSWORD);
-        request.setChallengeResponse(authentication);
+		@BeforeEach
+		public void makeServer() throws Exception {
+			final String REALM = HttpBasicTestCase.class.getSimpleName();
+			this.component = new Component();
+			final Server server = this.component.getServers().add(Protocol.HTTP, 0);
 
-        final Response response = client.handle(request);
+			final Application application = new Application() {
+				@Override
+				public Restlet createInboundRoot() {
+					ChallengeAuthenticator authenticator = new ChallengeAuthenticator(getContext(),
+							ChallengeScheme.HTTP_BASIC, REALM);
+					authenticator.setVerifier(new TestVerifier());
+					authenticator.setNext(new AuthenticatedRestlet());
+					return authenticator;
+				}
+			};
 
-        assertEquals(
-                Status.CLIENT_ERROR_UNAUTHORIZED, response.getStatus(),
-                "Long username w/wrong pw did not throw 403"
-        );
+			this.component.getDefaultHost().attach(application);
+			this.component.start();
+			request = new Request(Method.GET, "http://localhost:" + server.getActualPort());
+			client = new Client(Protocol.HTTP);
+		}
 
-        client.stop();
-    }
-
-    // Test various HTTP Basic auth connections
-    public void HttpBasicNone() throws Exception {
-        final Request request = new Request(Method.GET, this.uri);
-        final Client client = new Client(Protocol.HTTP);
-        final Response response = client.handle(request);
-        assertEquals(
-                Status.CLIENT_ERROR_UNAUTHORIZED, response.getStatus(),
-                "No user did not throw 401"
-        );
-        client.stop();
-    }
-
-    public void HttpBasicShort() throws Exception {
-        final Request request = new Request(Method.GET, this.uri);
-        final Client client = new Client(Protocol.HTTP);
-
-        final ChallengeResponse authentication = new ChallengeResponse(
-                ChallengeScheme.HTTP_BASIC, SHORT_USERNAME, SHORT_PASSWORD);
-        request.setChallengeResponse(authentication);
-
-        final Response response = client.handle(request);
-        assertEquals(
-                Status.SUCCESS_OK, response.getStatus(),
-                "Short username did not return 200 OK"
-        );
-        assertEquals(AUTHENTICATED_MSG, response.getEntity().getText());
-
-        client.stop();
-    }
-
-    public void HttpBasicShortWrong() throws Exception {
-        final Request request = new Request(Method.GET, this.uri);
-        final Client client = new Client(Protocol.HTTP);
-
-        final ChallengeResponse authentication = new ChallengeResponse(
-                ChallengeScheme.HTTP_BASIC, SHORT_USERNAME, LONG_PASSWORD);
-        request.setChallengeResponse(authentication);
-
-        final Response response = client.handle(request);
-
-        assertEquals(
-                Status.CLIENT_ERROR_UNAUTHORIZED, response.getStatus(),
-                "Short username did not throw 401"
-        );
-
-        client.stop();
-    }
-
-    public void HttpBasicWrongUser() throws Exception {
-        final Request request = new Request(Method.GET, this.uri);
-        final Client client = new Client(Protocol.HTTP);
-
-        final ChallengeResponse authentication = new ChallengeResponse(
-                ChallengeScheme.HTTP_BASIC, WRONG_USERNAME, SHORT_PASSWORD);
-        request.setChallengeResponse(authentication);
-
-        final Response response = client.handle(request);
-
-        assertEquals(
-                Status.CLIENT_ERROR_UNAUTHORIZED, response.getStatus(),
-                "Wrong username did not throw 401"
-        );
-
-        client.stop();
-    }
-
-    @BeforeEach
-    public void makeServer() throws Exception {
-        this.component = new Component();
-        final Server server = this.component.getServers().add(Protocol.HTTP, 0);
-
-        final Application application = new Application() {
-            @Override
-            public Restlet createInboundRoot() {
-                HttpBasicTestCase.this.verifier = new TestVerifier();
-                HttpBasicTestCase.this.authenticator = new ChallengeAuthenticator(
-                        getContext(), ChallengeScheme.HTTP_BASIC,
-                        HttpBasicTestCase.class.getSimpleName());
-                HttpBasicTestCase.this.authenticator
-                        .setVerifier(HttpBasicTestCase.this.verifier);
-                HttpBasicTestCase.this.authenticator
-                        .setNext(new AuthenticatedRestlet());
-                return HttpBasicTestCase.this.authenticator;
-            }
-        };
-
-        this.component.getDefaultHost().attach(application);
-        this.component.start();
-        this.uri = "http://localhost:" + server.getActualPort() + "/";
-    }
-
-    @AfterEach
-    public void stopServer() throws Exception {
-        if (this.component.isStarted()) {
-            this.component.stop();
-        }
-        this.component = null;
-    }
-
-    @Test
-    public void testHttpBasic() throws Exception {
-        HttpBasicWrongUser();
-        HttpBasicShort();
-        HttpBasicShortWrong();
-        HttpBasicNone();
-        HttpBasicLong();
-        HttpBasicLongWrong();
-    }
+		@AfterEach
+		public void cleanup() throws Exception {
+			client.stop();
+			if (this.component.isStarted()) {
+				this.component.stop();
+			}
+			this.component = null;
+		}
+	}
 
 }
