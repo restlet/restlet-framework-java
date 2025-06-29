84848c117e924e07bb5103a204605c3d4d56ed0c
Thierry Boileau <thboileau@gmaiil.com>
2025-06-29 20:26:39 +0200

    Bumped version 2.7.0-SNAPSHOT

--- changes.md
+++ changes.md
@@ -1,7 +1,9 @@
 Changes log
 ===========
 
-- 2.7 Milestone 2 (29-07-2025)
+- 2.7 Milestone 3 (??-??-2025)
+
+- 2.7 Milestone 2 (29-06-2025)
     - Misc
       - Removed deprecated Servlet extension and related classes in Spring extension
       - Removed deprecated methods in Restlet API
--- org.restlet/src/main/java/org/restlet/engine/Engine.java
+++ org.restlet/src/main/java/org/restlet/engine/Engine.java
@@ -96,7 +96,7 @@ public class Engine {
     public static final String MINOR_NUMBER = "7";
 
     /** Release number. */
-    public static final String RELEASE_NUMBER = ".0-m2";
+    public static final String RELEASE_NUMBER = ".0-SNAPSHOT";
 
     /** The org.restlet log level. */
     private static volatile Level restletLogLevel;
