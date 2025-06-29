55416bd1b3a77e588aa9b3b6f149f74e03056564
Thierry Boileau <thboileau@gmaiil.com>
2025-06-29 21:00:52 +0200

    Bumped version 2.6.1-SNAPSHOT

--- org.restlet.java/org.restlet/src/main/java/org/restlet/engine/Engine.java
+++ org.restlet.java/org.restlet/src/main/java/org/restlet/engine/Engine.java
@@ -90,7 +90,7 @@ public class Engine {
 	public static final String MINOR_NUMBER = "6";
 
 	/** Release number. */
-	public static final String RELEASE_NUMBER = ".0";
+	public static final String RELEASE_NUMBER = ".1-SNAPSHOT";
 
 	/** The org.restlet log level. */
 	private static volatile Level restletLogLevel;
