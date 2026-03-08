3c04a8c4f5c4e96d7163e78d1f85ca7e1d61128b
Thierry Boileau <thierry.boileau@qlik.com>
2026-03-08 18:55:06 +0100

    Issue #1487: use ThreadLocalRandom to generate random number in RouteList

--- org.restlet/src/main/java/org/restlet/util/RouteList.java
+++ org.restlet/src/main/java/org/restlet/util/RouteList.java
@@ -14,11 +14,10 @@ import org.restlet.Response;
 import org.restlet.Restlet;
 import org.restlet.routing.Route;
 
-import java.security.SecureRandom;
 import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Modifiable list of routes with some helper methods. Note that this class
@@ -37,8 +36,6 @@ import java.util.concurrent.CopyOnWriteArrayList;
 public final class RouteList extends WrapperList<Route> {
 	/** The index of the last route used in the round-robin mode. */
 	private volatile int lastIndex;
-	/** Used when asked to return a random route. */
-	private final SecureRandom random = new SecureRandom();
 
 	/**
 	 * Constructor.
@@ -162,7 +159,7 @@ public final class RouteList extends WrapperList<Route> {
 		int length = size();
 
 		if (length > 0) {
-			int j = random.nextInt(length);
+			int j = ThreadLocalRandom.current().nextInt(length);
 			Route route = get(j);
 
 			if (route.score(request, response) >= requiredScore) {
